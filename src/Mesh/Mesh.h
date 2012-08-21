c881b06b3729a0955e3aa82b1a5cad61536ae338
Jozsef Bakosi <jbakosi@lanl.gov>
2012-08-20 19:19:26 -0600

    More cleanup, even more to come

--- src/Mesh/Mesh.h
+++ src/Mesh/Mesh.h
@@ -1,26 +1,15 @@
-//  ------------------------------------------------------------------------------------------------------------
-//
-//  Copyright 2007 Jozsef Bakosi
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  ------------------------------------------------------------------------------------------------------------
-//
-//  externally callable functions defined in mesh.cc
-//
+// -----------------------------------------------------------------------------
+// \file    src/Mesh/Mesh.h
+// \author  jbakosi
+// \date    Thu Aug 14 9:32:00 2012
+// \brief   Unstructured mesh
+// \note    Copyright 2012 Jozsef Bakosi
+//          All rights reserved.
+// -----------------------------------------------------------------------------
 
-void prepmsh( int *npoin, int *nbpoin, int *nelem, double **coord, int **bpg, int **binpoel,
-	      int **inpoel, int **esup1, int **esup2, int **psup1, int **psup2, int **bpsup1,
-	      int **bpsup2, int **esupel1, int **esupel2, int **esuel, int**bptags, int **betags,
-	      double **Ae, double **dNx, double **dNy, double **dete, double **sqrtAp, double *minsqrtAp );
+void prepmsh(int *npoin, int *nbpoin, int *nelem, double **coord, int **bpg,
+             int **binpoel, int **inpoel, int **esup1, int **esup2, int **psup1,
+             int **psup2, int **bpsup1, int **bpsup2, int **esupel1,
+             int **esupel2, int **esuel, int**bptags, int **betags, double **Ae,
+             double **dNx, double **dNy, double **dete, double **sqrtAp,
+             double *minsqrtAp);
