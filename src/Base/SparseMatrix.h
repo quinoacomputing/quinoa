7500d7325ea740bdcfa6b610c9be106abd36b80f
Jozsef Bakosi <jbakosi@lanl.gov>
2012-08-26 21:22:37 -0600

    Revamp of SparseMatrix in progress...

--- src/Base/SparseMatrix.h
+++ src/Base/SparseMatrix.h
@@ -1,30 +1,52 @@
-// -----------------------------------------------------------------------------
-// \file    src/Base/SparseMatrix.h
-// \author  jbakosi
-// \date    The Aug 14 9:32:00 2012
-// \brief   Sparse Matrix functions
-// \note    Copyright 2012 Jozsef Bakosi
-//          All rights reserved.
-// -----------------------------------------------------------------------------
+//******************************************************************************
+/*!
+  \file      src/Base/SparseMatrix.h
+  \author    J. Bakosi
+  \date      Sun 26 Aug 2012 09:02:02 PM MDT
+  \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
+  \brief     Sparse matrix declaration
+  \details   Sparse matrix base class declaration
+*/
+//******************************************************************************
+#ifndef SparseMatrix_h
+#define SparseMatrix_h
 
-// structure prototype for sparse matrices
-struct sparsemat {
-      int size; // size: 1, matrix of (dof x size) x (dof x size)
-      int rsize;// size: 1, real size of matrix (rsize = dof x size)
-      int dof;  // size: 1, number of unknowns/node (degree of freedom)
-      int nnz;  // size: 1, total number of nonzeros
-      int *rnz; // size: size, number of nonzeros of each row
-      int *ia;  // size: size*dof+1, row pointers
-      int *ja;  // size: nnz, column indices
-      double *a;// size: nnz, nonzero values
-                // sizes: rsize, auxiliary vectors for conjugate gradients
-      double *r, *p, *z, *q, *d, *u;
-      int nlev; // size: 1, number of levels for level scheduling
-      int *lev1, *lev2;	// sizes: rsize,nlev+1	linked lists for level scheduling
+namespace Quinoa {
+
+//! Sparse matrix base class
+class SparseMatrix {
+
+  public:
+
+    //! Constructor
+             SparseMatrix(int size, int dof, int *psup1, int* psup2);
+    //! Destructor
+    virtual ~SparseMatrix();
+
+  private:
+
+    // Don't permit copy or assignment operators
+    SparseMatrix(const SparseMatrix&);
+    SparseMatrix& operator=(const SparseMatrix&);
+
+    int m_size;   //!< Size of matrix: (dof x size) x (dof x size)
+    int m_rsize;  //!< Width of matrix: dof x size
+    int m_dof;    //!< Number of degrees of freedom
+    int m_nnz;    //!< Total number of nonzeros
+    int *m_rnz;   //!< Number of nonzeros of each row, vector size: size
+    int *m_ia;    //!< Row pointers, vector size: size*dof+1
+    int *m_ja;    //!< Column indices, vector size: nnz
+    double *m_a;  //!< Nonzero values, vector size: nnz
+
+//    double *m_r, *m_p, *m_z, *m_q, *m_d, *m_u;
+//             //!< sizes: rsize, auxiliary vectors for conjugate gradients
+//    int m_nlev; //!< size: 1, number of levels for level scheduling
+//    int *m_lev1, *m_lev2;//!< sizes: rsize,nlev+1 linked lists for level scheduling
 };
 
-void create_sparsemat( sparsemat *m, int DOF, int size, int *psup1, int *psup2 );
+//! Free memory allocated for data structures of sparse matrix
 void destroy_sparsemat( sparsemat *m );
+
 void addmr( sparsemat *M, int row, int column, int i, double value );
 void addma( sparsemat *M, int row, int column, double value );
 void insmr( sparsemat *M, int row, int column, int i, double value );
@@ -38,3 +60,7 @@ void printmat_as_structure( sparsemat *M );
 void printmat_as_matrix( sparsemat *M );
 void printmat_as_matlab( sparsemat *M );
 void printmat2file_as_stored( sparsemat *M, char *filename, double *rhs );
+
+} // namespace Quinoa
+
+#endif // SparseMatrix.h
