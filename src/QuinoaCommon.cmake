82b5051f513c922b6b56ad62c9d7453f8b1ab760
Jozsef Bakosi <jbakosi@lanl.gov>
2012-08-30 23:31:19 -0600

    Add linux-gnu platform file
    icpc-12 clashes with gcc-4.7 header files, will use intel compilers in
    the future

--- src/QuinoaCommon.cmake
+++ src/QuinoaCommon.cmake
@@ -18,35 +18,35 @@ set(CMAKE_VERBOSE_MAKEFILE 1)
 #          ${INTEL_INCLUDE_GUESS}
 #)
 
-include_directories($ENV{MKLROOT}/include
-)
+#include_directories($ENV{MKLROOT}/include
+#)
 
 ### Libraries ###
 
-find_library(PTHREAD_LIBRARY
-             NAMES pthread
-             PATHS /usr/lib
-)
-
-find_library(MATH_LIBRARY
-             NAMES libm.a
-             PATHS /usr/lib64
-)
-
-find_library(MKL_INTEL_LIBRARY
-             NAMES mkl_intel_ilp64
-             PATHS $ENV{MKLROOT}/lib/intel64
-)
-
-find_library(MKL_INTEL_THREAD_LIBRARY
-             NAMES mkl_intel_thread
-             PATHS $ENV{MKLROOT}/lib/intel64
-)
-
-find_library(MKL_CORE_LIBRARY
-             NAMES mkl_core
-             PATHS $ENV{MKLROOT}/lib/intel64
-)
+#find_library(PTHREAD_LIBRARY
+#             NAMES pthread
+#             PATHS /usr/lib
+#)
+#
+#find_library(MATH_LIBRARY
+#             NAMES libm.a
+#             PATHS /usr/lib64
+#)
+#
+#find_library(MKL_INTEL_LIBRARY
+#             NAMES mkl_intel_ilp64
+#             PATHS $ENV{MKLROOT}/lib/intel64
+#)
+#
+#find_library(MKL_INTEL_THREAD_LIBRARY
+#             NAMES mkl_intel_thread
+#             PATHS $ENV{MKLROOT}/lib/intel64
+#)
+#
+#find_library(MKL_CORE_LIBRARY
+#             NAMES mkl_core
+#             PATHS $ENV{MKLROOT}/lib/intel64
+#)
 
 #find_library(EXO_C_LIBRARY
 #             NAMES exoIIv2c
