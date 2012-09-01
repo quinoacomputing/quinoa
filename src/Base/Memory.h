a5c7bb49e96cb258654a6b0fc6e13df3bc87f541
Jozsef Bakosi <jbakosi@lanl.gov>
2012-09-01 15:22:50 -0600

    Memory store basic functionaly in place

--- src/Base/Memory.h
+++ src/Base/Memory.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Base/Memory.h
   \author    J. Bakosi
-  \date      Thu 30 Aug 2012 11:08:36 PM MDT
+  \date      Sat 01 Sep 2012 03:20:20 PM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Memory store declaration
   \details   Memory store base class declaration
@@ -11,21 +11,86 @@
 #ifndef Memory_h
 #define Memory_h
 
-#include <set>
+#include <map>
+#include <string>
+#include <cassert>
+#include <iostream>
 
 #include <MemoryEntry.h>
+#include <Macros.h>
 
 namespace Quinoa {
 
 //! Memory store base class
+template<class V>
 class Memory {
+
+  typedef map<Int, MemoryEntry<V>*> EntryMap;
 
   public:
     //! Constructor
-             Memory();
+    Memory() {
+      m_entries = 0;
+    }
 
-    //! Destructor
-    virtual ~Memory();
+    //! Destructor: Free all allocated memory when leaving scope
+    ~Memory() {
+      freeAll();
+    }
+
+    //! Allocate memory entry
+    Int newEntry(size_t number,
+                 VarType varType,
+                 string name,
+                 bool plot = false,
+                 bool restart = false) {
+      assert(number > 0);
+      assert(0 <= varType && varType < NUM_VAR_TYPES);
+      assert(name.size() > 0);
+      V* ptr;
+      m_entry[m_entries] = new MemoryEntry<V>(number,
+                                              varType,
+                                              name,
+                                              plot,
+                                              restart,
+                                              ptr = new V [number*VarComp[varType]]);
+      cout << name << ": " << ptr << endl;
+      return m_entries++;
+    }
+
+    //! Deallocate all memory entries
+    void freeAll() {
+      if (m_entries) {
+        do delete m_entry[--m_entries]; while (m_entries);
+        m_entry.clear();
+      }
+    }
+
+    //! Return raw pointer for memory entry
+    V* getPtr(Int id) {
+      typename EntryMap::const_iterator it = m_entry.find(id);
+      if (it!=m_entry.end()) {
+        return it->second->m_ptr;
+      } else {
+        ERR("Variable not found!");
+      }
+    }
+
+    //! Return number of allocated bytes
+    size_t getBytes() {
+      if (m_entries) {
+        size_t bytes = 0;
+        typename EntryMap::const_iterator it;
+        for (it=m_entry.begin(); it!=m_entry.end(); it++) {
+          MemoryEntry<V>* e = it->second;
+          bytes += 3*sizeof(MemoryEntry<V>) +
+                   e->m_number * VarComp[e->m_type] * sizeof(V);
+        }
+        return bytes;
+      } else {
+        return 0;
+      }
+    }
 
   private:
     //! Don't permit copy operator
@@ -33,22 +98,13 @@ class Memory {
     //! Don't permit assigment operator
     Memory& operator=(const Memory&);
 
-    //! Allocate memory entry
-    void allocateEntry(size_t number,
-                       VariableType type,
-                       int dim,
-                       string name,
-                       StorageLayout layout = LINEAR_ARRAY,
-                       bool plot = false,
-                       bool restart = false);
-
-    //! Current number of entries in memory store
-    int m_entries;
+    //! Current number of entries in the memory store
+    Int m_entries;
 
     //! Memory entries
-    set<MemoryEntry> m_entry;
+    EntryMap m_entry;
 };
 
 } // namespace Quinoa
 
-#endif // Memory.h
+#endif // Memory_h
---
+++ src/Base/QuinoaTypes.h
@@ -0,0 +1,22 @@
+//******************************************************************************
+/*!
+  \file      src/Base/QuinoaTypes.h
+  \author    J. Bakosi
+  \date      Sat 01 Sep 2012 10:09:17 AM MDT
+  \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
+  \brief     Code-global type definitions
+  \details   Code-global type definitions
+*/
+//******************************************************************************
+#ifndef QuinoaTypes_h
+#define QuinoaTypes_h
+
+namespace Quinoa {
+
+typedef bool Bool;
+typedef int Int;
+typedef double Real;
+
+} // namespace Quinoa
+
+#endif // QuinoaTypes_h
