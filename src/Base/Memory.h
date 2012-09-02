0047b8b1c6de0a66c325eb9f9b01a4c9b9a69eb6
Jozsef Bakosi <jbakosi@lanl.gov>
2012-09-02 15:01:33 -0600

    Memory manager now stores in one vector with pointer conversion

--- src/Base/Memory.h
+++ src/Base/Memory.h
@@ -2,95 +2,53 @@
 /*!
   \file      src/Base/Memory.h
   \author    J. Bakosi
-  \date      Sat 01 Sep 2012 03:20:20 PM MDT
+  \date      Sun 02 Sep 2012 02:49:00 PM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
-  \brief     Memory store declaration
-  \details   Memory store base class declaration
+  \brief     Memory (a store for MemoryEntry objects) base class declaration
+  \details   Memory (a store for MemoryEntry objects) base class declaration
 */
 //******************************************************************************
 #ifndef Memory_h
 #define Memory_h
 
-#include <map>
+#include <vector>
 #include <string>
 #include <cassert>
 #include <iostream>
 
 #include <MemoryEntry.h>
-#include <Macros.h>
 
 namespace Quinoa {
 
-//! Memory store base class
-template<class V>
+//! Templated memory store for variable type V
 class Memory {
-
-  typedef map<Int, MemoryEntry<V>*> EntryMap;
 
   public:
     //! Constructor
-    Memory() {
-      m_entries = 0;
-    }
+    Memory();
 
     //! Destructor: Free all allocated memory when leaving scope
-    ~Memory() {
-      freeAll();
-    }
+    ~Memory();
 
     //! Allocate memory entry
     Int newEntry(size_t number,
-                 VarType varType,
+                 ValueType value,
+                 VariableType variable,
                  string name,
-                 bool plot = false,
-                 bool restart = false) {
-      assert(number > 0);
-      assert(0 <= varType && varType < NUM_VAR_TYPES);
-      assert(name.size() > 0);
-      V* ptr;
-      m_entry[m_entries] = new MemoryEntry<V>(number,
-                                              varType,
-                                              name,
-                                              plot,
-                                              restart,
-                                              ptr = new V [number*VarComp[varType]]);
-      cout << name << ": " << ptr << endl;
-      return m_entries++;
-    }
+                 Bool plot = false,
+                 Bool restart = false);
 
     //! Deallocate all memory entries
-    void freeAll() {
-      if (m_entries) {
-        do delete m_entry[--m_entries]; while (m_entries);
-        m_entry.clear();
-      }
-    }
+    void freeAll();
 
-    //! Return raw pointer for memory entry
-    V* getPtr(Int id) {
-      typename EntryMap::const_iterator it = m_entry.find(id);
-      if (it!=m_entry.end()) {
-        return it->second->m_ptr;
-      } else {
-        ERR("Variable not found!");
-      }
+    //! Return raw pointer for memory entry,
+    //! template V specifies return pointer type
+    template<class V> V* getPtr(Int id) {
+      return static_cast<V*>(m_entry[id]->m_ptr);
     }
 
     //! Return number of allocated bytes
-    size_t getBytes() {
-      if (m_entries) {
-        size_t bytes = 0;
-        typename EntryMap::const_iterator it;
-        for (it=m_entry.begin(); it!=m_entry.end(); it++) {
-          MemoryEntry<V>* e = it->second;
-          bytes += 3*sizeof(MemoryEntry<V>) +
-                   e->m_number * VarComp[e->m_type] * sizeof(V);
-        }
-        return bytes;
-      } else {
-        return 0;
-      }
-    }
+    size_t getBytes();
 
   private:
     //! Don't permit copy operator
@@ -98,11 +56,11 @@ class Memory {
     //! Don't permit assigment operator
     Memory& operator=(const Memory&);
 
-    //! Current number of entries in the memory store
+    //! Current number of entries
     Int m_entries;
 
     //! Memory entries
-    EntryMap m_entry;
+    vector<MemoryEntry*> m_entry;
 };
 
 } // namespace Quinoa
--- src/Main/Quinoa.C
+++ src/Main/Quinoa.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/Quinoa.C
   \author    J. Bakosi
-  \date      Sat 01 Sep 2012 03:21:38 PM MDT
+  \date      Sat 01 Sep 2012 11:46:43 PM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa main
   \details   Quinoa main
@@ -20,18 +20,20 @@ using namespace Quinoa;
 
 int main(int argc, char* argv[]) {
 
-  Memory<Int> intStore;
-  Memory<Real> realStore;
+  Memory Store;
 
-  Int e1 = intStore.newEntry(10, SCALAR_VAR, "scalars");
-  Int e2 = intStore.newEntry(10000, VECTOR_VAR, "vectors");
-  Int e3 = realStore.newEntry(11234, TENSOR_VAR, "tensors");
+  Int e1 = Store.newEntry(10, INT_VAL, SCALAR_VAR, "scalars");
+  Int e2 = Store.newEntry(10000, BOOL_VAL, VECTOR_VAR, "vectors");
+  Int e3 = Store.newEntry(11234, REAL_VAL, TENSOR_VAR, "tensors");
 
-  cout << intStore.getBytes() + realStore.getBytes() << endl;
+  cout << Store.getBytes() << endl;
 
-  Int* e1ptr = intStore.getPtr(e1);
+  Int* e1ptr = Store.getPtr<Int>(e1);
+  Bool* e2ptr = Store.getPtr<Bool>(e2);
+  Real* e3ptr = Store.getPtr<Real>(e3);
   cout << e1ptr << endl;
+  cout << e2ptr << endl;
+  cout << e3ptr << endl;
 
-  intStore.freeAll();
-  intStore.freeAll();
+  Store.freeAll();
 }
