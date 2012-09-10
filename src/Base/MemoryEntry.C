aa1a2046325e75f36cad9d1c77b950b8eee41fa1
Jozsef Bakosi <jbakosi@lanl.gov>
2012-09-10 13:55:43 +0900

    Add static-width one-liner for dumping memory entries

--- src/Base/MemoryEntry.C
+++ src/Base/MemoryEntry.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Base/MemoryEntry.C
   \author    J. Bakosi
-  \date      Mon 10 Sep 2012 06:04:31 AM KST
+  \date      Mon 10 Sep 2012 01:54:38 PM KST
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     MemoryEntry base class definition
   \details   Memoryentry base class definition
@@ -22,18 +22,19 @@ string
 MemoryEntry::line()
 //******************************************************************************
 //  One-liner returning all fields
+//  \return  One line info of a memory entry
 //! \author  J. Bakosi
 //******************************************************************************
 {
   stringstream ss;
-  ss << "  " << setw(10) << m_name
-     << "  " << setw(10) << m_number
-     << "  " << setw(10) << m_value
-     << "  " << setw(10) << m_variable
-     << "  " << setw(10) << m_nbytes
-     << "  " << setw(10) << m_plot
-     << "  " << setw(10) << m_restart
-     << "  " << setw(10) << m_ptr
+  ss << "  " << setw(MemoryEntryWidth[0]) << m_name
+     << "  " << setw(MemoryEntryWidth[1]) << m_number
+     << "  " << setw(MemoryEntryWidth[2]) << ValueName[m_value]
+     << "  " << setw(MemoryEntryWidth[3]) << VariableTypeName[m_variable]
+     << "  " << setw(MemoryEntryWidth[4]) << m_nbytes
+     << "  " << setw(MemoryEntryWidth[5]) << (m_plot == true ? "true" : "false")
+     << "  " << setw(MemoryEntryWidth[6]) << (m_restart == true ? "true" : "false")
+     << "  " << setw(MemoryEntryWidth[7]) << m_ptr
      << endl;
   return ss.str();
 }
