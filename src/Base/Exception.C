c9f44033d7f6f1cf340bbf63e1d3edb9a2a1a570
Jozsef Bakosi <jbakosi@lanl.gov>
2012-09-14 15:57:29 -0600

    Catch uncaught exceptions

--- src/Base/Exception.C
+++ src/Base/Exception.C
@@ -2,12 +2,14 @@
 /*!
   \file      src/Base/Exception.C
   \author    J. Bakosi
-  \date      Tue 04 Sep 2012 10:45:42 PM MDT
+  \date      Fri Sep 14 15:56:38 2012
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Exception base class definition
   \details   Exception base class definition
 */
 //******************************************************************************
+
+#include <iostream>
 
 #include <Driver.h>
 #include <Exception.h>
@@ -21,11 +23,14 @@ Exception::handleException(Driver* driver)
 //! \author J. Bakosi
 //******************************************************************************
 {
-  // If exception is fatal, do cleanup and exit
-  if (m_exception == FATAL) {
-    driver->finalize();
-    return FATAL_ERROR;
-  } else {
-    return NONFATAL_ERROR;
+  switch (m_exception) {
+    case UNCAUGHT: // If exception is uncaught, warn and fall through FATAL
+      cerr << "WARNING: Uncaught exception" << endl;
+    case FATAL:    // If exception is fatal, do cleanup and exit
+      cerr << "FATAL ERROR: Attempting cleanup & graceful exit..." << endl;
+      driver->finalize();
+      return FATAL_ERROR;
+    default:
+      return NONFATAL_ERROR;
   }
 }
--- src/Main/Quinoa.C
+++ src/Main/Quinoa.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/Quinoa.C
   \author    J. Bakosi
-  \date      Thu 13 Sep 2012 04:09:56 PM KST
+  \date      Fri Sep 14 15:57:12 2012
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa main
   \details   Quinoa main
@@ -27,6 +27,8 @@ int main(int argc, char* argv[]) {
   Memory memStore(1);   // arg: nthreads
   Driver driver(&memStore);
 
+  //cout << sizeof(ValType) << endl;
+
   ErrorCode error = NO_ERROR;
   try {
 
@@ -34,17 +36,20 @@ int main(int argc, char* argv[]) {
       memStore.newEntry(10, ValType::INT, VarType::SCALAR, "scalars");
 
     UnsMesh mesh(&memStore);
-    GmshReader inMesh("../../tmp/cylinder.msh", &mesh, &memStore);
-    inMesh.read();
-    GmshMeshWriter outMesh("../../tmp/cylinder_out.msh", &mesh, &memStore);
-    outMesh.write();
+    GmshReader inMesh("../../tmp/cylinde.msh", &mesh, &memStore);
+    //inMesh.read();
+    //GmshMeshWriter outMesh("../../tmp/cylinder_out.msh", &mesh, &memStore);
+    //outMesh.write();
 
     memStore.echoAllEntries();
     cout << "Allocated memory: " << memStore.getBytes() << endl;
     memStore.freeEntry(e);
 
-  } catch (MemoryException& e) { error = e.handleException(&driver); }
+  } // catch different exception types
+    catch (MemoryException& e) { error = e.handleException(&driver); }
     catch (MeshException& e) { error = e.handleException(&driver); }
+    // catch uncaught exceptions
+    catch (...) { Exception e(UNCAUGHT); error = e.handleException(&driver); }
 
   if (error != FATAL_ERROR) {
     cout << "still running..." << endl;
