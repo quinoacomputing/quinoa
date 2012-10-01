e9cd26ff52070b0e788c9c816cc4f50f064ad9e8
Jozsef Bakosi <jbakosi@lanl.gov>
2012-09-30 21:13:38 -0600

    Working on GmshTxtMeshReader2D::pack(): connectivities allocated

--- src/Mesh/UnsMesh.C
+++ src/Mesh/UnsMesh.C
@@ -2,13 +2,25 @@
 /*!
   \file      src/Mesh/UnsMesh.C
   \author    J. Bakosi
-  \date      Fri 21 Sep 2012 07:10:46 AM MDT
+  \date      Sun 30 Sep 2012 09:23:35 PM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Unstructured mesh class definition
   \details   Unstructured mesh class definition
 */
 //******************************************************************************
 
-//#include <UnsMesh.h>
+#include <iostream>
 
-//using namespace Quinoa;
+#include <UnsMesh.h>
+
+using namespace Quinoa;
+
+UnsMesh::~UnsMesh()
+//******************************************************************************
+//  Destructor: graceful free memory entries held
+//! \author J. Bakosi
+//******************************************************************************
+{
+  if (m_connLine) m_memory->freeEntry(m_connLine);
+  if (m_connTri)  m_memory->freeEntry(m_connTri);
+}
