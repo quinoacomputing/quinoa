158bea5ae8fc8da5654d7181914af2fd9021cdbb
Jozsef Bakosi <jbakosi@lanl.gov>
2012-10-24 06:18:21 -0600

    JPDF work in progress

--- src/Statistics/JPDF.h
+++ src/Statistics/JPDF.h
@@ -1,18 +1,20 @@
 //******************************************************************************
 /*!
-  \file      src/Statistics/PDF.h
+  \file      src/Statistics/JPDF.h
   \author    J. Bakosi
-  \date      Wed 24 Oct 2012 05:34:36 AM MDT
+  \date      Wed 24 Oct 2012 06:16:55 AM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
-  \brief     PDF estimator
-  \details   PDF estimator
+  \brief     Joint PDF estimator
+  \details   Joint PDF estimator
 */
 //******************************************************************************
 #ifndef PDF_h
 #define PDF_h
 
-#include <cmath>
+#include <cassert>
+#include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 #include <QuinoaTypes.h>
 
@@ -20,30 +22,22 @@ using namespace std;
 
 namespace Quinoa {
 
-//! PDF as unordered_map: bin id as key, counter as mapped value
-typedef unordered_map<int,real> Pdf;
-
-//! PDF estimator base class
-class PDF {
+//! JPDF estimator
+class JPDF {
 
   public:
-    //! Constructor: Initialize PDF container
-    //! \param[in]   binsize    Sample space bin size
-    PDF(const real binsize) : m_binsize(binsize), m_nsample(0) {}
+    //! Constructor: Initialize joint PDF container
+    JPDF(const real dim, const real binsize);
 
     //! Destructor: Clear PDF container
-    ~PDF() { m_pdf.clear(); }
+    ~JPDF();
 
     //! Insert new value into PDF
-    //! \param[in]   value    Value to insert
-    void insert(const real& value) {
-      ++m_nsample;
-      ++m_pdf[floor(value/m_binsize+0.5)];
-    }
+    void insert(const vector<real>& value);
 
     //! Constant accessor to PDF
     //! \param[out] Pointer to Pdf
-    const Pdf* getPDF() const { return &m_pdf; }
+    //const Pdf* getPDF() const { return &m_pdf; }
     //! Constant accessor to binsize
     //! \param[out] Sample space bin size
     const real& getBinsize() const { return m_binsize; }
@@ -53,14 +47,31 @@ class PDF {
 
   private:
     //! Don't permit copy constructor
-    PDF(const PDF&) = delete;
+    JPDF(const JPDF&) = delete;
     //! Don't permit copy assigment
-    PDF& operator=(const PDF&) = delete;
+    JPDF& operator=(const JPDF&) = delete;
     //! Don't permit move constructor
-    PDF(PDF&&) = delete;
+    JPDF(JPDF&&) = delete;
     //! Don't permit move assigment
-    PDF& operator=(PDF&&) = delete;
+    JPDF& operator=(JPDF&&) = delete;
 
+    //! Key type
+    using key_type = vector<int>;
+
+    // Hash function for vector<int>
+    struct key_hash {
+      size_t operator()(const key_type& key) const {
+        size_t h = 0;
+        for (auto& k : key) h ^= hash<int>()(k);
+        return h;
+      }
+    };
+
+    //! Joint PDF as unordered_map: bin ids as key, counter as mapped value
+    using Pdf = unordered_map<key_type,real,key_hash>;
+
+    key_type m_key;         //! Temporary key for finding the sample space bin
+    const int m_dim;        //!< Sample space dimension
     const real m_binsize;   //!< Sample space bin size
     int m_nsample;          //!< Number of samples collected
     Pdf m_pdf;              //!< Probability density function
