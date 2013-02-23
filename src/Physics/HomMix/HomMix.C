60fe005a55ca172de66a0e529f584543e7564efd
Jozsef Bakosi <jbakosi@lanl.gov>
2013-02-22 23:04:14 -0700

    Format screen output in HomMix

--- src/Physics/HomMix/HomMix.C
+++ src/Physics/HomMix/HomMix.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Physics/HomMix/HomMix.C
   \author    J. Bakosi
-  \date      Fri Feb 22 16:30:10 2013
+  \date      Fri 22 Feb 2013 11:03:03 PM MST
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Homogeneous material mixing
   \details   Homogeneous material mixing
@@ -10,6 +10,7 @@
 //******************************************************************************
 
 #include <sstream>
+#include <iomanip>
 
 //#include <sys/time.h>
 
@@ -77,6 +78,7 @@ HomMix::solve()
 {
   int it=0;
   real t=0.0;
+  bool wroteJPDF=false;
 
   const auto nstep = m_control->get<NSTEP>();
   const auto ttyi  = m_control->get<TTYI>();
@@ -85,20 +87,24 @@ HomMix::solve()
 
   m_timer->start(m_totalTime);
 
+  reportHeader();
+
   // Time stepping loop
   while (fabs(t-m_term) > numeric_limits<real>::epsilon() && it < nstep) {
 
     // Advance particles
     m_mix->advance(dt);
 
-    // Echo one-liner info
-    if (!(it % ttyi)) {
-      report(it, nstep, t, dt);
-    }
-
     // Output pdf at selected times
     if (!(it % pdfi)) {
       outJPDF(t);
+      wroteJPDF = true;
+    }
+
+    // Echo one-liner info
+    if (!(it % ttyi)) {
+      report(it, nstep, t, dt, wroteJPDF);
+      wroteJPDF = false;
     }
 
     // Increase timestep and iteration counter
@@ -106,29 +112,50 @@ HomMix::solve()
     ++it;
     if (t > m_term) t = m_term;
   }
+
+  report(it, nstep, t, dt, wroteJPDF);
 }
 
 void
-HomMix::report(const int it, const int nstep, const real t, const real dt)
+HomMix::reportHeader()
 //******************************************************************************
-//  One-liner report
-//! \param[in]  it        Iteration counter
-//! \param[in]  t         Time counter
-//! \param[in]  dt        Time step size
+//  Echo report header
 //! \author  J. Bakosi
 //******************************************************************************
 {
-  HMS elapsed, estimated;
-  m_timer->query(m_totalTime, elapsed);
-  m_timer->eta(m_totalTime, m_term, t, dt, nstep, it, estimated);
+  cout << "      it               t              dt        ETA    out\n"
+       << "----------------------------------------------------------" << endl;
+}
 
-  cout << "it = " << it << ", t = " << t << "\t dt = " << dt << ", "
-       << elapsed.h.count() << ":"
-       << elapsed.m.count() << ":"
-       << elapsed.s.count() << " "
-       << estimated.h.count() << ":"
-       << estimated.m.count() << ":"
-       << estimated.s.count() << endl;
+void
+HomMix::report(const int it,
+               const int nstep,
+               const real t,
+               const real dt,
+               const bool wroteJPDF)
+//******************************************************************************
+//  One-liner report
+//! \param[in]  it         Iteration counter
+//! \param[in]  nstep      Terminate time
+//! \param[in]  t          Time
+//! \param[in]  dt         Time step size
+//! \param[in]  wroteJPDF  True if joint PDF was written
+//! \author  J. Bakosi
+//******************************************************************************
+{
+  Watch eta;
+  m_timer->query(m_totalTime, eta);
+  m_timer->eta(m_totalTime, m_term, t, nstep, it, eta);
+
+  cout << setfill(' ') << setw(8) << it << "  "
+       << scientific << setprecision(8) << setw(14) << t << "  " << dt << "  "
+       << setfill('0') << setw(3) << eta.h.count() << ":"
+       << setw(2) << eta.m.count() << ":"
+       << setw(2) << eta.s.count() << "  ";
+
+  if (wroteJPDF) cout << "J";
+
+  cout << endl;
 }
 
 void
@@ -139,9 +166,6 @@ HomMix::outJPDF(const real t)
 //! \author  J. Bakosi
 //******************************************************************************
 {
-  // Echo
-  cout << "JPDF at t = " << t << endl;
-
   // Contruct filename
   stringstream ss;
   ss << m_jpdf_filename_base << "." << t << ".msh";
