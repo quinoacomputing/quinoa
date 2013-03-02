66c8d5cd30a818126055ae298bc226359f245d8e
Jozsef Bakosi <jbakosi@lanl.gov>
2013-03-01 21:52:41 -0700

    Parsing of expectations - work in progress
    Now expectations are separated into terms and terms into moments (for
    lack of a better word). Moments take a keword followed by a digit and
    call the push_term::apply() which can push to to the correct location in
    the bundle.
    
    Now we need to come up with a smart mapping between the enums of struct Term
    and the representation of Quantities in PEGTL.
    
    Also central moments need to trigger ordinary moments.

--- src/Control/ControlTypes.h
+++ src/Control/ControlTypes.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Control/ControlTypes.h
   \author    J. Bakosi
-  \date      Sun 24 Feb 2013 08:01:46 PM MST
+  \date      Fri Mar  1 13:47:46 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Types for control and parsing
   \details   Types for control and parsing
@@ -46,6 +46,34 @@ enum MixType { NO_MIX=0,
                NUM_MIX
 };
 
+//! Quantities whose statistics can be estimated
+enum Quantity { TRANSPORTED_SCALAR=0,
+                VELOCITY,
+                PRESSURE,
+                DENSITY
+};
+
+//! Moment specifies which type of moment is computed for a Quantity in a Term
+enum Moment { ORDINARY=0,      //!< Full variable, e.g. Y
+              CENTRAL          //!< Fluctuation, e.g. y = Y - <Y>
+};
+
+//! Term is a Moment of a Quantity to be ensemble averaged
+//! E.g. pressure fluctuation: {PRESSURE, CENTRAL},
+//! Eg. mean scalar: {TRANSPORTED_SCALAR, ORDINARY}
+struct Term {
+  Quantity quantity;
+  Moment moment;
+};
+
+//! Products are N Terms to be multiplied and ensemble averaged
+//! E.g. the scalar flux in x direction needs two terms for ensemble averaging:
+//! (Y-<Y>) and (U-<U>), then the moment is <yu> = <(Y-<Y>)(U-<U>)>
+//! E.g the third mixed central moment of three scalars needs three terms for
+//! ensemble averaging: (Y1-<Y1>), (Y2-<Y2>), and (Y3-<Y3>), then the moment is
+//! <y1y2y3> = <(Y1-<Y1>)(Y2-<Y2>)(Y3-<Y3>)>
+using Product = vector<Term>;
+
 //! Position enum for accessing fields of tuple Bundle
 enum BundlePosition { TITLE=0,
                       PHYSICS,
@@ -68,24 +96,24 @@ enum BundlePosition { TITLE=0,
 };
 
 //! Storage bundle for parsed data
-using Bundle = tuple< string,       //!<  0: Title
-                      PhysicsType,  //!<  1: Physics
-                      HydroType,    //!<  2: Hydrodynamics model
-                      MixType,      //!<  3: Material mix model
-                      int,          //!<  4: Number of time steps to take
-                      real,         //!<  5: Time to terminate time stepping
-                      real,         //!<  6: Size of time step
-                      int,          //!<  7: Number of mixing scalars
-                      int,          //!<  8: Total number of particles
-                      int,          //!<  9: TTY output interval
-                      int,          //!< 10: Dump output interval
-                      int,          //!< 11: Plot output interval
-                      int,          //!< 12: PDF output interval
-                      vector<real>, //!< 13: Vector of parameters 'b'
-                      vector<real>, //!< 14: Vector of parameters 'S'
-                      vector<real>, //!< 15: Vector of parameters 'kappa'
-                      vector<real>, //!< 16: Vector of parameters 'c_ij'
-                      vector<string>//!< 17: Vector of statistics
+using Bundle = tuple< string,         //!<  0: Title
+                      PhysicsType,    //!<  1: Physics
+                      HydroType,      //!<  2: Hydrodynamics model
+                      MixType,        //!<  3: Material mix model
+                      int,            //!<  4: Number of time steps to take
+                      real,           //!<  5: Time to terminate time stepping
+                      real,           //!<  6: Size of time step
+                      int,            //!<  7: Number of mixing scalars
+                      int,            //!<  8: Total number of particles
+                      int,            //!<  9: TTY output interval
+                      int,            //!< 10: Dump output interval
+                      int,            //!< 11: Plot output interval
+                      int,            //!< 12: PDF output interval
+                      vector<real>,   //!< 13: Vector of parameters 'b'
+                      vector<real>,   //!< 14: Vector of parameters 'S'
+                      vector<real>,   //!< 15: Vector of parameters 'kappa'
+                      vector<real>,   //!< 16: Vector of parameters 'c_ij'
+                      vector<Product> //!< 17: Vector of statistics
 >;
 
 //! Vector of bools indicating whether data is set in Bundle during parsing
