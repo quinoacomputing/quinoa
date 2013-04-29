7c83afd01a3d9ef9d96920bc803330fe9badba49
Jozsef Bakosi <jbakosi@lanl.gov>
2013-04-29 16:16:03 -0600

    Make Exception constructor noexcept

--- src/Base/Exception.C
+++ src/Base/Exception.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Base/Exception.C
   \author    J. Bakosi
-  \date      Mon Apr 29 16:00:20 2013
+  \date      Mon Apr 29 16:15:34 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Exception base class definition
   \details   Exception base class definition
@@ -29,14 +29,30 @@ Exception::Exception(const ExceptType except,
   m_except(except)
 //******************************************************************************
 //  Constructor: generate error message
+//! \details No-throw guarantee: this member function never throws exceptions.
 //! \author J. Bakosi
 //******************************************************************************
 {
-  if (m_line) {         // append information in file:line:func if available
-    stringstream s;
-    s << m_message << endl
-      << ">>> Exception in " << m_file << ":" << m_line << ": " << m_func;
-    m_message = s.str();
+  if (m_line) {         // append information on file:line:func if available
+    try {
+
+      stringstream s;
+      s << m_message << endl
+        << ">>> Exception in " << m_file << ":" << m_line << ": " << m_func;
+      m_message = s.str();
+
+    } // Catch std::exception
+      catch (exception& se) {
+        // Emit warning and continue
+        cout << "RUNTIME ERROR in Exception constructor: " << se.what() << endl
+             << "Continuing anyway..." << endl;
+      }
+      // Catch uncaught exceptions
+      catch (...) {
+        // Emit warning and continue
+        cout << "UNKNOWN EXCEPTION in Exception constructor" << endl
+             << "Continuing anyway..." << endl;
+      }
   }
 }
 
