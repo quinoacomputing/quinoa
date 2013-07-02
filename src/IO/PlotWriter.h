2ef28104d9bb40c5494ca2e45ad5095ed2873559
Jozsef Bakosi <jbakosi@lanl.gov>
2013-07-02 15:23:46 -0600

    Get rid of 'using namespace std' in PlotWriter.h

--- src/IO/PlotWriter.h
+++ src/IO/PlotWriter.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/IO/PlotWriter.h
   \author    J. Bakosi
-  \date      Fri Apr 26 17:11:40 2013
+  \date      Tue Jul  2 15:22:40 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     PlotWriter base class declaration
   \details   PlotWriter base class declaration
@@ -16,8 +16,6 @@
 
 #include <QuinoaTypes.h>
 
-using namespace std;
-
 namespace Quinoa {
 
 //! PlotWriter base class
@@ -25,7 +23,7 @@ class PlotWriter {
 
   public:
     //! Constructor: Acquire plot file handle
-    explicit PlotWriter(const string& filename);
+    explicit PlotWriter(const std::string& filename);
 
     //! Destructor: Release plot file handle
     virtual ~PlotWriter() noexcept;
@@ -34,8 +32,8 @@ class PlotWriter {
     //! Interface for plot write
     virtual void write(const int it, const real t) = 0;
 
-    const string m_filename;    //!< Plot file name
-    ofstream m_outPlot;         //!< Plot file output stream
+    const std::string m_filename;    //!< Plot file name
+    std::ofstream m_outPlot;         //!< Plot file output stream
 
   private:
     //! Don't permit copy constructor
