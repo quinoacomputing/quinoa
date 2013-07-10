8c7871d3ea429518784dcaf31106ac27e7296bbb
Jozsef Bakosi <jbakosi@lanl.gov>
2013-07-10 09:12:23 -0600

    Path simplifications due to env modules
    karman gnu/intel/clang and darwin gnu/intel build

--- src/QuinoaCommon.cmake
+++ src/QuinoaCommon.cmake
@@ -1,35 +1,25 @@
-# Common headers/libraries for all code
+# Headers and libraries paths
 
 set(CMAKE_VERBOSE_MAKEFILE 1)
-
-### Include directories
-
-include_directories($ENV{INTEL}/mkl/include)
-
-### Libraries
 
 find_library(MKL_LIBRARY
              NAMES mkl_intel_ilp64
              PATHS $ENV{MKLROOT}/lib/intel64
-             PATHS $ENV{INTEL}/mkl/lib/intel64
 )
 
 find_library(MKL_THREAD_LIBRARY
              NAMES mkl_intel_thread
              PATHS $ENV{MKLROOT}/lib/intel64
-             PATHS $ENV{INTEL}/mkl/lib/intel64
 )
 
 find_library(INTEL_OMP_LIBRARY
              NAMES iomp5
-             PATHS $ENV{INTEL}/lib/intel64
-             PATHS $ENV{INTEL}/compiler/lib/intel64
+             PATHS $ENV{MKLROOT}/../compiler/lib/intel64
 )
 
 find_library(MKL_CORE_LIBRARY
              NAMES mkl_core
              PATHS $ENV{MKLROOT}/lib/intel64
-             PATHS $ENV{INTEL}/mkl/lib/intel64
 )
 
 find_library(PTHREAD_LIBRARY
