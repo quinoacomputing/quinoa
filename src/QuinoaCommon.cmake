2764e8ce15b0a4204cfdae9b776e7f7018efe9a3
Jozsef Bakosi <jbakosi@lanl.gov>
2013-07-26 20:01:47 -0600

    Add comments to find_library calls to QuinoaCommon.cmake

--- src/QuinoaCommon.cmake
+++ src/QuinoaCommon.cmake
@@ -2,7 +2,8 @@
 
 set(CMAKE_VERBOSE_MAKEFILE 1)
 
-find_library(MKL_LIBRARY
+#### MKL
+find_library(MKL_INTERFACE_LIBRARY
              NAMES mkl_intel_ilp64
              PATHS $ENV{MKLROOT}/lib/intel64
 )
@@ -11,39 +12,43 @@ find_library(MKL_THREAD_LIBRARY
              NAMES mkl_intel_thread
              PATHS $ENV{MKLROOT}/lib/intel64
 )
+find_library(MKL_CORE_LIBRARY
+             NAMES mkl_core
+             PATHS $ENV{MKLROOT}/lib/intel64
+)
 
 find_library(INTEL_OMP_LIBRARY
              NAMES iomp5
              PATHS $ENV{MKLROOT}/../compiler/lib/intel64
 )
 
-find_library(MKL_CORE_LIBRARY
-             NAMES mkl_core
-             PATHS $ENV{MKLROOT}/lib/intel64
-)
-
+#### Pthread
 find_library(PTHREAD_LIBRARY
              NAMES pthread
              PATHS /usr/lib
              PATHS /usr/lib/x86_64-redhat-linux5E/lib64
 )
 
+#### Math
 find_library(MATH_LIBRARY
              NAMES m
              PATHS /usr/lib64
              PATHS /usr/lib/x86_64-redhat-linux5E/lib64
 )
 
+#### Z
 find_library(Z_LIBRARY
              NAMES z
              PATHS /usr/lib64
 )
 
+#### Silo
 find_library(SILO_LIBRARY
              NAMES siloh5
              PATHS ${TPL_DIR}/lib
 )
 
+#### HDF5
 find_library(HDF5_LIBRARY
              NAMES hdf5
              PATHS ${TPL_DIR}/lib
