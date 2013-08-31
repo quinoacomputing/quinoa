8b8fbb4196f56622bfa225b63878f0170f6054c5
Jozsef Bakosi <jbakosi@lanl.gov>
2013-08-31 07:38:31 -0600

    Only echo compilers from cmake if set

--- cmake/GetMPICompilers.cmake
+++ cmake/GetMPICompilers.cmake
@@ -9,7 +9,8 @@ function(get_mpi_compilers)
     OUTPUT_VARIABLE   UNDERLYING_C_COMPILER
   )
   # Remove the newline character at the end of UNDERLYING_C_COMPILER
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_C_COMPILER "${UNDERLYING_C_COMPILER}")
+  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_C_COMPILER
+         "${UNDERLYING_C_COMPILER}")
 
   # Find out underlying C++ compiler
   execute_process(
@@ -17,7 +18,8 @@ function(get_mpi_compilers)
     OUTPUT_VARIABLE   UNDERLYING_CXX_COMPILER
   )
   # Remove the newline character at the end of UNDERLYING_CXX_COMPILER
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_CXX_COMPILER "${UNDERLYING_CXX_COMPILER}")
+  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_CXX_COMPILER
+         "${UNDERLYING_CXX_COMPILER}")
 
   # Find out underlying Fortran compiler
   execute_process(
@@ -25,14 +27,33 @@ function(get_mpi_compilers)
     OUTPUT_VARIABLE   UNDERLYING_FORTRAN_COMPILER
   )
   # Remove the newline character at the end of UNDERLYING_FORTRAN_COMPILER
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_FORTRAN_COMPILER "${UNDERLYING_FORTRAN_COMPILER}")
+  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_FORTRAN_COMPILER
+         "${UNDERLYING_FORTRAN_COMPILER}")
 
   # Echo compilers
-  MESSAGE(STATUS "MPI C compiler: " ${MPI_C_COMPILER})
-  MESSAGE(STATUS "MPI C++ compiler: " ${MPI_CXX_COMPILER})
-  MESSAGE(STATUS "MPI Fortran compiler: " ${MPI_Fortran_COMPILER})
-  MESSAGE(STATUS "MPI underlying C compiler: " ${UNDERLYING_C_COMPILER})
-  MESSAGE(STATUS "MPI underlying C++ compiler: " ${UNDERLYING_CXX_COMPILER})
-  MESSAGE(STATUS "MPI underlying Fortran compiler: " ${UNDERLYING_FORTRAN_COMPILER})
+  if (MPI_C_COMPILER)
+    MESSAGE(STATUS "MPI C compiler: " ${MPI_C_COMPILER})
+  endif()
+
+  if (MPI_CXX_COMPILER)
+    MESSAGE(STATUS "MPI C++ compiler: " ${MPI_CXX_COMPILER})
+  endif()
+
+  if (MPI_Fortran_COMPILER)
+    MESSAGE(STATUS "MPI Fortran compiler: " ${MPI_Fortran_COMPILER})
+  endif()
+
+  if (UNDERLYING_C_COMPILER)
+    MESSAGE(STATUS "MPI underlying C compiler: " ${UNDERLYING_C_COMPILER})
+  endif()
+
+  if (UNDERLYING_CXX_COMPILER)
+    MESSAGE(STATUS "MPI underlying C++ compiler: " ${UNDERLYING_CXX_COMPILER})
+  endif()
+
+  if (UNDERLYING_FORTRAN_COMPILER)
+    MESSAGE(STATUS "MPI underlying Fortran compiler: "
+            ${UNDERLYING_FORTRAN_COMPILER})
+  endif()
 
 endfunction()
