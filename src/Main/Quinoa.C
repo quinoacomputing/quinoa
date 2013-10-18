a240f2c7c16e4c3f85b5c163978ad5c5d317f278
Jozsef Bakosi <jbakosi@lanl.gov>
2013-10-18 12:48:27 -0600

    Code reuse in main()

--- src/Main/Quinoa.C
+++ src/Main/Quinoa.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/Quinoa.C
   \author    J. Bakosi
-  \date      Fri Oct 18 11:36:56 2013
+  \date      Fri Oct 18 12:48:03 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa main
   \details   Quinoa main
@@ -11,12 +11,7 @@
 
 #include <Init.h>
 #include <Config.h>
-#include <Handler.h>
-#include <Print.h>
 #include <QuinoaDriver.h>
-
-using namespace quinoa;
-using namespace tk;
 
 int main(int argc, char* argv[])
 //******************************************************************************
@@ -24,38 +19,6 @@ int main(int argc, char* argv[])
 //! \author  J. Bakosi
 //******************************************************************************
 {
-  ErrCode error = ErrCode::SUCCESS;
-
-  try {
-
-    // Install our own new-handler
-    std::set_new_handler(newHandler);
-    // Install our own terminate-handler
-    std::set_terminate(terminateHandler);
-    // Install our own unexpected-handler
-    std::set_unexpected(unexpectedHandler);
-
-    // Create pretty printer
-    Print print;
-
-    // Echo program name
-    echoHeader(print, "Quinoa: Lagrangian particle hydrodynamics");
-
-    // Echo environment
-    print.part("Environment");
-    echoBuildEnv(print, QUINOA_EXECUTABLE);  //!< Build environment
-    echoRunEnv(print, argc, argv);           //!< Runtime environment
-
-    // Create driver
-    QuinoaDriver driver(argc, argv, print);
-
-    // Execute
-    driver.execute();
-
-  } catch (...) {
-      error = processException();
-    }
-
-  // Return error code
-  return static_cast<int>(error);
+  return tk::Main<quinoa::QuinoaDriver>( argc, argv,
+           "Quinoa: Lagrangian particle hydrodynamics", QUINOA_EXECUTABLE);
 }
