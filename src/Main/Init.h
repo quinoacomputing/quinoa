55f77803a60b4e42cd8d4ecfdef01632491c7932
Jozsef Bakosi <jbakosi@lanl.gov>
2013-10-18 12:48:27 -0600

    Code reuse in main()

--- src/Main/Init.h
+++ src/Main/Init.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/Init.h
   \author    J. Bakosi
-  \date      Fri Oct 18 11:20:18 2013
+  \date      Fri Oct 18 12:43:04 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Common initialization for mains
   \details   Common initialization for mains
@@ -13,24 +13,67 @@
 
 #include <string>
 
+#include <Handler.h>
 #include <Print.h>
 
 namespace tk {
 
-//!  Wrapper for POSIX API's getcwd() from unistd.h
+//! Wrapper for POSIX API's getcwd() from unistd.h
 std::string workdir();
 
-//!  Wrapper for the standard C library's gettimeofday() from
+//! Wrapper for the standard C library's gettimeofday() from
 std::string curtime();
 
-//!  Echo program title
+//! Echo program title
 void echoHeader(const tk::Print& print, const std::string& title);
 
-//!  Echo build environment
+//! Echo build environment
 void echoBuildEnv(const tk::Print& print, const std::string& executable);
 
-//!  Echo runtime environment
+//! Echo runtime environment
 void echoRunEnv(const tk::Print& print, int argc, char** argv);
+
+//! Main() templated on driver
+template< class Driver >
+int Main(int argc, char* argv[],
+         const std::string& name,
+         const std::string& executable)
+{
+  ErrCode error = ErrCode::SUCCESS;
+
+  try {
+
+    // Install our own new-handler
+    std::set_new_handler(newHandler);
+    // Install our own terminate-handler
+    std::set_terminate(terminateHandler);
+    // Install our own unexpected-handler
+    std::set_unexpected(unexpectedHandler);
+
+    // Create pretty printer
+    Print print;
+
+    // Echo program name
+    echoHeader(print, name);
+
+    // Echo environment
+    print.part("Environment");
+    echoBuildEnv(print, executable);  //!< Build environment
+    echoRunEnv(print, argc, argv);    //!< Runtime environment
+
+    // Create driver
+    Driver driver(argc, argv, print);
+
+    // Execute
+    driver.execute();
+
+  } catch (...) {
+      error = processException();
+    }
+
+  // Return error code
+  return static_cast<int>(error);
+}
 
 } // tk::
 
--- src/Main/RNGTest.C
+++ src/Main/RNGTest.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/RNGTest.C
   \author    J. Bakosi
-  \date      Fri Oct 18 11:38:13 2013
+  \date      Fri Oct 18 12:47:57 2013
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa random number generator test suite
   \details   Quinoa random number generator test suite
@@ -11,12 +11,7 @@
 
 #include <Init.h>
 #include <Config.h>
-#include <Handler.h>
-#include <Print.h>
 #include <RNGTestDriver.h>
-
-using namespace rngtest;
-using namespace tk;
 
 int main(int argc, char* argv[])
 //******************************************************************************
@@ -24,38 +19,6 @@ int main(int argc, char* argv[])
 //! \author  J. Bakosi
 //******************************************************************************
 {
-  ErrCode error = ErrCode::SUCCESS;
-
-  try {
-
-    // Install our own new-handler
-    std::set_new_handler(newHandler);
-    // Install our own terminate-handler
-    std::set_terminate(terminateHandler);
-    // Install our own unexpected-handler
-    std::set_unexpected(unexpectedHandler);
-
-    // Create pretty printer
-    Print print;
-
-    // Echo program name
-    echoHeader(print, "Quinoa: Random number generator test suite");
-
-    // Echo environment
-    print.part("Environment");
-    echoBuildEnv(print, RNGTEST_EXECUTABLE);  //!< Build environment
-    echoRunEnv(print, argc, argv);            //!< Runtime environment
-
-    // Create driver
-    RNGTestDriver driver(argc, argv, print);
-
-    // Execute
-    driver.execute();
-
-  } catch (...) {
-      error = processException();
-    }
-
-  // Return error code
-  return static_cast<int>(error);
+  return tk::Main<rngtest::RNGTestDriver>( argc, argv,
+           "Quinoa: Random number generator test suite", RNGTEST_EXECUTABLE);
 }
