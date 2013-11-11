415cc50bcac2b286a0b014e101669b02dc54ad7e
Jozsef Bakosi <jbakosi@lanl.gov>
2013-11-11 09:57:22 -0700

    Put in common io filename read into quinoa's grammar

--- src/Control/Quinoa/CmdLine/Grammar.h
+++ src/Control/Quinoa/CmdLine/Grammar.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Control/Quinoa/CmdLine/Grammar.h
   \author    J. Bakosi
-  \date      Mon 11 Nov 2013 09:32:03 AM MST
+  \date      Mon 11 Nov 2013 09:55:54 AM MST
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa's command line grammar definition
   \details   Grammar definition for parsing the command line. We use the Parsing
@@ -37,62 +37,21 @@ namespace cmd {
 
   // Quinoa's CmdLine grammar
 
-  //! control (i.e., input deck) file
-  struct control :
+  //! io parameter
+  template< typename keyword, typename io_tag >
+  struct io :
          tk::grm::process_cmd< Stack,
-                               kw::control,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::control > > {};
-
-  //! input file
-  struct input :
-         tk::grm::process_cmd< Stack,
-                               kw::input,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::input > > {};
-
-  //! output file
-  struct output :
-         tk::grm::process_cmd< Stack,
-                               kw::output,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::output > > {};
-
-  //! pdf output file
-  struct pdf :
-         tk::grm::process_cmd< Stack,
-                               kw::pdf,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::pdf > > {};
-
-  //! glob output file
-  struct glob :
-         tk::grm::process_cmd< Stack,
-                               kw::glob,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::glob > > {};
-
-  //! stat output file
-  struct stat :
-         tk::grm::process_cmd< Stack,
-                               kw::stat,
-                               tk::grm::Store< Stack,
-                                               ctr::io,
-                                               ctr::stat > > {};
+                               keyword,
+                               tk::grm::Store< Stack, ctr::io, io_tag > > {};
 
   //! command line keywords
   struct keywords :
-         pegtl::sor< control,
-                     input,
-                     output,
-                     pdf,
-                     glob,
-                     stat > {};
+         pegtl::sor< io< kw::control, ctr::control >,
+                     io< kw::input, ctr::input >,
+                     io< kw::output, ctr::output >,
+                     io< kw::pdf, ctr::pdf >,
+                     io< kw::glob, ctr::glob >,
+                     io< kw::stat, ctr::stat > > {};
 
   //! entry point: parse keywords and until end of string
   struct read_string :
