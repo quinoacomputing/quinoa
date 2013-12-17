ab119194d8067e7218fec1652e8e8e636bbfcea3
Jozsef Bakosi <jbakosi@lanl.gov>
2013-12-17 09:35:57 -0700

    Switch to -show from -showme:command to detect underlying compilers
    This works with intel mpi as well as openmpi

--- cmake/GetMPICompilers.cmake
+++ cmake/GetMPICompilers.cmake
@@ -11,33 +11,42 @@ function(get_mpi_compilers)
 
   # Find out underlying C compiler
   execute_process(
-    COMMAND           ${MPI_C_COMPILER} "-showme:command"
+    COMMAND           ${MPI_C_COMPILER} "-show"
     OUTPUT_VARIABLE   UNDERLYING_C_COMPILER
   )
-  # Remove the trailing newline
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_C_COMPILER
-         "${UNDERLYING_C_COMPILER}")
-  set(UNDERLYING_C_COMPILER "${UNDERLYING_C_COMPILER}" PARENT_SCOPE)
+  if (UNDERLYING_C_COMPILER)
+    string(REPLACE " " ";" C_COMP ${UNDERLYING_C_COMPILER})
+    LIST(GET C_COMP 0 UNDERLYING_C_COMPILER)
+    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_C_COMPILER
+           "${UNDERLYING_C_COMPILER}")
+    set(UNDERLYING_C_COMPILER "${UNDERLYING_C_COMPILER}" PARENT_SCOPE)
+  endif()
 
   # Find out underlying C++ compiler
   execute_process(
-    COMMAND           ${MPI_CXX_COMPILER} "-showme:command"
+    COMMAND           ${MPI_CXX_COMPILER} "-show"
     OUTPUT_VARIABLE   UNDERLYING_CXX_COMPILER
   )
-  # Remove the trailing newline
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_CXX_COMPILER
-         "${UNDERLYING_CXX_COMPILER}")
-  set(UNDERLYING_CXX_COMPILER "${UNDERLYING_CXX_COMPILER}" PARENT_SCOPE)
+  if (UNDERLYING_CXX_COMPILER)
+    string(REPLACE " " ";" CXX_COMP ${UNDERLYING_CXX_COMPILER})
+    LIST(GET CXX_COMP 0 UNDERLYING_CXX_COMPILER)
+    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_CXX_COMPILER
+           "${UNDERLYING_CXX_COMPILER}")
+    set(UNDERLYING_CXX_COMPILER "${UNDERLYING_CXX_COMPILER}" PARENT_SCOPE)
+  endif()
 
   # Find out underlying Fortran compiler
   execute_process(
-    COMMAND           ${MPI_Fortran_COMPILER} "-showme:command"
+    COMMAND           ${MPI_Fortran_COMPILER} "-show"
     OUTPUT_VARIABLE   UNDERLYING_FORTRAN_COMPILER
   )
-  # Remove the trailing newline
-  string(REGEX REPLACE "[\r\n]" "" UNDERLYING_FORTRAN_COMPILER
-         "${UNDERLYING_FORTRAN_COMPILER}")
-  set(UNDERLYING_FORTRAN_COMPILER "${UNDERLYING_FORTRAN_COMPILER}" PARENT_SCOPE)
+  if (UNDERLYING_FORTRAN_COMPILER)
+    string(REPLACE " " ";" F_COMP ${UNDERLYING_FORTRAN_COMPILER})
+    LIST(GET F_COMP 0 UNDERLYING_FORTRAN_COMPILER)
+    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_FORTRAN_COMPILER
+           "${UNDERLYING_FORTRAN_COMPILER}")
+    set(UNDERLYING_FORTRAN_COMPILER "${UNDERLYING_FORTRAN_COMPILER}" PARENT_SCOPE)
+  endif()
 
   # Echo compilers
   if (MPI_C_COMPILER)
