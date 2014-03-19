2f5dce8a4ec9f5b2ed531925957ad9d4d5e77044
Jozsef Bakosi <jbakosi@lanl.gov>
2014-03-19 13:23:01 -0600

    Simplify templatized Main()
    Who knew you could initialize a function pointer with an empty lambda,
    which is just what I needed here, as not every executable may have its own set
    of info function for TPLs.

--- src/Main/Gmsh2Exo.C
+++ src/Main/Gmsh2Exo.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Utilities/Gmsh2Exo.C
   \author    J. Bakosi
-  \date      Wed Mar 19 08:08:48 2014
+  \date      Wed Mar 19 13:20:48 2014
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Gmsh to Exodus II mesh file converter
   \details   Gmsh to Exodus II mesh file converter
@@ -10,9 +10,24 @@
 //******************************************************************************
 
 #include <Init.h>
-#include <InitGmsh2Exo.h>
 #include <Config.h>
 #include <Gmsh2ExoDriver.h>
+
+namespace gmsh2exo {
+
+void echoTPL(const tk::Print& /*print*/)
+//******************************************************************************
+//  Echo TPL version informaion for libs specific to Gmsh2Exo
+//! \author  J. Bakosi
+//******************************************************************************
+{
+//   print.raw("\n");
+//   echoHDF5(print, "HDF5 library");
+//   print.raw("\n");
+//   echoZlib(print, "Zlib compression library");
+}
+
+} // gmsh2exo::
 
 int main(int argc, char* argv[])
 //******************************************************************************
@@ -20,9 +35,10 @@ int main(int argc, char* argv[])
 //! \author  J. Bakosi
 //******************************************************************************
 {
-  return tk::Main< gmsh2exo::Gmsh2ExoDriver, gmsh2exo::echoTPL >
+  return tk::Main< gmsh2exo::Gmsh2ExoDriver >
                  ( argc,
                    argv,
                    "Quinoa: Gmsh to Exodus II mesh file converter",
-                   GMSH2EXO_EXECUTABLE );
+                   GMSH2EXO_EXECUTABLE,
+                   gmsh2exo::echoTPL );
 }
