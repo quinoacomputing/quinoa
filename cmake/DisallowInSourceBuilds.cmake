54d08f99f6aafbed91eaa7c2428521f3ebc60362
Jozsef Bakosi <jbakosi@lanl.gov>
2014-03-27 16:00:05 -0600

    Disallow in-source builds

---
+++ cmake/DisallowInSourceBuilds.cmake
@@ -0,0 +1,23 @@
+if(__disallow_in_source_builds)
+	return()
+endif()
+set(disallow_in_source_buildsrs YES)
+
+
+function(disallow_in_source_builds)
+
+  # Disallow in-source builds
+  if("${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_BINARY_DIR}")
+    message(FATAL_ERROR "ERROR! In-source builds are not allowed!\n"
+    "Please create a separate directory and configure there; for example:\n"
+    "  $ mkdir build\n"
+    "  $ cd build\n"
+    "  $ cmake [OPTIONS] ..."
+    "\n"
+    "NOTE: You must first delete the newly created CMakeCache.txt file and"
+    "CMakeFiles directory under the source directory or you will not be able to"
+    "configure correctly! For example:\n"
+    "  $ rm -r CMakeCache.txt CMakeFiles")
+  endif()
+
+endfunction()
