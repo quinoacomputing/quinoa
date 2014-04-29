56bf2daa11352daca2be068931892fe1ff938efd
Jozsef Bakosi <jbakosi@lanl.gov>
2014-04-29 11:25:50 -0600

    Build rngtest with Charm++ works - proof-of-concept
     * Does nothing useful at this point, just pasted the example from
       <charm>/examples/charm++/array.
    
     * Custom CMake dependencies appear to work. This is proof-of-concept only. Will
       put this in a generic CMake macro or function.
    
     * Turn off shared libs for now. It breaks with Charm.
    
     * Will need to fix the annoying problem causing the CMake cache not possible
       to regenerate only from scratch.

--- src/Main/RNGTest.C
+++ src/Main/RNGTest.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/RNGTest.C
   \author    J. Bakosi
-  \date      Wed Mar 19 13:16:52 2014
+  \date      Tue Apr 29 10:54:38 2014
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa random number generator test suite
   \details   Quinoa random number generator test suite
@@ -12,6 +12,7 @@
 #include <Init.h>
 #include <Config.h>
 #include <RNGTestDriver.h>
+#include <rngtest.decl.h>
 
 namespace rngtest {
 
@@ -25,16 +26,88 @@ void echoTPL(const tk::Print& /*print*/)
 
 } // rngtest::
 
-int main(int argc, char* argv[])
-//******************************************************************************
-//  Quinoa: Random number generator test suite
-//! \author  J. Bakosi
-//******************************************************************************
-{
-  return tk::Main< rngtest::RNGTestDriver >
-                 ( argc,
-                   argv,
-                   "Quinoa: Random number generator (RNG) test suite",
-                   RNGTEST_EXECUTABLE,
-                   rngtest::echoTPL );
-}
+/*readonly*/ CProxy_Main mainProxy;
+
+struct Main : CBase_Main {
+  int numRecv;
+
+  Main( CkArgMsg* msg ) : numRecv(0)
+  {
+    delete msg;
+
+    mainProxy = thisProxy;
+
+    const int X = 3;
+    const int Y = 3;
+    const int Z = 3;
+    CProxy_ArrayA a1 = CProxy_ArrayA::ckNew(X);
+    CProxy_ArrayB b1 = CProxy_ArrayB::ckNew(X, Y);
+    CProxy_ArrayC c1 = CProxy_ArrayC::ckNew(X, Y, Z);
+
+    a1.e();
+    b1.e();
+    c1.e();
+  }
+
+  void finished() {
+    if (++numRecv == 3) CkExit();
+  }
+};
+
+struct ArrayA : CBase_ArrayA {
+  ArrayA() {
+    CkPrintf("ArrayA: created element %d\n", thisIndex);
+  }
+  ArrayA(CkMigrateMessage*) { }
+  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
+};
+
+struct ArrayB : CBase_ArrayB {
+  ArrayB() {
+    CkPrintf("ArrayB: created element (%d,%d)\n", thisIndex.x, thisIndex.y);
+  }
+  ArrayB(CkMigrateMessage*) { }
+  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
+};
+
+struct ArrayC : CBase_ArrayC {
+  ArrayC() {
+    CkPrintf("ArrayB: created element (%d,%d,%d)\n", thisIndex.x, thisIndex.y, thisIndex.z);
+  }
+  ArrayC(CkMigrateMessage*) { }
+  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
+};
+
+struct ArrayD : CBase_ArrayD {
+  ArrayD() {}
+  ArrayD(CkMigrateMessage*) { }
+  void e() { }
+};
+
+struct ArrayE : CBase_ArrayE {
+  ArrayE() {}
+  ArrayE(CkMigrateMessage*) { }
+  void e() { }
+};
+
+struct ArrayF : CBase_ArrayF {
+  ArrayF() {}
+  ArrayF(CkMigrateMessage*) { }
+  void e() { }
+};
+
+#include <rngtest.def.h>
+
+// int main(int argc, char* argv[])
+// //******************************************************************************
+// //  Quinoa: Random number generator test suite
+// //! \author  J. Bakosi
+// //******************************************************************************
+// {
+//   return tk::Main< rngtest::RNGTestDriver >
+//                  ( argc,
+//                    argv,
+//                    "Quinoa: Random number generator (RNG) test suite",
+//                    RNGTEST_EXECUTABLE,
+//                    rngtest::echoTPL );
+// }
