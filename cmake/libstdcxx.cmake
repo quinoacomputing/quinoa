1f662d25643496c59794a1b8043075119f45320f
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-02 13:19:35 -0600

    Improve the Charm++ build process
    * Add cmake function to handle selecting the standard C++ library:
      libstdc++ or libc++ are the two options for now. Both TPL and Quinoa
      builds use this function.
    
    * Accordingly, the Charm++/AMPI TPL build links to the user-specified
      standard C++ library.
    
    * The default standard C++ library depends on the compiler id detected
      by cmake: for clang the default is to use libc++, everything else will
      default to libstdc++, which does not require any particular compiler
      flags.
    
    * Fix some warnings in RNGTest.C due to Charm (this will change), also
      turn off some warnings triggered by the Charm-generated code in
      .decl.h and def.h.

---
+++ cmake/libstdcxx.cmake
@@ -0,0 +1,35 @@
+if(__libstdcxx)
+	return()
+endif()
+set(__libstdcxx YES)
+
+
+# Set which C++ standard library to use; this should be the same as what is used
+# for quinoa
+
+function(setlibstdcxx LIBCXX_DEFAULT)
+
+  # Available options, for more info, see:
+  # libstdc++: http://gcc.gnu.org/libstdc++
+  # libc++: http://libcxx.llvm.org
+  set(LIBCXX_VALUES "libstdc++" "libc++")
+
+  # Initialize all to off
+  set(LIBCXX_STDCPP off)  # 0
+  set(LIBCXX_CPP off)     # 1
+  # Set default and select from list
+  set(LIBCXX ${LIBCXX_DEFAULT} CACHE STRING "Select standard C++ library. Available options: ${LIBCXX_VALUES}.")
+  SET_PROPERTY (CACHE LIBCXX PROPERTY STRINGS ${LIBCXX_VALUES})
+  STRING (TOLOWER ${LIBCXX} LIBCXX)
+  LIST (FIND LIBCXX_VALUES ${LIBCXX} LIBCXX_INDEX)
+  # Evaluate selected option and put in a define for it
+  IF (${LIBCXX_INDEX} EQUAL 0)
+    set(LIBCXX_STDCPP on PARENT_SCOPE)
+  ELSEIF (${LIBCXX_INDEX} EQUAL 1)
+    set(LIBCXX_CPP on PARENT_SCOPE)
+  ELSEIF (${LIBCXX_INDEX} EQUAL -1)
+    MESSAGE(FATAL_ERROR "Standard C++ library '${LIBCXX}' not supported, valid entries are ${LIBCXX_VALUES}.")
+  ENDIF()
+  message(STATUS "Standard C++ library (LIBCXX): " ${LIBCXX})
+
+endfunction()
