22bd2e4e5751e1c06c07b722b4737eba018099a7
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-03 17:43:05 -0600

    Add intel-specific ignores to charm modules
    Also replace -Wpedantic with -pedantic which works with older gcc as well as
    newer ones.

--- cmake/charm.cmake
+++ cmake/charm.cmake
@@ -19,11 +19,30 @@ function(addCharmModule SRCPATH MODULE HOSTFILE)
 
   add_custom_target(${MODULE}CharmModule
                     DEPENDS ${MODULE}.decl.h ${MODULE}.def.h)
+
   add_dependencies(${RNGTEST_EXECUTABLE} ${MODULE}CharmModule)
 
   # Ignore some warnings for the source file including the Charm-generated
   # .decl.h and .def.h
   set_source_files_properties(${HOSTFILE} PROPERTIES COMPILE_FLAGS "-Wno-shadow -Wno-unused-variable -Wno-unused-parameter -Wno-deprecated-register -Wno-mismatched-tags -Wno-cast-align")
+
+  if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")  # intel-specific ignores
+    set_source_files_properties(${HOSTFILE} PROPERTIES
+      # Suppress the following Intel compiler warnings/remarks:
+      #  177: variable was declared but never referenced
+      #  181: argument of type "unsigned char *" is incompatible with format
+      #       "%c", expecting argument of type "char *"
+      # 1572: floating-point equality and inequality comparisons are unreliable
+      # 1599: declaration hides variable
+      # 1720: function "<class>::operator new" has no corresponding member
+      #       operator delete (to be called if an exception is thrown during
+      #       initialization of an allocated object)
+      # 1944: declaration shadows a member of 'this'
+      # 2259: non-pointer conversion from "int" to "unsigned char" may lose
+      #       significant bits
+      # 3280: declaration hides member
+      COMPILE_FLAGS "-diag-disable 177,181,1572,1599,1720,1944,2259,3280")
+  endif()
 
   message(STATUS "Add Charm++ module ${MODULE}.ci in ${HOSTFILE} at ${CMAKE_SOURCE_DIR}/${SRCPATH}/")
 
