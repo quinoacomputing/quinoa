c195433abaa6e491e0f2f422f4c91119d10c3471
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-03 16:53:46 -0600

    Put in macro/function + Cray detect for MPI compilers cmake

--- cmake/MPICompilers.cmake
+++ cmake/MPICompilers.cmake
@@ -3,73 +3,78 @@ if(__get_mpi_compilers)
 endif()
 set(__get_mpi_compilers YES)
 
+macro(get_compiler CMAKE_COMPILER CMAKE_MPI_COMPILER UNDERLYING_COMPILER
+                   COMP_TYPE)
+
+  set(UNDERLYING_COMPILER "")
+
+  # Detect a Cray machine. This is based on testing for the following
+  # environment variables. At least one of these is always defined on a Cray,
+  # depending on what programming environment is loaded among the modules.
+  if (NOT DEFINED ENV{CRAY_PRGENVPGI} AND
+      NOT DEFINED ENV{CRAY_PRGENVGNU} AND
+      NOT DEFINED ENV{CRAY_PRGENVCRAY} AND
+      NOT DEFINED ENV{CRAY_PRGENVINTEL})
+
+    execute_process(
+      COMMAND           ${CMAKE_MPI_COMPILER} "-show"
+      OUTPUT_VARIABLE   TMP_COMPILER
+      ERROR_VARIABLE    ERR
+    )
+    if(ERR)
+      MESSAGE(FATAL_ERROR
+              "Executing command '${CMAKE_MPI_COMPILER} -show' failed.")
+    endif()
+
+    if (TMP_COMPILER)
+      string(REPLACE " " ";" COMP ${TMP_COMPILER})
+      LIST(GET COMP 0 TMP_COMPILER)
+      string(REGEX REPLACE "[\r\n]" "" TMP_COMPILER "${TMP_COMPILER}")
+      if(NOT IS_ABSOLUTE ${TMP_COMPILER})
+        execute_process(
+          COMMAND           which ${TMP_COMPILER}
+          OUTPUT_VARIABLE   TMP_COMPILER
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+      endif()
+      set(${UNDERLYING_COMPILER} "${TMP_COMPILER}")
+      set(${UNDERLYING_COMPILER} "${TMP_COMPILER}" PARENT_SCOPE)
+    endif()
+  else()
+    # For Cray machines, the underlying compiler is the same as the wrapper;
+    # export it to both local and parent scope.
+    message(STATUS "Cray detected, not querying underlying compiler for "
+                   ${CMAKE_MPI_COMPILER})
+    set(${UNDERLYING_COMPILER} "${CMAKE_MPI_COMPILER}")
+    set(${UNDERLYING_COMPILER} "${CMAKE_MPI_COMPILER}" PARENT_SCOPE)
+  endif()
+
+endmacro()
+
 
 function(get_mpi_compilers)
 
   # Find MPI
-  find_package(MPI)
+  find_package(MPI REQUIRED)
+  # Export to parent scope
   set(MPI_C_FOUND ${MPI_C_FOUND} PARENT_SCOPE)
   set(MPI_CXX_FOUND ${MPI_CXX_FOUND} PARENT_SCOPE)
   set(MPI_Fortran_FOUND ${MPI_Fortran_FOUND} PARENT_SCOPE)
 
-  # Find out underlying C compiler
-  execute_process(
-    COMMAND           ${MPI_C_COMPILER} "-show"
-    OUTPUT_VARIABLE   UNDERLYING_C_COMPILER
-  )
-  if (UNDERLYING_C_COMPILER)
-    string(REPLACE " " ";" C_COMP ${UNDERLYING_C_COMPILER})
-    LIST(GET C_COMP 0 UNDERLYING_C_COMPILER)
-    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_C_COMPILER
-           "${UNDERLYING_C_COMPILER}")
-    if(NOT IS_ABSOLUTE ${UNDERLYING_C_COMPILER})
-      execute_process(
-        COMMAND           which ${UNDERLYING_C_COMPILER}
-        OUTPUT_VARIABLE   UNDERLYING_C_COMPILER
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-    endif()
-    set(UNDERLYING_C_COMPILER "${UNDERLYING_C_COMPILER}" PARENT_SCOPE)
+  # Find underlying C, C++, and Fortran compilers
+  if (MPI_C_COMPILER)
+    get_compiler(${CMAKE_C_COMPILER} ${MPI_C_COMPILER}
+                 UNDERLYING_C_COMPILER "C")
   endif()
 
-  # Find out underlying C++ compiler
-  execute_process(
-    COMMAND           ${MPI_CXX_COMPILER} "-show"
-    OUTPUT_VARIABLE   UNDERLYING_CXX_COMPILER
-  )
-  if (UNDERLYING_CXX_COMPILER)
-    string(REPLACE " " ";" CXX_COMP ${UNDERLYING_CXX_COMPILER})
-    LIST(GET CXX_COMP 0 UNDERLYING_CXX_COMPILER)
-    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_CXX_COMPILER
-           "${UNDERLYING_CXX_COMPILER}")
-    if(NOT IS_ABSOLUTE ${UNDERLYING_CXX_COMPILER})
-      execute_process(
-        COMMAND           which ${UNDERLYING_CXX_COMPILER}
-        OUTPUT_VARIABLE   UNDERLYING_CXX_COMPILER
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-    endif()
-    set(UNDERLYING_CXX_COMPILER "${UNDERLYING_CXX_COMPILER}" PARENT_SCOPE)
+  if (MPI_CXX_COMPILER)
+    get_compiler(${CMAKE_CXX_COMPILER} ${MPI_CXX_COMPILER}
+                 UNDERLYING_CXX_COMPILER "C++")
   endif()
 
-  # Find out underlying Fortran compiler
-  execute_process(
-    COMMAND           ${MPI_Fortran_COMPILER} "-show"
-    OUTPUT_VARIABLE   UNDERLYING_FORTRAN_COMPILER
-  )
-  if (UNDERLYING_FORTRAN_COMPILER)
-    string(REPLACE " " ";" F_COMP ${UNDERLYING_FORTRAN_COMPILER})
-    LIST(GET F_COMP 0 UNDERLYING_FORTRAN_COMPILER)
-    string(REGEX REPLACE "[\r\n]" "" UNDERLYING_FORTRAN_COMPILER
-           "${UNDERLYING_FORTRAN_COMPILER}")
-    if(NOT IS_ABSOLUTE ${UNDERLYING_FORTRAN_COMPILER})
-      execute_process(
-        COMMAND           which ${UNDERLYING_FORTRAN_COMPILER}
-        OUTPUT_VARIABLE   UNDERLYING_FORTRAN_COMPILER
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-    endif()
-    set(UNDERLYING_FORTRAN_COMPILER "${UNDERLYING_FORTRAN_COMPILER}" PARENT_SCOPE)
+  if (MPI_Fortran_COMPILER)
+    get_compiler(${CMAKE_Fortran_COMPILER} ${MPI_Fortran_COMPILER}
+                 UNDERLYING_Fortran_COMPILER "Fortran")
   endif()
 
   # Echo MPI wrappers
@@ -103,7 +108,7 @@ function(get_mpi_compilers)
     endif()
   endif()
 
-  # Echo underlying compilers
+  # Echo underling compilers
   if (UNDERLYING_C_COMPILER)
     MESSAGE(STATUS "Underlying C compiler: " ${UNDERLYING_C_COMPILER})
   endif()
@@ -112,9 +117,8 @@ function(get_mpi_compilers)
     MESSAGE(STATUS "Underlying C++ compiler: " ${UNDERLYING_CXX_COMPILER})
   endif()
 
-  if (UNDERLYING_FORTRAN_COMPILER)
-    MESSAGE(STATUS "Underlying Fortran compiler: "
-            ${UNDERLYING_FORTRAN_COMPILER})
+  if (UNDERLYING_Fortran_COMPILER)
+    MESSAGE(STATUS "Underlying Fortran compiler: " ${UNDERLYING_Fortran_COMPILER})
   endif()
 
 endfunction()
