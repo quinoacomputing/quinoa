43f998c62a969a71fdda4bd4a37981f8756c9fc7
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-05 17:48:25 -0600

    Custom Charm++-build only for HOSTFILE works
    Switching strategy with Charm++: now only building Charm++ host files with the
    charmc wrapper, which requires a custom cmake compile command. Everything else
    (non-Charm++) objects are built by CMAKE_CXX_COMPILER.

--- cmake/charm.cmake
+++ cmake/charm.cmake
@@ -4,30 +4,34 @@ endif()
 set(__addCharmModule YES)
 
 
-function(addCharmModule SRCPATH MODULE HOSTFILE)
+function(addCharmModule SRCPATH MODULE HOSTFILE PARTOF)
 # Arguments:
-#   SRCPATH:  path where the .ci Charm++ module file resides, relative to src/
+#   SRCPATH:  path where the .ci Charm++ module file resides, relative to src,
+#   i.e., CMAKE_SOURCE_DIR
 #   MODULE:   name of the Charm++ module
-#   HOSTFILE: name of the .C file that includes the Charm++-generated .decl.h
-#             and .def.h
+#   HOSTFILE: name of the .C file (without the extension) that includes the
+#             Charm++-generated .decl.h and .def.h
+#   PARTOF:   name of the library or executable the Charm++ module will link to
 
   # Add custom command generating .decl.h and .def.h from .ci
   add_custom_command(OUTPUT ${MODULE}.decl.h ${MODULE}.def.h
                      DEPENDS ${CMAKE_SOURCE_DIR}/${SRCPATH}/${MODULE}.ci
                      COMMAND ${CHARM_COMPILER}
                              ${CMAKE_SOURCE_DIR}/${SRCPATH}/${MODULE}.ci)
-
   add_custom_target(${MODULE}CharmModule
                     DEPENDS ${MODULE}.decl.h ${MODULE}.def.h)
+  add_dependencies(${PARTOF} ${MODULE}CharmModule)
 
-  add_dependencies(${RNGTEST_EXECUTABLE} ${MODULE}CharmModule)
+  # Add (current) binary directory, i.e. which this function was called from, to
+  # list of include directories as .decl.h and .def.h will be generated there
+  include_directories(${PROJECT_BINARY_DIR})
 
-  # Ignore some warnings for the source file including the Charm-generated
-  # .decl.h and .def.h
-  set_source_files_properties(${HOSTFILE} PROPERTIES COMPILE_FLAGS "-Wno-shadow -Wno-unused-variable -Wno-unused-parameter -Wno-deprecated-register -Wno-mismatched-tags -Wno-cast-align")
+  # Ignore some warnings for HOSTFILE including the Charm-generated .decl.h and
+  # .def.h
+  set_source_files_properties(${HOSTFILE}.C PROPERTIES COMPILE_FLAGS "-Wno-shadow -Wno-unused-variable -Wno-unused-parameter -Wno-deprecated-register -Wno-mismatched-tags -Wno-cast-align")
 
   if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")  # intel-specific ignores
-    set_source_files_properties(${HOSTFILE} PROPERTIES
+    set_source_files_properties(${HOSTFILE}.C PROPERTIES
       # Suppress the following Intel compiler warnings/remarks:
       #  177: variable was declared but never referenced
       #  181: argument of type "unsigned char *" is incompatible with format
@@ -44,6 +48,30 @@ function(addCharmModule SRCPATH MODULE HOSTFILE)
       COMPILE_FLAGS "-diag-disable 177,181,1572,1599,1720,1944,2259,3280")
   endif()
 
-  message(STATUS "Add Charm++ module ${MODULE}.ci in ${HOSTFILE} at ${CMAKE_SOURCE_DIR}/${SRCPATH}/")
+  # Build list of compiler arguments with each include directory, -I..., -I...
+  get_property(include_dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+               PROPERTY INCLUDE_DIRECTORIES)
+  set(INCDIR_FLAGS "")
+  foreach(dir ${include_dirs})
+    message(STATUS "dir='${dir}'")
+    LIST(APPEND INCDIR_FLAGS "-I${dir}")
+  endforeach()
+
+  # Add custom command compiling the Charm++ host file using the charmc wrapper
+  add_custom_command(OUTPUT ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.o
+                     DEPENDS ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.C
+                     COMMAND ${CHARM_COMPILER}
+                             ${CMAKE_CXX_FLAGS}
+                             ${CXXFLAGS}
+                             ${CMAKE_SHARED_MODULE_CXX_FLAGS}
+                             ${INCDIR_FLAGS}
+                             -o ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.o
+                             ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.C)
+  add_custom_target([CharmHost]${HOSTFILE}.o
+                    DEPENDS ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.o
+                            ${MODULE}CharmModule)
+  add_dependencies(${PARTOF} [CharmHost]${HOSTFILE}.o)
+
+  message(STATUS "Add Charm++ module ${MODULE}.ci in ${HOSTFILE}.C at ${CMAKE_SOURCE_DIR}/${SRCPATH}/ linking to ${PARTOF}")
 
 endfunction()
---
+++ cmake/get_compiler_flags.cmake
@@ -0,0 +1,52 @@
+macro(get_compiler_flags)
+
+  # Convert CMAKE_DEBUG_TYPE to upper case so even if the user gives it via the
+  # command line, the tests below work as intended
+  string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
+
+  # Forward cmake compiler flags to automake builds by setting our own variables
+  # CFLAGS, CXXFLAGS, and FFLAGS. Only these flags should then be used by the
+  # individual external projects builds below.
+  if(CMAKE_BUILD_TYPE STREQUAL "")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "DEBUG")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_DEBUG}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_DEBUG}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "RELEASE")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_RELEASE}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_RELEASE}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "RELWITHDEBINFO")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_RELWITHDEBINFO}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_RELWITHDEBINFO}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "MINSIZEREL")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_MINSIZEREL}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_MINSIZEREL}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_MINSIZEREL}")
+  endif()
+
+  # Remove first empty character of compiler flag strings
+  if (CFLAGS)
+    STRING(REGEX REPLACE "^ " "" CFLAGS ${CFLAGS})
+  endif()
+  if (CXXFLAGS)
+    STRING(REGEX REPLACE "^ " "" CXXFLAGS ${CXXFLAGS})
+  endif()
+  if (FFLAGS)
+    STRING(REGEX REPLACE "^ " "" FFLAGS ${FFLAGS})
+  endif()
+
+  # Echo compiler flags string used
+  message(STATUS "C compiler flags: '${CFLAGS}'")
+  message(STATUS "C++ compiler flags: '${CXXFLAGS}'")
+  message(STATUS "Fortran compiler flags: '${FFLAGS}'")
+
+endmacro()
