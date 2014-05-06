768ab98cf0a05d59f5b95e3eb32bae4744f6b82e
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-05 18:09:54 -0600

    Properly add extra Charm++ host file compiler flags

--- cmake/charm.cmake
+++ cmake/charm.cmake
@@ -5,13 +5,13 @@ set(__addCharmModule YES)
 
 
 function(addCharmModule SRCPATH MODULE HOSTFILE PARTOF)
-# Arguments:
-#   SRCPATH:  path where the .ci Charm++ module file resides, relative to src,
-#   i.e., CMAKE_SOURCE_DIR
-#   MODULE:   name of the Charm++ module
-#   HOSTFILE: name of the .C file (without the extension) that includes the
-#             Charm++-generated .decl.h and .def.h
-#   PARTOF:   name of the library or executable the Charm++ module will link to
+  # Arguments:
+  #   SRCPATH:  path where the .ci Charm++ module file resides, relative to src,
+  #   i.e., CMAKE_SOURCE_DIR
+  #   MODULE:   name of the Charm++ module
+  #   HOSTFILE: name of the .C file (without the extension) that includes the
+  #             Charm++-generated .decl.h and .def.h
+  #   PARTOF:   name of the library or executable the Charm++ module will link to
 
   # Add custom command generating .decl.h and .def.h from .ci
   add_custom_command(OUTPUT ${MODULE}.decl.h ${MODULE}.def.h
@@ -22,30 +22,29 @@ function(addCharmModule SRCPATH MODULE HOSTFILE PARTOF)
                     DEPENDS ${MODULE}.decl.h ${MODULE}.def.h)
   add_dependencies(${PARTOF} ${MODULE}CharmModule)
 
-  # Add (current) binary directory, i.e. which this function was called from, to
-  # list of include directories as .decl.h and .def.h will be generated there
+  # Add (current) binary directory, i.e., which this function was called from,
+  # to list of include directories as .decl.h and .def.h will be generated there
   include_directories(${PROJECT_BINARY_DIR})
 
-  # Ignore some warnings for HOSTFILE including the Charm-generated .decl.h and
-  # .def.h
-  set_source_files_properties(${HOSTFILE}.C PROPERTIES COMPILE_FLAGS "-Wno-shadow -Wno-unused-variable -Wno-unused-parameter -Wno-deprecated-register -Wno-mismatched-tags -Wno-cast-align")
-
+  # Ignore some warnings for HOSTFILE that includes the Charm-generated .decl.h
+  # and .def.h
   if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")  # intel-specific ignores
-    set_source_files_properties(${HOSTFILE}.C PROPERTIES
-      # Suppress the following Intel compiler warnings/remarks:
-      #  177: variable was declared but never referenced
-      #  181: argument of type "unsigned char *" is incompatible with format
-      #       "%c", expecting argument of type "char *"
-      # 1572: floating-point equality and inequality comparisons are unreliable
-      # 1599: declaration hides variable
-      # 1720: function "<class>::operator new" has no corresponding member
-      #       operator delete (to be called if an exception is thrown during
-      #       initialization of an allocated object)
-      # 1944: declaration shadows a member of 'this'
-      # 2259: non-pointer conversion from "int" to "unsigned char" may lose
-      #       significant bits
-      # 3280: declaration hides member
-      COMPILE_FLAGS "-diag-disable 177,181,1572,1599,1720,1944,2259,3280")
+    # Suppress the following Intel compiler warnings/remarks:
+    #  177: variable was declared but never referenced
+    #  181: argument of type "unsigned char *" is incompatible with format
+    #       "%c", expecting argument of type "char *"
+    # 1572: floating-point equality and inequality comparisons are unreliable
+    # 1599: declaration hides variable
+    # 1720: function "<class>::operator new" has no corresponding member
+    #       operator delete (to be called if an exception is thrown during
+    #       initialization of an allocated object)
+    # 1944: declaration shadows a member of 'this'
+    # 2259: non-pointer conversion from "int" to "unsigned char" may lose
+    #       significant bits
+    # 3280: declaration hides member
+    set(HOSTFILE_FLAGS "-diag-disable 177,181,1572,1599,1720,1944,2259,3280")
+  else()
+    set(HOSTFILE_FLAGS "-Wno-shadow -Wno-unused-variable -Wno-unused-parameter -Wno-deprecated-register -Wno-mismatched-tags -Wno-cast-align")
   endif()
 
   # Build list of compiler arguments with each include directory, -I..., -I...
@@ -53,7 +52,6 @@ function(addCharmModule SRCPATH MODULE HOSTFILE PARTOF)
                PROPERTY INCLUDE_DIRECTORIES)
   set(INCDIR_FLAGS "")
   foreach(dir ${include_dirs})
-    message(STATUS "dir='${dir}'")
     LIST(APPEND INCDIR_FLAGS "-I${dir}")
   endforeach()
 
@@ -63,15 +61,18 @@ function(addCharmModule SRCPATH MODULE HOSTFILE PARTOF)
                      COMMAND ${CHARM_COMPILER}
                              ${CMAKE_CXX_FLAGS}
                              ${CXXFLAGS}
+                             ${HOSTFILE_FLAGS}
                              ${CMAKE_SHARED_MODULE_CXX_FLAGS}
                              ${INCDIR_FLAGS}
                              -o ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.o
-                             ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.C)
+                             -c ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.C)
   add_custom_target([CharmHost]${HOSTFILE}.o
                     DEPENDS ${CMAKE_SOURCE_DIR}/${SRCPATH}/${HOSTFILE}.o
                             ${MODULE}CharmModule)
   add_dependencies(${PARTOF} [CharmHost]${HOSTFILE}.o)
 
+  # Echo status on adding Charm++ module
   message(STATUS "Add Charm++ module ${MODULE}.ci in ${HOSTFILE}.C at ${CMAKE_SOURCE_DIR}/${SRCPATH}/ linking to ${PARTOF}")
+  message(STATUS "  Extra ${HOSTFILE}.C compiler flags: ${HOSTFILE_FLAGS}")
 
 endfunction()
