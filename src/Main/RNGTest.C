ccad3f82c7d01f2051de778ac4e5221e599dde3c
Jozsef Bakosi <jbakosi@lanl.gov>
2014-05-07 17:22:54 -0600

    Hookup correct main to rngtest
    Also setup preliminary Charm++ module 'testu01suite' containing a chare array
    'TestChareContainer'.

--- src/Main/RNGTest.C
+++ src/Main/RNGTest.C
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/RNGTest.C
   \author    J. Bakosi
-  \date      Thu 01 May 2014 10:02:29 PM MDT
+  \date      Wed 07 May 2014 02:44:27 PM MDT
   \copyright Copyright 2005-2012, Jozsef Bakosi, All rights reserved.
   \brief     Quinoa random number generator test suite
   \details   Quinoa random number generator test suite
@@ -29,92 +29,26 @@ void echoTPL(const tk::Print& /*print*/)
 /*readonly*/ CProxy_Main mainProxy;
 
 class Main : public CBase_Main {
-  int numRecv;
 
   public:
-  Main( CkArgMsg* msg ) : numRecv(0)
-  {
-    delete msg;
 
-    mainProxy = thisProxy;
+    // Cosntructor
+    Main( CkArgMsg* msg ) {
+      tk::Main< rngtest::RNGTestDriver >
+              ( msg->argc,
+                msg->argv,
+                "Quinoa: Random number generator (RNG) test suite",
+                RNGTEST_EXECUTABLE,
+                rngtest::echoTPL );
+      delete msg;
 
-    const int X = 3;
-    const int Y = 3;
-    const int Z = 3;
-    CProxy_ArrayA a1 = CProxy_ArrayA::ckNew(X);
-    CProxy_ArrayB b1 = CProxy_ArrayB::ckNew(X, Y);
-    CProxy_ArrayC c1 = CProxy_ArrayC::ckNew(X, Y, Z);
+      mainProxy = thisProxy;
 
-    a1.e();
-    b1.e();
-    c1.e();
-  }
+      CkExit();
+    }
 
-  void finished() {
-    if (++numRecv == 3) CkExit();
-  }
-};
-
-class ArrayA : public CBase_ArrayA {
-  public:
-  ArrayA() {
-    CkPrintf("ArrayA: created element %d\n", thisIndex);
-  }
-  ArrayA(CkMigrateMessage*) { }
-  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
-};
-
-class ArrayB : public CBase_ArrayB {
-  public:
-  ArrayB() {
-    CkPrintf("ArrayB: created element (%d,%d)\n", thisIndex.x, thisIndex.y);
-  }
-  ArrayB(CkMigrateMessage*) { }
-  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
-};
-
-class ArrayC : public CBase_ArrayC {
-  public:
-  ArrayC() {
-    CkPrintf("ArrayB: created element (%d,%d,%d)\n", thisIndex.x, thisIndex.y, thisIndex.z);
-  }
-  ArrayC(CkMigrateMessage*) { }
-  void e() { contribute(CkCallback(CkReductionTarget(Main, finished), mainProxy)); }
-};
-
-class ArrayD : public CBase_ArrayD {
-  public:
-  ArrayD() {}
-  ArrayD(CkMigrateMessage*) { }
-  void e() { }
-};
-
-class ArrayE : public CBase_ArrayE {
-  public:
-  ArrayE() {}
-  ArrayE(CkMigrateMessage*) { }
-  void e() { }
-};
-
-class ArrayF : public CBase_ArrayF {
-  public:
-  ArrayF() {}
-  ArrayF(CkMigrateMessage*) { }
-  void e() { }
+    // Destructor
+    //~Main() { CkExit(); }
 };
 
 #include <rngtest.def.h>
-
-// int main(int argc, char* argv[])
-// //******************************************************************************
-// //  Quinoa: Random number generator test suite
-// //! \author  J. Bakosi
-// //******************************************************************************
-// {
-//   return tk::Main< rngtest::RNGTestDriver >
-//                  ( argc,
-//                    argv,
-//                    "Quinoa: Random number generator (RNG) test suite",
-//                    RNGTEST_EXECUTABLE,
-//                    rngtest::echoTPL );
-// }
