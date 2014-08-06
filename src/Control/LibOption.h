14d690ed1f92104bf64545def61ff074d01a009d
Jozsef Bakosi <jbakosi@lanl.gov>
2014-08-06 10:04:52 -0600

    Simplify Control/LibOption
    Will refactor with quinoa, will write unit tests then

--- src/Control/LibOption.h
+++ src/Control/LibOption.h
@@ -2,43 +2,29 @@
 /*!
   \file      src/Control/LibOption.h
   \author    J. Bakosi
-  \date      Wed 06 Aug 2014 09:34:21 AM MDT
+  \date      Wed 06 Aug 2014 10:04:45 AM MDT
   \copyright 2005-2014, Jozsef Bakosi.
-  \brief     Library option
-  \details   Library option
+  \brief     Library toggle
+  \details   Library toggle
 */
 //******************************************************************************
 #ifndef LibOption_h
 #define LibOption_h
 
-#include <Option.h>
-
 namespace tk {
 
-//! Generic library option interface templated on option 'Type'
-template< class Type >
-class LibOption : public Option< Type > {
+//! Library toggle
+template< class Option >
+class LibOption {
 
   public:
-    //! Destructor
-    ~LibOption() noexcept override = default;
-
     //! Lookup library option parameter
-    const typename Type::ParamType& param( typename Type::EnumType value ) const
-    { return Option< Type >::m_option.param( value ); }
+    const typename Option::ParamType& param( typename Option::EnumType value )
+    const { return Option().param( value ); }
 
     //! Lookup library option library
-    typename Type::LibType lib( typename Type::EnumType value ) const {
-      return Option< Type >::m_option.lib( value );
-    }
-
-  private:
-    //! Permit compiler to generate copy constructor
-    //! Permit compiler to generate move constructor
-    //! Don't permit copy assigment
-    LibOption& operator=(const LibOption&) = delete;
-    //! Don't permit move assigment
-    LibOption& operator=(LibOption&&) = delete;
+    typename Option::LibType lib( typename Option::EnumType value ) const
+    { return Option().lib( value ); }
 };
 
 } // tk::
