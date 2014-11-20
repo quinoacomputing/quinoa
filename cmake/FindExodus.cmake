a46d876b90ddd6007037489ef0a6f89eeee1469c
Jozsef Bakosi <jbakosi@lanl.gov>
2014-11-20 12:26:03 -0700

    TPL build now search for system-wide ExodusII/Nemesis
    If found, uses it, if not, build ours (via Trilinos)

---
+++ cmake/FindExodus.cmake
@@ -0,0 +1,37 @@
+# Find the Exodus II and Nemesis finite element data model library from Sandia
+#
+#  EXODUS_FOUND - System has both ExodusII and Nemesis
+#  EXODUS_INCLUDES - The ExodusII include directory
+#  EXODUS_LIBRARIES - The libraries needed to use ExodusII
+#  NEMESIS_INCLUDES - The Nemesis include directory
+#  NEMESIS_LIBRARIES - The libraries needed to use Nemesis
+#
+#  Set EXODUS_ROOT before calling find_package to a path to add an additional
+#  search path, e.g.,
+#
+#  Usage:
+#
+#  set(EXODUS_ROOT "/path/to/custom/exodus_and_nemesis") # prefer over system
+#  find_package(Exodus)
+#  if(EXODUS_FOUND)
+#    target_link_libraries (TARGET ${EXODUS_LIBRARIES})
+#    target_link_libraries (TARGET ${NEMESIS_LIBRARIES})
+#  endif()
+
+if(EXODUS_INCLUDES AND EXODUS_LIBRARIES AND NEMESIS_INCLUDES AND NEMESISLIBRARIES)
+  # Already in cache, be silent
+  set (EXODUS_FIND_QUIETLY TRUE)
+endif()
+
+FIND_PATH(EXODUS_INCLUDES NAMES exodusII.h HINTS ${EXODUS_ROOT}/include)
+FIND_LIBRARY(EXODUS_LIBRARIES NAMES exodus exoIIv2 HINTS ${EXODUS_ROOT}/lib)
+
+FIND_PATH(NEMESIS_INCLUDES NAMES ne_nemesisI.h HINTS ${EXODUS_ROOT}/include)
+FIND_LIBRARY(NEMESIS_LIBRARIES NAMES nemesis HINTS ${EXODUS_ROOT}/lib)
+
+# handle the QUIETLY and REQUIRED arguments and set EXODUS_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Exodus DEFAULT_MSG EXODUS_LIBRARIES EXODUS_INCLUDES NEMESIS_LIBRARIES NEMESIS_INCLUDES)
+
+MARK_AS_ADVANCED(EXODUS_INCLUDES EXODUS_LIBRARIES NEMESIS_INCLUDES NEMESIS_LIBRARIES)
