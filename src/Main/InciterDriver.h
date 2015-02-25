524a15e03765fcf57f6ef502e9b0f962319fbea5
Jozsef Bakosi <jbakosi@lanl.gov>
2015-02-25 07:47:36 -0700

    Charm++/MPI interop for Inciter
    This is work-in-progress towards interoperation of Charm++ and the MPI library
    Zoltan, in order to do an initial static load-balance of the mesh using Zoltan.
    
    For now, we successfully nitialize MPI followed by CharmLibInit() initializing
    the Charm++ runtime system. This required removing of AMPI from the Charm++ TPL
    build, since the build picked up Charm++'s AMPI's mpi.h instead of OpenMPI's,
    which confused the build.
    
    At this point also successfully initialize/destroy Zoltan and create a Zoltan
    struct (Zoltan_Initialize, Zoltan_Destroy, and Zoltan_Create functions),
    however, exucion hangs with more than a single PEs. Investigating with Charm++
    developers...

--- src/Main/InciterDriver.h
+++ src/Main/InciterDriver.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Main/InciterDriver.h
   \author    J. Bakosi
-  \date      Tue 24 Feb 2015 10:22:08 AM MST
+  \date      Tue 24 Feb 2015 12:16:36 PM MST
   \copyright 2012-2015, Jozsef Bakosi.
   \brief     Inciter driver
   \details   Inciter driver.
@@ -34,6 +34,9 @@ class InciterDriver {
                uint64_t chunksize,
                uint64_t remainder,
                uint64_t nchare ) const;
+
+    //! Partition mesh using Zoltan
+    void partition( const tk::UnsMesh& mesh ) const;
 
     const InciterPrint& m_print;        //!< Pretty printer
     std::string m_input;                //!< Input file name
