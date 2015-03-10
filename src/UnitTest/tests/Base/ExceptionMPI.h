d4b0138ff0cd45c95ef8770db131400f77e71ddd
Jozsef Bakosi <jbakosi@lanl.gov>
2015-03-09 21:13:57 -0600

    Add unit tests for ErrChkMPI when not all ranks pass the same condition

--- src/UnitTest/tests/Base/ExceptionMPI.h
+++ src/UnitTest/tests/Base/ExceptionMPI.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/UnitTest/tests/Base/ExceptionMPI.h
   \author    J. Bakosi
-  \date      Sun 08 Mar 2015 01:20:08 PM MDT
+  \date      Mon 09 Mar 2015 09:13:38 PM MDT
   \copyright 2012-2015, Jozsef Bakosi.
   \brief     Unit tests for Base/ExceptionMPI.h
   \details   Unit tests for Base/ExceptionMPI.h
@@ -28,10 +28,10 @@ ExceptionMPI_group ExceptionMPI( "Base/ExceptionMPI" );
 
 //! Test definitions for group
 
-//! Test that AssertMPI macro throws if condition is false
+//! Test that AssertMPI macro throws if condition is false on all ranks
 template<> template<>
 void ExceptionMPI_object::test< 1 >() {
-  set_test_name( "AssertMPI macro throws when false" );
+  set_test_name( "AssertMPI macro throws all false" );
 
   try {
     AssertMPI( 0 == 1, "msg" );
@@ -45,10 +45,10 @@ void ExceptionMPI_object::test< 1 >() {
   }
 }
 
-//! Test that AssertMPI macro does not throw if condition is true
+//! Test that AssertMPI macro does not throw if condition is true on all ranks
 template<> template<>
 void ExceptionMPI_object::test< 2 >() {
-  set_test_name( "AssertMPI macro doesn't throw when true" );
+  set_test_name( "AssertMPI macro doesn't throw all true" );
 
   try {
     AssertMPI( 1 == 1, "msg" );
@@ -58,10 +58,10 @@ void ExceptionMPI_object::test< 2 >() {
   }
 }
 
-//! Test that ErrChkMPI macro throws if condition is false
+//! Test that ErrChkMPI macro throws if condition is false on all ranks
 template<> template<>
 void ExceptionMPI_object::test< 3 >() {
-  set_test_name( "ErrChkMPI macro throws when false" );
+  set_test_name( "ErrChkMPI macro throws all false" );
 
   try {
     ErrChkMPI( 0 == 1, "msg" );
@@ -72,10 +72,10 @@ void ExceptionMPI_object::test< 3 >() {
   }
 }
 
-//! Test that ErrChkMPI macro does not throw if condition is true
+//! Test that ErrChkMPI macro does not throw if condition is true on all ranks
 template<> template<>
 void ExceptionMPI_object::test< 4 >() {
-  set_test_name( "ErrChkMPI macro doesn't throw when true" );
+  set_test_name( "ErrChkMPI macro doesn't throw all true" );
 
   try {
     ErrChkMPI( 0 != 1, "msg" );
@@ -83,6 +83,43 @@ void ExceptionMPI_object::test< 4 >() {
   catch ( tk::Exception& e ) {
     fail( "should not throw excecption" );
   }
+}
+
+//! Test that ErrChkMPI macro throws if condition is false on the 0th rank
+template<> template<>
+void ExceptionMPI_object::test< 5 >() {
+  set_test_name( "ErrChkMPI macro throws 0th false" );
+
+  try {
+    int peid;
+    MPI_Comm_rank( MPI_COMM_WORLD, &peid );
+    ErrChkMPI( peid == 0 ? 0 == 1 : 1 == 1, "msg" );
+    fail( "should throw exception" );
+  }
+  catch ( tk::Exception& e ) {
+    // exception thrown, test ok
+  }
+}
+
+//! Test that ErrChkMPI macro throws if condition is true on only the 0th rank
+template<> template<>
+void ExceptionMPI_object::test< 6 >() {
+  set_test_name( "ErrChkMPI macro throws 0th true only" );
+
+  int numpes;
+  MPI_Comm_size( MPI_COMM_WORLD, &numpes );
+  if ( numpes > 1 )
+    try {
+      int peid;
+      MPI_Comm_rank( MPI_COMM_WORLD, &peid );
+      ErrChkMPI( peid == 0 ? 1 == 1 : 0 == 1, "msg" );
+      fail( "should throw exception" );
+    }
+    catch ( tk::Exception& e ) {
+      // exception thrown, test ok
+    }
+  else
+    skip( "in serial, needs multiple PEs" );
 }
 
 } // tut::
