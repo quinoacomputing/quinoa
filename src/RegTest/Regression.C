c89b977039547dcefea84c70d88432f67de1c83b
Jozsef Bakosi <jbakosi@lanl.gov>
2015-06-01 10:12:21 -0600

    Fix includes in RegTest/Regression.[Ch]

--- src/RegTest/Regression.C
+++ src/RegTest/Regression.C
@@ -2,13 +2,13 @@
 /*!
   \file      src/RegTest/Regression.C
   \author    J. Bakosi
-  \date      Sun 22 Mar 2015 11:34:05 AM MDT
+  \date      Mon 01 Jun 2015 10:10:37 AM MDT
   \copyright 2012-2015, Jozsef Bakosi.
   \brief     Regression test class definition
   \details   Regression test class definition.
 */
 //******************************************************************************
 
-#include <Regression.h>
+#include "Regression.h"
 
-#include <regression.def.h>
+#include "regression.def.h"
