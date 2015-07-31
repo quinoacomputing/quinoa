aa0ecf5125e50dd3e43329305f4689a2787100e6
Jozsef Bakosi <jbakosi@lanl.gov>
2015-07-31 13:59:52 -0600

    Set mpirun process binding for ctest
    Set '-bind-to none -map-by node' by default, which enables ctest to distribute
    tests across the network.

--- cmake/test_runner.cmake
+++ cmake/test_runner.cmake
@@ -6,6 +6,7 @@ message("Test runner configuration:")
 message("  TEST_NAME (name of test)                                    : ${TEST_NAME}")
 message("  WORKDIR (test run directory)                                : ${WORKDIR}")
 message("  CHARMRUN (used to run Charm++ executables)                  : ${CHARMRUN}")
+message("  MPIRUN_BIND_ARGS (mpirun process binding)                   : ${MPIRUN_BIND_ARGS}")
 message("  TEST_EXECUTABLE (executable tested)                         : ${TEST_EXECUTABLE}")
 message("  TEST_EXECUTABLE_ARGS (executable arguments)                 : ${TEST_EXECUTABLE_ARGS}")
 message("  TEST_LABELS (test labels)                                   : ${TEST_LABLES}")
@@ -28,16 +29,16 @@ message("\nRemoving existing result (if any): ${TEXT_RESULT} ${BIN_RESULT}\n")
 file(REMOVE ${TEXT_RESULT} ${BIN_RESULT})
 
 # Run the test
-set(test_command ${CHARMRUN} +p${NUMPES} -bind-to none ${TEST_EXECUTABLE}
-                 ${TEST_EXECUTABLE_ARGS})
+set(test_command ${CHARMRUN} +p${NUMPES} ${MPIRUN_BIND_ARGS}
+                 ${TEST_EXECUTABLE} ${TEST_EXECUTABLE_ARGS})
 string(REPLACE ";" " " test_command_string "${test_command}")
 message("\nRunning test command: '${test_command_string}'\n")
-execute_process(COMMAND ${test_command} RESULT_VARIABLE ERRCODE)
+execute_process(COMMAND ${test_command} RESULT_VARIABLE ERROR)
 
 # Check return value from test
-if(ERRCODE)
+if(ERROR)
 
-  message(FATAL_ERROR "Test failed to run: '${test_command_string}' returned error code: ${ERRCODE}")
+  message(FATAL_ERROR "Test failed to run: '${test_command_string}' returned error code: ${ERROR}")
 
 else() # Test command ran successfully, attempt to do diffs
 
@@ -47,10 +48,10 @@ else() # Test command ran successfully, attempt to do diffs
                           ${TEXT_RESULT} ${TEXT_DIFF_PROG_CONF})
     string(REPLACE ";" " " text_diff_command_string "${text_diff_command}")
     message("\nRunning text diff command: '${text_diff_command_string}'\n")
-    execute_process(COMMAND ${text_diff_command} RESULT_VARIABLE ERRCODE)
+    execute_process(COMMAND ${text_diff_command} RESULT_VARIABLE ERROR)
     # Check return value from textual diff command
-    if(ERRCODE)
-      message("Textual diff failed to run: '${text_diff_command_string}' returned error code: ${ERRCODE}")
+    if(ERROR)
+      message("Textual diff failed to run: '${text_diff_command_string}' returned error code: ${ERROR}")
     endif()
 
   endif()
