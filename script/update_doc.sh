5311e635dc914754cc05abfe31a2d9fcf9a47081
Jozsef Bakosi <jbakosi@lanl.gov>
2015-08-02 20:32:21 -0600

    Run unit and regression test suite in drone

--- script/update_doc.sh
+++ script/update_doc.sh
@@ -2,7 +2,7 @@
 # 
 # \file      script/update_doc.sh
 # \author    J. Bakosi
-# \date      Sun 02 Aug 2015 08:12:50 PM MDT
+# \date      Sun 02 Aug 2015 08:21:14 PM MDT
 # \copyright 2012-2015, Jozsef Bakosi.
 # \brief     Update documentation and upload to github pages
 # 
@@ -22,5 +22,8 @@ ninja doc
 cd doc/html
 touch .nojekyll
 git add .
+cd -
+git add unittest_coverage
+git add regression_coverage
 git commit -m "Automated documentation build for changeset ${DOC4COMMIT}"
 git push origin gh-pages
