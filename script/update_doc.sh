8f8cdd9a55082cdc2590377745b0d9c7579ef9ab
Jozsef Bakosi <jbakosi@lanl.gov>
2015-08-05 12:11:08 -0600

    Adjust update_doc.sh for more accurate coverage
    Revamped the update_doc.sh script, which now wipes the work directory between
    the generation of unit test coverage, regression test coverage, and doc
    generation.
    
    Also, the script is now working in its own work directory and cleanes up after
    itself. Thus it is more independent of the build dir that it used to work in.

--- script/update_doc.sh
+++ script/update_doc.sh
@@ -4,23 +4,55 @@
 # \author    J. Bakosi
 # \date      Sun 02 Aug 2015 10:06:34 PM MDT
 # \copyright 2012-2015, Jozsef Bakosi.
-# \brief     Update documentation and upload to github pages
+# \brief     Update documentation and test coverage and upload to github pages
+# \details   This script assumes that
+#   - a clone already exists and the TPLs are already built
+#   - the environment is setup for running cmake
 # 
 ################################################################################
 
-cd /home/jbakosi/code/quinoa
+# Set quinoa directory
+QUINOA=/home/jbakosi/code/quinoa
+# Set work directory
+WORKDIR=${QUINOA}/gh-pages
+
+# Get commit we are generating documentation for
+cd ${QUINOA}
 DOC4COMMIT=$(git rev-parse --verify HEAD)
-cd build/gnu
-rm -rf doc/html
+
+# Change to work directory
+mkdir ${WORKDIR}
+cd ${WORKDIR}
+
+# Generate unit test coverage report, move it to /tmp, and clean
+cmake -G Ninja ../src
+ninja unittest_coverage
+mv doc/html/unittest_coverage /tmp
+rm * .ninja_* -rf
+
+# Generate regression test coverage report, move it to /tmp, and clean
+cmake -G Ninja ../src
+ninja regression_coverage
+mv doc/html/regression_coverage /tmp
+rm * .ninja_* -rf
+
+# Start out in empty build-dir with a fresh clone of the gh-pages branch
 git clone git@github.com:jbakosi/quinoa.git --branch gh-pages --single-branch doc/html
 cd doc/html
 git rm -rf .
 cd -
-ninja unittest_coverage
-ninja regression_coverage
+
+# Generate documentation, move code coverage reports in place, and push
+cmake -G Ninja ../src
 ninja doc
 cd doc/html
+mv /tmp/unittest_coverage .
+mv /tmp/regression_coverage .
 touch .nojekyll
 git add .
 git commit -m "Automated documentation build for changeset ${DOC4COMMIT}"
 git push origin gh-pages
+
+# Remove work directory
+cd ${QUINOA}
+rm -rf ${WORKDIR}
