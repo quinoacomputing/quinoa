119e79dc9f28f10cced34f78ce70f04ef474b99d
Jozsef Bakosi <jbakosi@lanl.gov>
2016-04-26 07:52:25 -0600

    Fix static-link line for walker
    This reinstates our ability to produce fully statically linked executables.
    
    [CI SKIP]

--- cmake/FindLAPACKE.cmake
+++ cmake/FindLAPACKE.cmake
@@ -1,13 +1,14 @@
-# Find the C-interface to LAPACK
+# Find the C-interface to LAPACK as well as LAPACK/BLAS
 #
-#  LAPACKE_FOUND - System has LAPACKE
+#  LAPACKE_FOUND - System has LAPACKE as well as LAPACK/BLAS
 #  LAPACKE_INCLUDE - The LAPACKE include directory
-#  LAPACKE_LIBRARY - The libraries needed to use LAPACKE
+#  LAPACKE_LIBRARY - The libraries needed to use LAPACKE with dynamic linking
+#  LAPACK_LIBRARIES - The libraries required to link LAPACKE with static linking
 #
 #  Set LAPACKE_ROOT before calling find_package to a path to add an additional
 #  search path, e.g.,
 #
-#  Usage:
+#  Usage (assuming dynamic linking):
 #
 #  set(LAPACKE_ROOT "/path/to/custom/lapacke") # prefer over system
 #  find_package(LAPACKE)
@@ -16,7 +17,7 @@
 #  endif()
 
 # If already in cache, be silent
-if(LAPACKE_INCLUDE AND LAPACKE_LIBRARY)
+if(LAPACKE_INCLUDE AND LAPACKE_LIBRARY AND LAPACK_LIBRARIES)
   set (LAPACKE_FIND_QUIETLY TRUE)
 endif()
 
@@ -25,13 +26,31 @@ find_path(LAPACKE_INCLUDE lapacke.h DOC "C-interface to LAPACK"
 
 if(NOT BUILD_SHARED_LIBS)
   find_library(LAPACKE_LIBRARY NAMES liblapacke.a HINTS ${LAPACKE_ROOT}/lib)
+  # Static linking requires explicitly all dependent libraries
+  find_library(LAPACK_LIBRARY NAMES liblapack.a HINTS ${LAPACKE_ROOT}/lib)
+  find_library(BLAS_LIBRARY NAMES libblas.a HINTS ${LAPACKE_ROOT}/lib)
+  # These two are also searched on multiarch lib paths. This requires knowing
+  # the architecture and the major version of the compiler. The compiler version
+  # is detected in src/CMakeLists.txt and the architecture is detected by
+  # including GNUInstallDirs in cmake/TPLs.cmake.
+  find_library(GFORTRAN_LIBRARY NAMES libgfortran.a HINTS ${LAPACKE_ROOT}/lib
+               /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR})
+  find_library(QUADMATH_LIBRARY NAMES libquadmath.a  HINTS ${LAPACKE_ROOT}/lib
+               /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR})
 else()
   find_library(LAPACKE_LIBRARY NAMES lapacke HINTS ${LAPACKE_ROOT}/lib)
 endif()
 
+# Only for reporting on what is found
+set(LAPACK_LIBRARIES ${LAPACKE_LIBRARY} ${LAPACK_LIBRARY} ${BLAS_LIBRARY}
+                     ${GFORTRAN_LIBRARY} ${QUADMATH_LIBRARY})
+
+# For output only
+set(LAPACK_LIBS ${LAPACK_LIBRARIES})
+
 # Handle the QUIETLY and REQUIRED arguments and set LAPACKE_FOUND to TRUE if
 # all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LAPACKE DEFAULT_MSG LAPACKE_LIBRARY LAPACKE_INCLUDE)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LAPACKE DEFAULT_MSG LAPACK_LIBS)
 
-MARK_AS_ADVANCED(LAPACKE_INCLUDE LAPACKE_LIBRARY)
+MARK_AS_ADVANCED(LAPACKE_INCLUDE LAPACKE_LIBRARY LAPACK_LIBRARIES)
