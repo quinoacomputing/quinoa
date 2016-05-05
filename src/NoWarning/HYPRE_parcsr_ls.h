d0405a0686c1f4f942817fd921a4c57055b9b730
Jozsef Bakosi <jbakosi@lanl.gov>
2016-05-05 13:44:47 -0600

    Hide warnings triggered on alpine

--- src/NoWarning/HYPRE_parcsr_ls.h
+++ src/NoWarning/HYPRE_parcsr_ls.h
@@ -1,14 +1,15 @@
 //******************************************************************************
 /*!
-  \file      src/NoWarning/pugixml.h
+  \file      src/NoWarning/HYPRE_parcsr_ls.h
   \author    J. Bakosi
-  \date      Thu 05 May 2016 01:41:45 PM MDT
+  \date      Thu 05 May 2016 01:42:23 PM MDT
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
-  \brief     Include pugixml.hpp with turning off specific compiler warnings
+  \brief     Include HYPRE_parcsr_ls.h with turning off specific compiler
+             warnings
 */
 //******************************************************************************
-#ifndef nowarning_pugixml_h
-#define nowarning_pugixml_h
+#ifndef nowarning_HYPRE_parcsr_ls_h
+#define nowarning_HYPRE_parcsr_ls_h
 
 #if defined(__clang__)
 #elif defined(__GNUC__)
@@ -16,11 +17,11 @@
   #pragma GCC diagnostic ignored "-Weffc++"
 #endif
 
-#include <pugixml.hpp>
+#include <HYPRE_parcsr_ls.h>
 
 #if defined(__clang__)
 #elif defined(__GNUC__)
   #pragma GCC diagnostic pop
 #endif
 
-#endif // nowarning_pugixml_h
+#endif // nowarning_HYPRE_parcsr_ls_h
