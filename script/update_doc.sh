cf61fc8cfd2c9081c218d17fa5ce72dd8e9cdaff
Jozsef Bakosi <jbakosi@lanl.gov>
2016-05-08 18:20:58 -0600

    Fix logo location copy src
    Also switch to https for log display in README, since doxygen displays the logo
    only tis way.

--- script/update_doc.sh
+++ script/update_doc.sh
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \date      Sun 02 Aug 2015 10:06:34 PM MDT
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
-# \brief     Update Sun 08 May 2016 08:10:58 AM MDT
+# \brief     Update Sun 08 May 2016 06:26:26 PM MDT
 # \details   This script assumes that
 #   - a clone already exists and the TPLs are already built
 #   - the environment is setup for running cmake
@@ -56,9 +56,9 @@ mv /tmp/unittest_coverage .
 mv /tmp/regression_coverage .
 mv /tmp/test_coverage .
 touch .nojekyll
-cp ../images/quinoa.svg .
+cp ../../../doc/images/quinoa.svg .
 git add .
-git commit -m "Automated documentation build for changeset ${DOC4COMMIT}"
+git commit -m "Documentation for changeset ${DOC4COMMIT}"
 git push origin gh-pages
 
 # Remove work directory
