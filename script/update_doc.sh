f7721f6ee1a9570cfcdd537ffe540fb69661407c
Jozsef Bakosi <jbakosi@lanl.gov>
2016-05-12 07:04:18 -0600

    Adjust update_doc script for cron

--- script/update_doc.sh
+++ script/update_doc.sh
@@ -1,66 +1,97 @@
+#!/bin/bash -eux
+# vim: filetype=sh:
 ################################################################################
 # 
 # \file      script/update_doc.sh
 # \author    J. Bakosi
-# \date      Sun 02 Aug 2015 10:06:34 PM MDT
+# \date      Wed 11 May 2016 11:59:39 AM MDT
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
-# \brief     Update Sun 08 May 2016 06:26:26 PM MDT
-# \details   This script assumes that
-#   - a clone already exists and the TPLs are already built
-#   - the environment is setup for running cmake
+# \brief     Regenerate doc and test coverage and upload to github pages
+# \details   This script clones the github repository, builds the third-party
+#   libraries and the code in DEBUG, generates the documentation, and uploads to
+#   github.
 # 
 ################################################################################
 
-# Set quinoa directory
-QUINOA=/home/jbakosi/code/quinoa
+# trap and cleanup on signals INT, TERM, and EXIT
+trap cleanup INT TERM EXIT
+
 # Set work directory
-WORKDIR=${QUINOA}/gh-pages
-
-# Get commit we are generating documentation for
-cd ${QUINOA}
-DOC4COMMIT=$(git rev-parse --verify HEAD)
-
-# Change to work directory
-mkdir ${WORKDIR}
-cd ${WORKDIR}
-
-# Generate unit test coverage report, move it to /tmp, and clean
-cmake -G Ninja ../src
-ninja unittest_coverage
-mv doc/html/unittest_coverage /tmp
-rm * .ninja_* -rf
-
-# Generate regression test coverage report, move it to /tmp, and clean
-cmake -G Ninja ../src
-ninja regression_coverage
-mv doc/html/regression_coverage /tmp
-rm * .ninja_* -rf
-
-# Generate full test coverage report, move it to /tmp, and clean
-cmake -G Ninja ../src
-ninja test_coverage
-mv doc/html/test_coverage /tmp
-rm * .ninja_* -rf
-
-# Start out in empty build-dir with a fresh clone of the gh-pages branch
-git clone git@github.com:quinoacomputing/quinoa.git --branch gh-pages --single-branch doc/html
-cd doc/html
-git rm -rf .
-cd -
-
-# Generate documentation, move code coverage reports in place, and push
-cmake -G Ninja ../src
-ninja doc
-cd doc/html
-mv /tmp/unittest_coverage .
-mv /tmp/regression_coverage .
-mv /tmp/test_coverage .
-touch .nojekyll
-cp ../../../doc/images/quinoa.svg .
-git add .
-git commit -m "Documentation for changeset ${DOC4COMMIT}"
-git push origin gh-pages
+WORKDIR=/tmp/q
+rm -rf ${WORKDIR} && mkdir ${WORKDIR}
 
 # Remove work directory
-cd ${QUINOA}
-rm -rf ${WORKDIR}
+cleanup()
+{
+  rm -rf ${WORKDIR}
+  exit
+}
+
+# Clone from github
+cd ${WORKDIR}
+git clone git@github.com:quinoacomputing/quinoa.git
+cd ${WORKDIR}/quinoa
+
+# Get git commit sha1 for code and doc branches
+CODE_SHA=$(git rev-parse --verify master)
+DOC_SHA=$(git rev-parse --verify origin/gh-pages)
+
+# See if doc commit sha1 equals that of code (if so, no need to regenerate doc)
+if [ $CODE_SHA != $DOC_SHA ]; then
+
+  # Query number of CPUs
+  case "$OSTYPE" in
+    darwin*)  CPUS=`sysctl -n hw.ncpu`;;
+    linux*)   CPUS=`cat /proc/cpuinfo | grep MHz | wc -l`;;
+  esac
+
+  # Build third-party libraries
+  mkdir tpl/build && cd tpl/build && cmake .. && make -sj$CPUS
+
+  # Set build directory
+  BUILDDIR=${WORKDIR}/quinoa/gh-pages
+
+  # Change to work directory
+  mkdir ${BUILDDIR}
+  cd ${BUILDDIR}
+
+  # Generate unit test coverage report, move it to ${WORKDIR}, and clean
+  cmake -G Ninja ../src
+  ninja unittest_coverage
+  mv doc/html/unittest_coverage ${WORKDIR}
+  rm * .ninja_* -rf
+
+  # Generate regression test coverage report, move it to ${WORKDIR}, and clean
+  cmake -G Ninja ../src
+  ninja regression_coverage
+  mv doc/html/regression_coverage ${WORKDIR}
+  rm * .ninja_* -rf
+
+  # Generate full test coverage report, move it to ${WORKDIR}, and clean
+  cmake -G Ninja ../src
+  ninja test_coverage
+  mv doc/html/test_coverage ${WORKDIR}
+  rm * .ninja_* -rf
+
+  # Start out in empty build-dir with a fresh clone of the gh-pages branch
+  git clone git@github.com:quinoacomputing/quinoa.git --branch gh-pages --single-branch doc/html
+  cd doc/html
+  git rm -rf .
+  cd -
+
+  # Generate documentation, move code coverage reports in place, and push
+  cmake -G Ninja ../src
+  ninja doc
+  cd doc/html
+  mv ${WORKDIR}/unittest_coverage .
+  mv ${WORKDIR}/regression_coverage .
+  mv ${WORKDIR}/test_coverage .
+  touch .nojekyll
+  cp ../../../doc/images/quinoa.svg .
+  git add .
+  git commit --no-gpg-sign -m "Documentation for changeset ${CODE_SHA}"
+  git push origin gh-pages
+
+fi
+
+cleanup
