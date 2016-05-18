788e26cfbd27b8ec265a4ad99edf69d7ecbac4f9
Jozsef Bakosi <jbakosi@lanl.gov>
2016-05-17 21:52:50 -0600

    Offer ncpus in test run script as optional arg

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -4,7 +4,7 @@
 # 
 # \file      script/run_tests.sh
 # \author    J. Bakosi
-# \date      Fri 29 Apr 2016 09:25:59 AM MDT
+# \date      Tue 17 May 2016 09:52:22 PM MDT
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Run multiple test suites as part of automated testing
 # \details   Run multiple test suites as part of automated testing.
@@ -17,18 +17,22 @@
 #  This script will try to run the test suites in whatever build directory it is
 #  called in.
 #
-#  Note that it should also be fine putting the commands in this script into
-#  .drone.yml, however, for some unknown reason, only the first command was
-#  executed, so we call this script, running multiple test suites, used for
-#  automated testing.
+#  If an argument is given, use that as the number of CPUs, if not grab them
+#  all.
 #
 ################################################################################
 
-# Query number of CPUs
-case "$OSTYPE" in
-  darwin*)  CPUS=`sysctl -n hw.ncpu`;;
-  linux*)   CPUS=`cat /proc/cpuinfo | grep MHz | wc -l`;;
-esac
+# If an argument is given, use that as the number of CPUs, if not grab them all
+if [ "$#" -eq 1 ]; then
+  CPUS=$1
+else
+  # Query number of CPUs
+  case "$OSTYPE" in
+    darwin*)  CPUS=`sysctl -n hw.ncpu`;;
+    linux*)   CPUS=`cat /proc/cpuinfo | grep MHz | wc -l`;;
+  esac
+fi
+echo "Will use $CPUS CPUs"
 
 # Run unit test suite
 ./charmrun +p$CPUS --allow-run-as-root Main/unittest -v
