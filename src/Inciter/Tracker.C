dab9d76b2a368ce4c835da65ab4f3b58c6fbc9f5
Jozsef Bakosi <jbakosi@lanl.gov>
2016-07-16 07:12:55 -0600

    Add new files forgotten in 1d2d3cc

---
+++ src/Inciter/Tracker.C
@@ -0,0 +1,44 @@
+// *****************************************************************************
+/*!
+  \file      src/Inciter/Tracker.C
+  \author    F.J. Gonzalez
+  \date      Fri 15 Jul 2016 09:32:30 AM MDT
+  \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
+  \details   Tracker advances Lagrangian particles passively tracking fluid
+    flow.. There are a potentially large number of Tracker Charm++ chares
+    created by Conductor. Each tracker gets a chunk of the full load (part of
+    the total number particles) and does the same: initializes and advances a
+    particles in time based on a velocity field.
+*/
+// *****************************************************************************
+
+#include "Tracker.h"
+#include "Macro.h"
+
+using inciter::Tracker;
+
+Tracker::Tracker( const ConductorProxy& conductor ) : m_conductor( conductor )
+// *****************************************************************************
+//  Constructor
+//! \param[in] conductor Host (Conductor) proxy
+//! \author F.J. Gonzalez
+// *****************************************************************************
+{
+}
+
+void
+Tracker::advance( tk::real dt, uint64_t it, tk::real t )
+// *****************************************************************************
+// Advance equations to next stage
+//! \param[in] dt Size of time step
+//! \param[in] it Iteration count
+//! \param[in] t Physical time
+//! \author F.J. Gonzalez
+// *****************************************************************************
+{
+  IGNORE(dt);
+  IGNORE(it);
+  IGNORE(t);
+}
+
+#include "NoWarning/tracker.def.h"
---
+++ src/Inciter/Tracker.h
@@ -0,0 +1,50 @@
+// *****************************************************************************
+/*!
+  \file      src/Inciter/Tracker.h
+  \author    F.J. Gonzalez
+  \date      Fri 15 Jul 2016 09:18:58 PM MDT
+  \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
+  \brief     Tracker advances Lagrangian particles passively tracking fluid flow
+  \details   Tracker advances Lagrangian particles passively tracking fluid
+    flow.. There are a potentially large number of Tracker Charm++ chares
+    created by Conductor. Each tracker gets a chunk of the full load (part of
+    the total number particles) and does the same: initializes and advances a
+    particles in time based on a velocity field.
+*/
+// *****************************************************************************
+#ifndef Tracker_h
+#define Tracker_h
+
+#include <unordered_map>
+
+#include "Types.h"
+
+#include "NoWarning/conductor.decl.h"
+#include "NoWarning/tracker.decl.h"
+
+namespace inciter {
+
+//! \brief Tracker Charm++ chare used to advance Lagrangian particles tracking
+//!   fluid flow in space and time
+class Tracker : public CBase_Tracker {
+
+  private:
+    using ConductorProxy = CProxy_Conductor;
+
+  public:
+    //! Constructor
+    explicit Tracker( const CProxy_Conductor& conductor );
+
+    //! Migrate constructor
+    explicit Tracker( CkMigrateMessage* ) : m_conductor() {}
+
+    //! Advance particles to next stage
+    void advance( tk::real dt, uint64_t it, tk::real t );
+
+  private:
+    ConductorProxy m_conductor;         //!< Conductor proxy
+};
+
+} // inciter::
+
+#endif // Tracker_h
