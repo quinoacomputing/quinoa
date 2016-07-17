f5f5e58ed70e73a03d909dfd2ebea1a5b21fb59a
Jozsef Bakosi <jbakosi@lanl.gov>
2016-07-16 22:35:01 -0600

    Rename Euler, Poisson, and AdvDiff names to be consistent with the rest

--- src/Control/Keywords.h
+++ src/Control/Keywords.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Control/Keywords.h
   \author    J. Bakosi
-  \date      Sat 16 Jul 2016 10:16:15 PM MDT
+  \date      Sat 16 Jul 2016 10:36:16 PM MDT
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
   \brief     Definition of all keywords
   \details   This file contains the definition of all keywords, including those
@@ -2586,7 +2586,7 @@ struct pde_u0_info {
 using pde_u0 = keyword< pde_u0_info, u,'0' >;
 
 struct advdiff_info {
-  static std::string name() { return "Scalar advection-diffusion"; }
+  static std::string name() { return "Advection-diffusion"; }
   static std::string shortDescription() { return
     "Start configuration block for an advection-diffusion equation"; }
   static std::string longDescription() { return
@@ -2605,7 +2605,7 @@ struct advdiff_info {
 using advdiff = keyword< advdiff_info, a,d,v,d,i,f,f >;
 
 struct poisson_info {
-  static std::string name() { return "Poisson equation"; }
+  static std::string name() { return "Poisson"; }
   static std::string shortDescription() { return
     "Start configuration block for a Poisson equation"; }
   static std::string longDescription() { return
@@ -2621,7 +2621,7 @@ struct poisson_info {
 using poisson = keyword< poisson_info, p,o,i,s,s,o,n >;
 
 struct euler_info {
-  static std::string name() { return "Euler equations"; }
+  static std::string name() { return "Euler"; }
   static std::string shortDescription() { return
     "Start configuration block for the Euler equations"; }
   static std::string longDescription() { return
