fd0cd9340c69eea924691d3a944212008c965e17
Jozsef Bakosi <jbakosi@lanl.gov>
2016-07-26 12:02:13 -0600

    Generate npar number of particles into each cell

--- src/Inciter/Performer.h
+++ src/Inciter/Performer.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Inciter/Performer.h
   \author    J. Bakosi
-  \date      Tue 26 Jul 2016 07:42:59 AM MDT
+  \date      Tue 26 Jul 2016 11:34:34 AM MDT
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
   \brief     Performer advances a system of systems of PDEs
   \details   Performer advances a system of systems of PDEs. There are a
@@ -25,6 +25,7 @@
 
 #include "Types.h"
 #include "MeshNodes.h"
+#include "Particles.h"
 #include "DerivedData.h"
 #include "VectorReducer.h"
 #include "Inciter/InputDeck/InputDeck.h"
@@ -104,14 +105,11 @@ class Performer : public CBase_Performer {
     void advance( uint8_t stage, tk::real dt, uint64_t it, tk::real t );
 
     //! Generates particles into mesh cells
-    void genPar( std::size_t npar );
+    void genPar();
 
     //! Receive particles coordinate information 
     //! NOTE: Currently hard coding particle locations
-    void parinel( const std::vector< tk::real >& xp,
-                  const std::vector< tk::real >& yp,
-                  const std::vector< tk::real >& zp,
-                  std::size_t npar );  
+    void parinel();
 
     /** @name Pack/Unpack: Serialize Performer object for Charm++ */
     ///@{
@@ -178,6 +176,7 @@ class Performer : public CBase_Performer {
     tk::MeshNodes m_u, m_uf, m_un;
     //! Sparse matrix sotring the diagonals and off-diagonals of nonzeros
     tk::MeshNodes m_lhsd, m_lhso;
+    tk::Particles m_particles;          //!< (Tracker) particles properties
 
     //! Send off global row IDs to linear system merger, setup global->local IDs
     void setupIds();
