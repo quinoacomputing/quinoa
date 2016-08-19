836bbf4437795569b5f3320477ab802bcb3e8101
Jozsef Bakosi <jbakosi@lanl.gov>
2016-08-19 11:29:08 -0600

    Remove dependency on boost::ice_and.h
    Thanks to https://github.com/boostorg/units/pull/17.

--- src/Base/CharmUtil.h
+++ src/Base/CharmUtil.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Base/CharmUtil.h
   \author    J. Bakosi
-  \date      Tue 10 May 2016 01:57:55 PM MDT
+  \date      Fri 19 Aug 2016 11:27:26 AM MDT
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
   \brief     Charm++ utilities
   \details   Charm++ utilities
@@ -11,14 +11,14 @@
 #ifndef CharmUtil_h
 #define CharmUtil_h
 
-#include "NoWarning/ice_and.h"
+#include <boost/mpl/bool.hpp>
 
 namespace tk {
 
 //! Type trait querying whether T is a strongly typed enum
 template< typename T >
-using is_enum_class = typename boost::type_traits::ice_and<
-                        std::is_enum< T >::value,
+using is_enum_class = typename boost::mpl::bool_<
+                        std::is_enum< T >::value &&
                         !std::is_convertible< T, uint8_t >::value >;
 
 } // tk::
