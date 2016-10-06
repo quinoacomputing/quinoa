895b5d915c6692e1b41adc5d1bc18c73434821cc
Jozsef Bakosi <jbakosi@lanl.gov>
2016-10-06 10:02:00 -0600

    Ignore warning from clang

--- src/NoWarning/beta_distribution.h
+++ src/NoWarning/beta_distribution.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/NoWarning/beta_distribution.h
   \author    J. Bakosi
-  \date      Fri 30 Sep 2016 12:39:32 PM MDT
+  \date      Thu 06 Oct 2016 10:01:46 AM MDT
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
   \brief     Include boost/random/beta_distribution.hpp with turning off
              specific compiler warnings
@@ -15,6 +15,7 @@
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
   #pragma clang diagnostic ignored "-Wfloat-equal"
+  #pragma clang diagnostic ignored "-Wreserved-id-macro"
 #elif defined(__GNUC__)
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wfloat-equal"
