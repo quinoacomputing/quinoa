2d497f91125ddce8e17e19c45b6de5775ff960be
Jozsef Bakosi <jbakosi@lanl.gov>
2016-12-01 15:36:14 -0700

    Make pstreams include path consistent with that of other TPLs

--- cmake/FindPStreams.cmake
+++ cmake/FindPStreams.cmake
@@ -4,14 +4,14 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the Pstreams library
-# \date      Sat 11 Jun 2016 08:20:07 PM MDT
+# \date      Thu 01 Dec 2016 03:35:02 PM MST
 #
 ################################################################################
 
 # Find the Pstreams library
 #
 #  PSTREAMS_FOUND - System has pstreams
-#  PSTREAMS_INCLUDES - The pstreams include directory (pstreams is header-only)
+#  PSTREAMS_INCLUDE_PATH - The pstreams include directory (pstreams is header-only)
 #
 #  Set PSTREAMS_ROOT before calling find_package to a path to add an additional
 #  search path, e.g.,
@@ -22,17 +22,17 @@
 #  find_package(Pstreams)
 
 # If already in cache, be silent
-if(PSTREAMS_INCLUDES)
+if(PSTREAMS_INCLUDE_PATH)
   set (PSTREAMS_FIND_QUIETLY TRUE)
 endif()
 
-FIND_PATH(PSTREAMS_INCLUDES NAMES pstream.h
+FIND_PATH(PSTREAMS_INCLUDE_PATH NAMES pstream.h
           PATH_SUFFIXES pstreams
           HINTS ${PSTREAMS_ROOT}/include $ENV{PSTREAMS_ROOT})
 
 # Handle the QUIETLY and REQUIRED arguments and set PSTREAMS_FOUND to TRUE if
 # all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pstreams DEFAULT_MSG PSTREAMS_INCLUDES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pstreams DEFAULT_MSG PSTREAMS_INCLUDE_PATH)
 
-MARK_AS_ADVANCED(PSTREAMS_INCLUDES)
+MARK_AS_ADVANCED(PSTREAMS_INCLUDE_PATH)
