8d9ffe43fc08a24d529ef613ae67c92b2ba7d0c1
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-09 13:49:14 -0700

    Convert RNGTest to new PEGTL
    Convert both command line and input deck parsers.

--- src/Control/RNGTest/InputDeck/Grammar.h
+++ src/Control/RNGTest/InputDeck/Grammar.h
@@ -2,7 +2,7 @@
 /*!
   \file      src/Control/RNGTest/InputDeck/Grammar.h
   \author    J. Bakosi
-  \date      Tue 26 Jul 2016 07:41:48 AM MDT
+  \date      Mon 09 Jan 2017 01:39:34 PM MST
   \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
   \brief     Random number generator test suite grammar definition
   \details   Random number generator test suite input deck grammar definition.
@@ -15,7 +15,6 @@
 #define RNGTestInputDeckGrammar_h
 
 #include "CommonGrammar.h"
-#include "PEGTLParsed.h"
 #include "Keywords.h"
 #include "QuinoaConfig.h"
 
@@ -32,16 +31,6 @@ extern ctr::InputDeck g_inputdeck_defaults;
 //! RNGTest input deck facilitating user input for testing RNGs
 namespace deck {
 
-  //! PEGTLParsed type specialized to RNGTest's input deck parser
-  //! \details PEGTLInputDeck is practically InputDeck equipped with PEGTL
-  //!   location information so the location can be tracked during parsing.
-  //! \author J. Bakosi
-  using PEGTLInputDeck =
-          tk::ctr::PEGTLParsed< ctr::InputDeck,
-                                pegtl::file_input< ctr::Location >,
-                                tag::cmd,
-                                ctr::CmdLine >;
-
   //! \brief Specialization of tk::grm::use for RNGTest's control file parser
   //! \author J. Bakosi
   template< typename keyword >
@@ -50,25 +39,41 @@ namespace deck {
                             ctr::InputDeck::keywords2,
                             ctr::InputDeck::keywords3 >;
 
-  // RNGTest's InputDeck State
+} // ::deck
+} // ::rngtest
 
-  //! Everything is stored in Stack during parsing
-  //! \author J. Bakosi
-  using Stack = PEGTLInputDeck;
+namespace tk {
+namespace grm {
+
+  // Note that PEGTL action specializations must be in the same namespace as the
+  // template being specialized. See http://stackoverflow.com/a/3052604.
 
   // RNGTest's InputDeck actions
 
+  //! Rule used to trigger action
+  template< class Option, typename... tags >
+  struct store_rngtest_option : pegtl::success {};
   //! \brief Put option in state at position given by tags
   //! \details This is simply a wrapper around tk::grm::store_option passing the
-  //!    stack defaults.
+  //!    stack defaults for rngtest.
   //! \author J. Bakosi
   template< class Option, typename... tags >
-  struct store_option : pegtl::action_base< store_option< Option, tags... > > {
-    static void apply( const std::string& value, Stack& stack ) {
-      tk::grm::store_option< Stack, use, Option, ctr::InputDeck, tags... >
-                           ( stack, value, g_inputdeck_defaults );
+  struct action< store_rngtest_option< Option, tags... > > {
+    template< typename Input, typename Stack >
+    static void apply( const Input& in, Stack& stack ) {
+      store_option< Stack, rngtest::deck::use, Option, rngtest::ctr::InputDeck,
+                    Input, tags... >
+                  ( stack, in, rngtest::g_inputdeck_defaults );
     }
   };
+
+} // ::grm
+} // ::tk
+
+namespace rngtest {
+
+//! RNGTest input deck facilitating user input for testing RNGs
+namespace deck {
 
   // RNGTest's InputDeck grammar
 
@@ -77,11 +82,11 @@ namespace deck {
   struct rngs :
          pegtl::sor<
                      #ifdef HAS_MKL
-                     tk::mkl::rngs< Stack, use,
+                     tk::mkl::rngs< use,
                                     tag::selected, tag::rng,
                                     tag::param, tag::rngmkl >,
                      #endif
-                     tk::rngsse::rngs< Stack, use,
+                     tk::rngsse::rngs< use,
                                        tag::selected, tag::rng,
                                        tag::param, tag::rngsse > > {};
 
@@ -89,16 +94,14 @@ namespace deck {
   //! \author J. Bakosi
   template< typename battery_kw >
   struct testu01 :
-         pegtl::ifmust< tk::grm::scan< Stack,
-                                       typename battery_kw::pegtl_string,
-                                       store_option< ctr::Battery,
-                                                     tag::selected,
-                                                     tag::battery > >,
-                        pegtl::sor<
-                          tk::grm::block< use< kw::end >, rngs >,
-                          pegtl::apply<
-                          tk::grm::error< Stack,
-                                          tk::grm::MsgKey::UNFINISHED > > > > {};
+         pegtl::if_must<
+           tk::grm::scan< typename battery_kw::pegtl_string,
+                          tk::grm::store_rngtest_option< ctr::Battery,
+                                                         tag::selected,
+                                                         tag::battery > >,
+           pegtl::sor< tk::grm::block< use< kw::end >, rngs >,
+                       tk::grm::msg< tk::grm::MsgType::ERROR,
+                                     tk::grm::MsgKey::UNFINISHED > > > {};
 
   //! \brief Match all batteries
   //! \author J. Bakosi
@@ -110,7 +113,7 @@ namespace deck {
   //! \brief All keywords
   //! \author J. Bakosi
   struct keywords :
-         pegtl::sor< tk::grm::title< Stack, use >, battery > {};
+         pegtl::sor< tk::grm::title< use >, battery > {};
 
   //! \brief Grammar entry point: parse keywords and ignores until eof
   //! \author J. Bakosi
