ef614e960863e955cd6784d78efe09945113b634
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-11 08:36:27 -0700

    Ignore some clang warnings for PEGTL

--- src/NoWarning/pegtl.h
+++ src/NoWarning/pegtl.h
@@ -11,6 +11,10 @@
 #define nowarning_pegtl_h
 
 #if defined(__clang__)
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wshadow"
+  #pragma clang diagnostic ignored "-Wsign-conversion"
+  #pragma clang diagnostic ignored "-Wshadow-field-in-constructor"
 #elif defined(__GNUC__)
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wshadow"
@@ -19,6 +23,7 @@
 #include <pegtl.hh>
 
 #if defined(__clang__)
+  #pragma clang diagnostic pop
 #elif defined(__GNUC__)
   #pragma GCC diagnostic pop
 #endif
