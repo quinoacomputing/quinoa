4b26f6a9a71e525d3dc9f3936af07875f787d086
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-11 11:16:02 -0700

    Find LAPACKE based on env var
    Can be used, e.g., on Mac OS with macports

--- cmake/FindLAPACKE.cmake
+++ cmake/FindLAPACKE.cmake
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the C-interface to LAPACK as well as LAPACK/BLAS
-# \date      Fri 06 May 2016 06:42:27 AM MDT
+# \date      Wed 11 Jan 2017 10:31:04 AM MST
 #
 ################################################################################
 
@@ -39,24 +39,39 @@ else()
 endif()
 
 find_path(LAPACKE_INCLUDE lapacke.h DOC "C-interface to LAPACK"
-          HINTS ${LAPACKE_ROOT}/include
-          PATH_SUFFIXES lapacke)
+          HINTS ${LAPACKE_ROOT}/include $ENV{LAPACKE_ROOT}/include
+          PATH_SUFFIXES lapack lapacke)
 
 if(NOT BUILD_SHARED_LIBS)
-  find_library(LAPACKE_LIBRARY NAMES liblapacke.a HINTS ${LAPACKE_ROOT}/lib)
+  find_library(LAPACKE_LIBRARY NAMES liblapacke.a HINTS ${LAPACKE_ROOT}/lib
+                                                        $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
   # Static linking requires explicitly all dependent libraries
-  find_library(LAPACK_LIBRARY NAMES liblapack.a HINTS ${LAPACKE_ROOT}/lib)
-  find_library(BLAS_LIBRARY NAMES libblas.a HINTS ${LAPACKE_ROOT}/lib)
+  find_library(LAPACK_LIBRARY NAMES liblapack.a HINTS ${LAPACKE_ROOT}/lib
+                                                      $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
+  find_library(BLAS_LIBRARY NAMES libblas.a HINTS ${LAPACKE_ROOT}/lib
+                                                  $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
   # These two are also searched on multiarch lib paths. This requires knowing
   # the architecture and the major version of the compiler. The compiler version
   # is detected in src/CMakeLists.txt and the architecture is detected by
   # including GNUInstallDirs in cmake/TPLs.cmake.
-  find_library(GFORTRAN_LIBRARY NAMES libgfortran.a HINTS ${LAPACKE_ROOT}/lib
-               /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR})
-  find_library(QUADMATH_LIBRARY NAMES libquadmath.a  HINTS ${LAPACKE_ROOT}/lib
-               /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR})
+  find_library(GFORTRAN_LIBRARY NAMES libgfortran.a
+               HINTS ${LAPACKE_ROOT}/lib
+                     /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR}
+                     $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
+  find_library(QUADMATH_LIBRARY NAMES libquadmath.a
+               HINTS ${LAPACKE_ROOT}/lib
+                     /usr/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_MAJOR}
+                     $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
 else()
-  find_library(LAPACKE_LIBRARY NAMES lapacke reflapacke HINTS ${LAPACKE_ROOT}/lib)
+  find_library(LAPACKE_LIBRARY NAMES lapacke reflapacke
+               HINTS ${LAPACKE_ROOT}/lib
+                     $ENV{LAPACKE_ROOT}/lib
+               PATH_SUFFIXES lapack lapacke)
 endif()
 
 # Only for reporting on what is found
