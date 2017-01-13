28f981242551c67516b8be117fa382c3fa0a6cbf
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-13 15:52:03 -0700

    Find RNGSSE2 based on env var
    Useful with a rngsse2 environment module setting RNGSSE2_ROOT on CI
    build agent machines.

--- cmake/FindRNGSSE2.cmake
+++ cmake/FindRNGSSE2.cmake
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the RNGSSE2 library
-# \date      Fri 13 Jan 2017 11:56:12 AM MST
+# \date      Fri 13 Jan 2017 03:44:47 PM MST
 #
 ################################################################################
 
@@ -30,17 +30,28 @@ if(RNGSSE2_INCLUDES)
   set (RNGSSE2_FIND_QUIETLY TRUE)
 endif()
 
-FIND_PATH(RNGSSE2_INCLUDE_GM19 NAMES gm19.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GM29 NAMES gm29.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GM31 NAMES gm31.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GM55 NAMES gm55.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GM61 NAMES gm61.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GQ58X1 NAMES gq58x1.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GQ58X3 NAMES gq58x3.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_GQ58X4 NAMES gq58x4.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_MT19937 NAMES mt19937.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_LFSR113 NAMES lfsr113.h HINTS ${RNGSSE2_ROOT}/include)
-FIND_PATH(RNGSSE2_INCLUDE_MRG32K3A NAMES mrg32k3a.h HINTS ${RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GM19 NAMES gm19.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GM29 NAMES gm29.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GM31 NAMES gm31.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GM55 NAMES gm55.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GM61 NAMES gm61.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GQ58X1 NAMES gq58x1.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GQ58X3 NAMES gq58x3.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_GQ58X4 NAMES gq58x4.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_MT19937 NAMES mt19937.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_LFSR113 NAMES lfsr113.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
+FIND_PATH(RNGSSE2_INCLUDE_MRG32K3A NAMES mrg32k3a.h HINTS ${RNGSSE2_ROOT}/include
+                                                  $ENV{RNGSSE2_ROOT}/include)
 
 set(RNGSSE2_INCLUDES ${RNGSSE2_INCLUDE_GM19}
                      ${RNGSSE2_INCLUDE_GM29}
@@ -55,9 +66,11 @@ set(RNGSSE2_INCLUDES ${RNGSSE2_INCLUDE_GM19}
                      ${RNGSSE2_INCLUDE_MRG32K3A})
 
 if(NOT BUILD_SHARED_LIBS)
- find_library(RNGSSE2_LIBRARY NAMES librngsse.a HINTS ${RNGSSE2_ROOT}/lib)
+ find_library(RNGSSE2_LIBRARY NAMES librngsse.a HINTS ${RNGSSE2_ROOT}/lib
+                                                      $ENV{RNGSSE2_ROOT}/lib)
 else()
- find_library(RNGSSE2_LIBRARY NAMES rngsse HINTS ${RNGSSE2_ROOT}/lib)
+ find_library(RNGSSE2_LIBRARY NAMES rngsse HINTS ${RNGSSE2_ROOT}/lib
+                                                 $ENV{RNGSSE2_ROOT}/lib)
 endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set RNGSSE2_FOUND to TRUE if
