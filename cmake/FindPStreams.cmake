8c180d6eb7821ac58993ac28c30efbee9b4037ca
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-20 13:17:24 -0700

    Make FindPStreams.cmake conforming

--- cmake/FindPStreams.cmake
+++ cmake/FindPStreams.cmake
@@ -4,14 +4,14 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the Pstreams library
-# \date      Thu 01 Dec 2016 03:35:02 PM MST
+# \date      Fri 20 Jan 2017 01:15:41 PM MST
 #
 ################################################################################
 
 # Find the Pstreams library
 #
 #  PSTREAMS_FOUND - System has pstreams
-#  PSTREAMS_INCLUDE_PATH - The pstreams include directory (pstreams is header-only)
+#  PSTREAMS_INCLUDE_DIRS - The pstreams include directories
 #
 #  Set PSTREAMS_ROOT before calling find_package to a path to add an additional
 #  search path, e.g.,
@@ -22,17 +22,19 @@
 #  find_package(Pstreams)
 
 # If already in cache, be silent
-if(PSTREAMS_INCLUDE_PATH)
+if(PSTREAMS_INCLUDE_DIRS)
   set (PSTREAMS_FIND_QUIETLY TRUE)
 endif()
 
-FIND_PATH(PSTREAMS_INCLUDE_PATH NAMES pstream.h
+FIND_PATH(PSTREAMS_INCLUDE_DIR NAMES pstream.h
           PATH_SUFFIXES pstreams
           HINTS ${PSTREAMS_ROOT}/include $ENV{PSTREAMS_ROOT})
+
+set(PSTREAMS_INCLUDE_DIRS ${PSTREAMS_INCLUDE_DIR})
 
 # Handle the QUIETLY and REQUIRED arguments and set PSTREAMS_FOUND to TRUE if
 # all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pstreams DEFAULT_MSG PSTREAMS_INCLUDE_PATH)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pstreams DEFAULT_MSG PSTREAMS_INCLUDE_DIRS)
 
-MARK_AS_ADVANCED(PSTREAMS_INCLUDE_PATH)
+MARK_AS_ADVANCED(PSTREAMS_INCLUDE_DIRS)
