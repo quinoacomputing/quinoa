68b7a36295b4c564c51307651aa03c3f62abf9f5
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-20 12:06:14 -0700

    Make FindExodiff.cmake conforming

--- cmake/FindExodiff.cmake
+++ cmake/FindExodiff.cmake
@@ -1,30 +1,32 @@
-# Copyright (C) 2016 Los Alamos Natl. Lab
+################################################################################
 #
-# This file was derived from FindGnuplot.cmake shipped with CMake 2.6.3.
+# \file      cmake/FindExodiff.cmake
+# \author    J. Bakosi
+# \copyright 2016, Los Alamos National Security, LLC.
+# \brief     Find the Adaptive Entropy Coding library
+# \date      Fri 20 Jan 2017 12:01:56 PM MST
 #
-# - this module looks for exodiff
+################################################################################
+
+# Find the exodiff executable part of the ExodusII / Trilinos/SEACAS package
 #
-# Once done this will define
+#  Exodiff_FOUND      - True if the exodiff executable was found
+#  EXODIFF_EXECUTABLE - The exodiff executable
 #
-#  Exodiff_FOUND - system has exodiff
-#  Exodiff_EXECUTABLE - the exodiff executable
+#  Set EXODUS_ROOT before calling find_package to a path to add an additional
+#  search path, e.g.,
 #
-#=============================================================================
-# Copyright 2002-2009 Kitware, Inc.
+#  Usage:
 #
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distributed this file outside of CMake, substitute the full
-#  License text for the above reference.)
+#  set(EXODUS_ROOT "/path/to/custom/exodus") # prefer over system
+#  find_package(Exodiff)
+#  if(Exodiff_FOUND)
+#    # use ${EXODIFF_EXECUTABLE} ...
+#  endif()
 
 INCLUDE(FindCygwin)
 
-FIND_PROGRAM(Exodiff_EXECUTABLE
+FIND_PROGRAM(EXODIFF_EXECUTABLE
   NAMES exodiff
   PATHS ${CYGWIN_INSTALL_PATH}/bin ${EXODUS_ROOT}/bin $ENV{EXODUS_ROOT}/bin
 )
@@ -32,11 +34,6 @@ FIND_PROGRAM(Exodiff_EXECUTABLE
 # handle the QUIETLY and REQUIRED arguments and set Exodiff_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Exodiff DEFAULT_MSG Exodiff_EXECUTABLE)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Exodiff DEFAULT_MSG EXODIFF_EXECUTABLE)
 
-IF(NOT Exodiff_FOUND)
-  message(STATUS "exodiff not found, help cmake to find it by setting Exodiff_EXECUTABLE")
-ENDIF(NOT Exodiff_FOUND)
-
-MARK_AS_ADVANCED( Exodiff_EXECUTABLE )
-
+MARK_AS_ADVANCED(EXODIFF_EXECUTABLE)
