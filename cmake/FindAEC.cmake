f476c17e719c230812f2062d1a395800e3960318
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-20 11:22:25 -0700

    Make FindAEC.cmake conforming

--- cmake/FindAEC.cmake
+++ cmake/FindAEC.cmake
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the Adaptive Entropy Coding library
-# \date      Fri 06 May 2016 06:41:51 AM MDT
+# \date      Fri 20 Jan 2017 11:21:02 AM MST
 #
 ################################################################################
 
@@ -12,8 +12,8 @@
 # https://www.dkrz.de/redmine/projects/aec/wiki
 #
 #  AEC_FOUND - System has AEC
-#  AEC_INCLUDE - The AEC include directory
-#  AEC_LIBRARY - The libraries needed to use AEC
+#  AEC_INCLUDE_DIRS - The AEC include directory
+#  AEC_LIBRARIES - The libraries needed to use AEC
 #
 #  Set AEC_ROOT before calling find_package to a path to add an additional
 #  search path, e.g.,
@@ -23,11 +23,11 @@
 #  set(AEC_ROOT "/path/to/custom/aec") # prefer over system
 #  find_package(AEC)
 #  if(AEC_FOUND)
-#    target_link_libraries (TARGET ${AEC_LIBRARY})
+#    target_link_libraries (TARGET ${AEC_LIBRARIES})
 #  endif()
 
 # If already in cache, be silent
-if(AEC_INCLUDE AND AEC_LIBRARY)
+if(AEC_INCLUDE_DIRS AND AEC_LIBRARY)
   set (AEC_FIND_QUIETLY TRUE)
 endif()
 
@@ -39,11 +39,12 @@ else()
   find_library(AEC_LIBRARY NAMES aec HINTS ${AEC_ROOT}/lib)
 endif()
 
-set(AEC_LIBRARY ${AEC_LIBRARY})
+set(AEC_INCLUDE_DIRS ${AEC_INCLUDE})
+set(AEC_LIBRARIES ${AEC_LIBRARY})
 
 # Handle the QUIETLY and REQUIRED arguments and set AEC_FOUND to TRUE if
 # all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(AEC DEFAULT_MSG AEC_LIBRARY AEC_INCLUDE)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(AEC DEFAULT_MSG AEC_LIBRARIES AEC_INCLUDE_DIRS)
 
-MARK_AS_ADVANCED(AEC_INCLUDE AEC_LIBRARY)
+MARK_AS_ADVANCED(AEC_INCLUDE_DIRS AEC_LIBRARIES)
