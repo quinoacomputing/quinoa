187d32de268e478a11d0d93068a70020beaa43e8
Jozsef Bakosi <jbakosi@lanl.gov>
2017-01-24 16:51:09 -0700

    Re-expose individual MKL libs
    This allows a more fine-grained control over the link line to resolve
    all symbols due to cyclic dependencing among the MKL libs, especially
    with static linking on crays.

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Find the Math Kernel Library from Intel
-# \date      Fri 20 Jan 2017 01:07:39 PM MST
+# \date      Thu 26 Jan 2017 02:05:50 PM MST
 #
 ################################################################################
 
@@ -13,6 +13,9 @@
 #  MKL_FOUND - System has MKL
 #  MKL_INCLUDE_DIRS - MKL include files directories
 #  MKL_LIBRARIES - The MKL libraries
+#  MKL_INTERFACE_LIBRARY - MKL interface library
+#  MKL_SEQUENTIAL_LAYER_LIBRARY - MKL sequential layer library
+#  MKL_CORE_LIBRARY - MKL core library
 #
 #  The environment variables MKLROOT and INTEL are used to find the library.
 #  Everything else is ignored. If MKL is found "-DMKL_ILP64" is added to
@@ -26,7 +29,8 @@
 #  endif()
 
 # If already in cache, be silent
-if (MKL_INCLUDE_DIRS AND MKL_LIBRARIES)
+if (MKL_INCLUDE_DIRS AND MKL_LIBRARIES AND MKL_INTERFACE_LIBRARY AND
+    MKL_SEQUENTIAL_LAYER_LIBRARY AND MKL_CORE_LIBRARY)
   set (MKL_FIND_QUIETLY TRUE)
 endif()
 
@@ -88,12 +92,15 @@ else()
 
   set(MKL_INCLUDE_DIRS "")
   set(MKL_LIBRARIES "")
+  set(MKL_INTERFACE_LIBRARY "")
+  set(MKL_SEQUENTIAL_LAYER_LIBRARY "")
+  set(MKL_CORE_LIBRARY "")
 
 endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
 # all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS MKL_INTERFACE_LIBRARY MKL_SEQUENTIAL_LAYER_LIBRARY MKL_CORE_LIBRARY)
 
-MARK_AS_ADVANCED(MKL_INCLUDE_DIRS MKL_LIBRARIES)
+MARK_AS_ADVANCED(MKL_INCLUDE_DIRS MKL_LIBRARIES MKL_INTERFACE_LIBRARY MKL_SEQUENTIAL_LAYER_LIBRARY MKL_CORE_LIBRARY)
