481db7167e04ba543564a1b63d4107c5f514b186
Jozsef Bakosi <jbakosi@lanl.gov>
2017-03-03 11:51:00 -0700

    Only enable code coverage if explicitly requested
    Previously, we enabled code coverage if all prerequisites have been
    found. Now we only enable it if it is explicitly requested by setting
    the cmake variable COVERAGE, which is unset by default.

--- cmake/DetectCodeCoverage.cmake
+++ cmake/DetectCodeCoverage.cmake
@@ -4,7 +4,7 @@
 # \author    J. Bakosi
 # \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
 # \brief     Detect prerequesites for code coverage analysis
-# \date      Thu 02 Mar 2017 10:30:26 AM MST
+# \date      Fri 03 Mar 2017 11:50:24 AM MST
 #
 ################################################################################
 
@@ -20,18 +20,19 @@ find_program( LCOV lcov )
 find_program( GENHTML genhtml )
 find_program( SED sed )
 
-# Code coverage analysis only supported if all prerequisites (gcov, lcov,
-# genhtml) found using the GNU compiler and only on debug builds.
-if ( GCOV AND
+# Code coverage analysis only supported if all prerequisites found and the user
+# has requested it via the cmake variable COVERAGE=on..
+if ( COVERAGE AND
+     GCOV AND
      LCOV AND
      GENHTML AND
      SED AND
      ( CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR
        CMAKE_CXX_COMPILER_ID STREQUAL "GNU" ) )
 
-  message(STATUS "Code coverage analysis enabled: compiler:${CMAKE_CXX_COMPILER_ID}, build:${CMAKE_BUILD_TYPE}, gcov:${GCOV}, lcov:${LCOV}, genhtml:${GENHTML}, sed:${SED}")
+  message(STATUS "Code coverage analysis enabled: All prerequisites found: COVERAGE:${COVERAGE}, compiler:${CMAKE_CXX_COMPILER_ID}, gcov:${GCOV}, lcov:${LCOV}, genhtml:${GENHTML}, sed:${SED}")
 
-  # Enable code coverage analysis
+  # Enable code coverage analysis.
   SET(CODE_COVERAGE ON)
 
   # Make flag enabling code coverage analysis available in parent cmake scope
@@ -42,6 +43,6 @@ if ( GCOV AND
 
 else()
 
-  message(STATUS "Code coverage analysis disabled. Not all prerequisites found: compiler:${CMAKE_CXX_COMPILER_ID} (only supported with GNU and Clang), build:${CMAKE_BUILD_TYPE} (only supported with DEBUG), gcov:${GCOV}, lcov:${LCOV}, genhtml:${GENHTML}, sed:${SED}")
+  message(STATUS "Code coverage analysis disabled. Not all prerequisites found: COVERAGE:${COVERAGE}, compiler:${CMAKE_CXX_COMPILER_ID}, gcov:${GCOV}, lcov:${LCOV}, genhtml:${GENHTML}, sed:${SED}")
 
 endif()
