0140ad8f108bb75f551a99f1cf50604152598fe7
Jozsef Bakosi <jbakosi@lanl.gov>
2017-04-04 09:27:04 -0600

    Fix mac build class name

--- .teamcity/Quinoa_2_Mac/buildTypes/Quinoa_2_Mac_Build.kt
+++ .teamcity/Quinoa_2_Mac/buildTypes/Quinoa_2_Mac_Build.kt
@@ -3,12 +3,12 @@ package Quinoa_2_Mac.buildTypes
 import Quinoa_2_Mac.buildParams.*
 import jetbrains.buildServer.configs.kotlin.v10.*
 
-class Quinoa_2_Mac_MacBuild( bp: BuildParams ) : BuildType({
+class Quinoa_2_Mac_Build( bp: BuildParams ) : BuildType({
 
     template(Quinoa_2_Mac.buildTypes.Quinoa_2_Mac_Matrix)
 
     uuid = "7df011b4-4795-4b89-9d03-aba1b1cb53f7_$paramToId"
-    extId = "Quinoa_2_Mac_MacBuild_$paramToId"
+    extId = "Quinoa_2_Mac_Build_$paramToId"
     name = "${bp.buildtype.toString()}, ${bp.compiler.toString()}"
     description = "Mac matrix build instance"
 
