59481df187dbaa897035bd8e2a3755578aff864a
Robert Pavel <rspavel@lanl.gov>
2017-04-07 09:05:34 -0600

    Resolved MPI Error Checking without MPI
    As per Issue #55:
    Removed MPI-based error checker's dependency on mandatory MPI dependency while
    maintaining same structure of existing macro.

--- src/Base/ExceptionMPI.h
+++ src/Base/ExceptionMPI.h
@@ -47,6 +47,7 @@ namespace tk {
 //!    executed on multiple MPI ranks. Within asynchrounous Charm++ chares, the
 //!    simpler ErrChk macro suffices.
 //! \author J. Bakosi
+#ifdef QUINOA_CONFIG_MPI_ENABLED
 #define ErrChkMPI(expr, ...) \
 { \
   int err = (expr) ? 0 : 1; \
@@ -54,6 +55,13 @@ namespace tk {
   MPI_Allreduce( &err, &globalerr, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD ); \
   if ( globalerr ) Throw( __VA_ARGS__); \
 }
+#else
+#define ErrChkMPI(expr, ...)\
+{\
+  int QUINOA_ErrChkMPI_err = (expr) ? 0 : 1; \
+  if ( QUINOA_ErrChkMPI_err ) Throw( __VA_ARGS__); \
+}
+#endif
 
 } // tk::
 
