fe0bd65faec8ddb24d1f6bd69753a0f4729e57d4
Jozsef Bakosi <jbakosi@lanl.gov>
2017-04-19 19:08:29 -0600

    Update teamcity module setup, now including those for intel builds

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -20,15 +20,16 @@ object Quinoa_2_Linux_Matrix : Template({
 
     val stepPrefix = """
       . ${'$'}SPACK_ROOT/share/spack/setup-env.sh
-      [ %compiler% == clang ] && module load clang/latest openmpi/1.10.2/clang/latest
-      [ %compiler% == gnu ] && module load openmpi-2.0.1-gcc-4.8.5-jv7w2de
-      [ %compiler% == intel ] && module load intel/2018 openmpi/1.10.2/intel/2018
+      [ %compiler% == clang ] && module load clang/latest openmpi/1.10.2/clang/latest hdf5-1.10.0-patch1-gcc-4.8.5-mmtlfty netcdf-4.4.1-gcc-4.8.5-5xen4a5 hypre-2.10.1-gcc-4.8.5-beuxbxv
+      [ %compiler% == gnu ] && module load openmpi-2.0.1-gcc-4.8.5-jv7w2de hdf5-1.10.0-patch1-gcc-4.8.5-mmtlfty netcdf-4.4.1-gcc-4.8.5-5xen4a5 hypre-2.10.1-gcc-4.8.5-beuxbxv
+      [ %compiler% == intel ] && module load intel/2018 mpi hdf5/intel netcdf/intel hypre/intel
       [ %mathlib% == mkl ] && module load mkl/2018
-      [ %mathlib% == lapack ] && module load netlib-lapack-3.6.1-gcc-4.8.5-snwxnfw
+      [[ %mathlib% == lapack && %compiler% == intel ]] && module load lapack/intel
+      [[ %mathlib% == lapack && %compiler% != intel ]] && module load netlib-lapack-3.6.1-gcc-4.8.5-snwxnfw
       [ %rngsse2% == true ] && module load rngsse2
       [ %testu01% == true ] && module load testu01
-      module load charm/%compiler%-%stdlibcpp% h5part/%compiler%-%stdlibcpp% trilinos/%compiler%-%stdlibcpp%/%mathlib%
-      module load pugixml pegtl pstreams boost-1.61.0-gcc-4.8.5-q2hywin hypre-2.10.1-gcc-4.8.5-beuxbxv gmsh-2.12.0-gcc-4.8.5-p3vpjfb hdf5-1.10.0-patch1-gcc-4.8.5-mmtlfty netcdf-4.4.1-gcc-4.8.5-5xen4a5 random123 tut cartesian_product numdiff libc++
+      module load charm/%compiler%-%stdlibcpp% h5part/%compiler% trilinos/%compiler%-%stdlibcpp%/%mathlib%
+      module load pugixml pegtl pstreams boost-1.61.0-gcc-4.8.5-q2hywin gmsh-2.12.0-gcc-4.8.5-p3vpjfb random123 tut cartesian_product numdiff libc++
       module list""".trimIndent()
 
     steps {
