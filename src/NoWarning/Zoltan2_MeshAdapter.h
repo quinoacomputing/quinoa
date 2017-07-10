e3aae0f627c82da85f77a7be3c25436580d282b2
Jozsef Bakosi <jbakosi@lanl.gov>
2017-07-10 11:30:58 -0600

    Revert "Turn off warnings for gcc 7"
    This reverts commit f5af5866118d4a4db09dbb030cfe3a246006d21e. Older gcc
    versions don't like this.

--- src/NoWarning/Zoltan2_MeshAdapter.h
+++ src/NoWarning/Zoltan2_MeshAdapter.h
@@ -46,7 +46,6 @@
   #pragma GCC diagnostic ignored "-Wsuggest-attribute=noreturn"
   #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
   #pragma GCC diagnostic ignored "-Wshadow"
-  #pragma GCC diagnostic ignored "-Wint-in-bool-context"
 #elif defined(__INTEL_COMPILER)
   #pragma warning( push )
   #pragma warning( disable: 239 )
