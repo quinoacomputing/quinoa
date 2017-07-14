0ad0d11e2bc55cd45363ede19e663303ff0c574d
Jozsef Bakosi <jbakosi@lanl.gov>
2017-07-14 07:13:43 -0600

    Prepare copyright update script for next year

--- script/update_copyright.sh
+++ script/update_copyright.sh
@@ -1,11 +1,11 @@
 ################################################################################
 #
 # \file      script/update_copyright.sh
-# \copyright 2012-2015, Jozsef Bakosi, 2016, Los Alamos National Security, LLC.
+# \copyright 2012-2015, J. Bakosi, 2016-2017, Los Alamos National Security, LLC.
 # \brief     Switch copyright year in all files
 #
 ################################################################################
 
-find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/opyright 2012-2015, Jozsef Bakosi, 2016/opyright 2012-2015, J. Bakosi, 2016-2017/g'
-find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright (c) 2016/Copyright (c) 2017/g'
-find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright 2016/Copyright 2017/g'
+find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/opyright 2012-2015, J. Bakosi, 2016-2017/opyright 2012-2015, J. Bakosi, 2016-2018/g'
+find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright (c) 2016-2017/Copyright (c) 2016-2018/g'
+find <dirs> -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright 2016-2017/Copyright 2016-2018/g'
