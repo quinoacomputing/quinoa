70b4ad72854a45f5fb641fee5e225990b2f1b9ac
Jozsef Bakosi <jbakosi@lanl.gov>
2017-08-18 15:11:36 -0600

    Latest on mixmassfracbeta

--- src/DiffEq/MixMassFractionBetaCoeffPolicy.h
+++ src/DiffEq/MixMassFractionBetaCoeffPolicy.h
@@ -570,12 +570,12 @@ class MixMassFracBetaCoeffHydroTimeScaleHomDecay {
         // latest: tk::real mix = 1.0+r[c]*yt;
 
         if (m_it == 0) m_s.push_back( S[c] );
-        tk::real f = 1.0;
+        //tk::real f = 1.0;
         //tk::real f = 1.0 / (1.0+r[c]);
         //tk::real f = std::pow(theta,0.5);
         //tk::real f = (1.0+A)*theta/(1.0+30.0*A*theta);
         //tk::real f = 1.0 / ( 1.0 + std::pow(pe-1.0,2.0)*std::sqrt(d2/d/d) );
-        //tk::real f = 1.0 / std::pow( 1.0 + std::pow(pe-1.0,2.0), 0.5 );//*std::pow(ds,0.5) );
+        tk::real f = 1.0 / std::pow( 1.0 + std::pow(pe-1.0,2.0), 0.5 );
         //tk::real f = std::pow(1.0 / std::sqrt( 1.0 + std::pow(pe-1.0,2.0) ), 2.0);
         //tk::real f = theta / std::sqrt( 1.0 + std::pow(pe-1.0,2.0) );
         //tk::real f = std::pow(theta,0.5) / std::sqrt( 1.0 + std::pow(pe-1.0,2.0) );
@@ -645,13 +645,16 @@ class MixMassFracBetaCoeffHydroTimeScaleHomDecay {
         //tk::real beta3 = c0 * (d2/d/d/ds) / (1.0+ds) * (1.0+d2/d/d);
         //tk::real beta3 = c0 * d2/d/d/ds / (1.0+ds) / (1.0 + d2/d/d + d2/d/d/ds) * (1.0+d2/d/d);
         //tk::real beta2 = 1.0 / (1.0+ds);
-        tk::real beta2 = d2/d/d/ds/(1.0+ds);
+        tk::real beta2 = 1.0; //d2/d/d/ds/(1.0+ds);
         //tk::real beta3 = c0*c1 * d2/d/d/ds / (1.0+ds) * (1.0+r[c])/(1.0+r[c]*yt);
         //tk::real beta3 = c0*c1 * (1.0+ds)/(1.0+d2/d/d) * (1.0+r[c])/(1.0+r[c]*yt);
-        tk::real eta = (1.0+d2/d/d);
-        tk::real beta3 = c0*c1 ;//* eta * (1.0+r[c])/(1.0+r[c]*yt);
+        tk::real eta = d2/d/d/ds;
+        //tk::real Thetap = thetab*0.5*(1.0+eta/(1.0+eta*ds));
+        tk::real Thetap = thetab*eta/(1.0+ds);
+        tk::real beta3 = 4.0*c0*c1;
+        tk::real beta10 = 1.1*(1.0+ds)/(1.0+d2/d/d);
         tk::real beta1 = bprime[c] / (1.0 + d2/d/d + d2/d/d/ds) *
-                         (1.0 + beta2*thetab*f + beta3*thetab*(1.0-thetab)*f);
+                         (beta10 + beta2*Thetap*f + beta3*Thetap*(1.0-Thetap)*f);
         b[c] = beta1 * ts;
         // cleanup of current baseline:
         k[c] = kprime[c] * beta1 * ts * ds;
