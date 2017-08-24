f011b33f07d60a74725ee971350137ede369344f
Jozsef Bakosi <jbakosi@lanl.gov>
2017-08-24 11:28:43 -0600

    Latest on mixmassfracbeta

--- src/DiffEq/MixMassFractionBetaCoeffPolicy.h
+++ src/DiffEq/MixMassFractionBetaCoeffPolicy.h
@@ -651,8 +651,8 @@ class MixMassFracBetaCoeffHydroTimeScaleHomDecay {
         tk::real eta = d2/d/d/ds;
         //tk::real Thetap = thetab*0.5*(1.0+eta/(1.0+eta*ds));
         tk::real Thetap = thetab*eta/(1.0+ds);
-        tk::real beta3 = 4.0*c0*c1;
-        tk::real beta10 = 1.1*(1.0+ds)/(1.0+d2/d/d);
+        tk::real beta3 = c1;
+        tk::real beta10 = c0*(1.0+ds)/(1.0+d2/d/d);
         tk::real beta1 = bprime[c] / (1.0 + d2/d/d + d2/d/d/ds) *
                          (beta10 + beta2*Thetap*f + beta3*Thetap*(1.0-Thetap)*f);
         b[c] = beta1 * ts;
