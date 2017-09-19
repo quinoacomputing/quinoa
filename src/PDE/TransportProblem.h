dd7f43ce0a9b78276466e77e8cd35cf989155669
Jozsef Bakosi <jbakosi@lanl.gov>
2017-09-19 08:38:16 -0600

    Re-enable slotted cylinder test case
    By defining side() and dirbc(). Appears correct with multiple
    components.

--- src/PDE/TransportProblem.h
+++ src/PDE/TransportProblem.h
@@ -280,6 +280,93 @@ class TransportProblemShearDiff {
 //!   627-665.
 class TransportProblemSlotCyl {
 
+  private:
+    //! Evaluate analytical solution at (x,y,t) for all components
+    //! \param[in] x X coordinate where to evaluate the solution
+    //! \param[in] y Y coordinate where to evaluate the solution
+    //! \param[in] t Time where to evaluate the solution
+    //! \return Values of all components evaluated at (x,y,t)
+    static std::vector< tk::real >
+    solution( tk::ctr::ncomp_type ncomp,
+              tk::real x, tk::real y, tk::real t )
+    {
+      using std::sin; using std::cos;
+      std::vector< tk::real > s( ncomp, 0.0 );
+      for (ncomp_t c=0; c<ncomp; ++c) {
+        auto T = t + 2.0*M_PI/ncomp * c;
+        const tk::real R0 = 0.15;
+        // center of the cone
+        tk::real x0 = 0.5;
+        tk::real y0 = 0.25;
+        tk::real r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
+        tk::real kx = 0.5 + r*sin( T );
+        tk::real ky = 0.5 - r*cos( T );
+        // center of the hump
+        x0 = 0.25;
+        y0 = 0.5;
+        r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
+        tk::real hx = 0.5 + r*sin( T-M_PI/2.0 ),
+                 hy = 0.5 - r*cos( T-M_PI/2.0 );
+        // center of the slotted cylinder
+        x0 = 0.5;
+        y0 = 0.75;
+        r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
+        tk::real cx = 0.5 + r*sin( T+M_PI ),
+                 cy = 0.5 - r*cos( T+M_PI );
+        // end points of the cylinder slot
+        tk::real i1x = 0.525, i1y = cy - r*cos( std::asin(0.025/r) ),
+                 i2x = 0.525, i2y = 0.8,
+                 i3x = 0.475, i3y = 0.8;
+        // rotate end points of cylinder slot
+        tk::real ri1x = 0.5 + cos(T)*(i1x-0.5) - sin(T)*(i1y-0.5),
+                 ri1y = 0.5 + sin(T)*(i1x-0.5) + cos(T)*(i1y-0.5),
+                 ri2x = 0.5 + cos(T)*(i2x-0.5) - sin(T)*(i2y-0.5),
+                 ri2y = 0.5 + sin(T)*(i2x-0.5) + cos(T)*(i2y-0.5),
+                 ri3x = 0.5 + cos(T)*(i3x-0.5) - sin(T)*(i3y-0.5),
+                 ri3y = 0.5 + sin(T)*(i3x-0.5) + cos(T)*(i3y-0.5);
+        // direction of slot sides
+        tk::real v1x = ri2x-ri1x, v1y = ri2y-ri1y,
+                 v2x = ri3x-ri2x, v2y = ri3y-ri2y;
+        // lengths of direction of slot sides vectors
+        tk::real v1 = std::sqrt(v1x*v1x + v1y*v1y),
+                 v2 = std::sqrt(v2x*v2x + v2y*v2y);
+        // cone
+        r = std::sqrt((x-kx)*(x-kx) + (y-ky)*(y-ky)) / R0;
+        if (r<1.0) s[c] = 0.6*(1.0-r);
+        // hump
+        r = std::sqrt((x-hx)*(x-hx) + (y-hy)*(y-hy)) / R0;
+        if (r<1.0) s[c] = 0.2*(1.0+cos(M_PI*std::min(r,1.0)));
+        // cylinder
+        r = std::sqrt((x-cx)*(x-cx) + (y-cy)*(y-cy)) / R0;
+        const std::array< tk::real, 2 > r1{{ v1x, v1y }},
+                                        r2{{ x-ri1x, y-ri1y }};
+        const auto d1 = (r1[0]*r2[1] - r2[0]*r1[1]) / v1;
+        const std::array< tk::real, 2 > r3{{ v2x, v2y }},
+                                        r4{{ x-ri2x, y-ri2y }};
+        const auto d2 = (r3[0]*r4[1] - r4[0]*r3[1]) / v2;
+        if (r<1.0 && (d1>0.05 || d1<0.0 || d2<0.0)) s[c] = 0.6;
+      }
+      return s;
+    }
+
+    //! \brief Evaluate the increment from t to t+dt of the analytical solution
+    //!   at (x,y,z) for all components
+    //! \param[in] x X coordinate where to evaluate the solution
+    //! \param[in] y Y coordinate where to evaluate the solution
+    //! \param[in] t Time where to evaluate the solution increment starting from
+    //! \param[in] dt Time increment at which evaluate the solution increment to
+    //! \return Increment in values of all components evaluated at (x,y,t+dt)
+    static std::vector< tk::real >
+    solinc( tk::ctr::ncomp_type ncomp,
+            tk::real x, tk::real y, tk::real t, tk::real dt )
+    {
+      auto st1 = solution( ncomp, x, y, t );
+      auto st2 = solution( ncomp, x, y, t+dt );
+      std::transform( begin(st1), end(st1), begin(st2), begin(st2),
+                      []( tk::real s, tk::real& d ){ return d -= s; } );
+      return st2;
+    }
+
   public:
     //! Do error checking on PDE parameters
     static void errchk( tk::ctr::ncomp_type, tk::ctr::ncomp_type ) {}
@@ -290,76 +377,76 @@ class TransportProblemSlotCyl {
     //! \param[in] ncomp Number of components in this transport equation
     //! \param[in] offset System offset specifying the position of the system of
     //!   PDEs among other systems
-    //! \param[in] time Physical time
+    //! \param[in] t Physical time
     static void init( const std::array< std::vector< tk::real >, 3 >& coord,
                       tk::Fields& unk,
                       tk::ctr::ncomp_type,
                       tk::ctr::ncomp_type ncomp,
                       tk::ctr::ncomp_type offset,
-                      tk::real time )
+                      tk::real t )
     {
-      using std::sin; using std::cos;
       Assert( coord[0].size() == unk.nunk(), "Size mismatch" );
-      for (ncomp_t c=0; c<ncomp; ++c) {
-        auto t = time + 2.0*M_PI/ncomp * c;
-        const tk::real R0 = 0.15;
-        // center of the cone
-        tk::real x0 = 0.5;
-        tk::real y0 = 0.25;
-        tk::real r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
-        tk::real kx = 0.5 + r*sin( t );
-        tk::real ky = 0.5 - r*cos( t );
-        // center of the hump
-        x0 = 0.25;
-        y0 = 0.5;
-        r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
-        tk::real hx = 0.5 + r*sin( t-M_PI/2.0 ),
-                 hy = 0.5 - r*cos( t-M_PI/2.0 );
-        // center of the slotted cylinder
-        x0 = 0.5;
-        y0 = 0.75;
-        r = std::sqrt((x0-0.5)*(x0-0.5) + (y0-0.5)*(y0-0.5));
-        tk::real cx = 0.5 + r*sin( t+M_PI ),
-                 cy = 0.5 - r*cos( t+M_PI );
-        // end points of the cylinder slot
-        tk::real i1x = 0.525, i1y = cy - r*cos( std::asin(0.025/r) ),
-                 i2x = 0.525, i2y = 0.8,
-                 i3x = 0.475, i3y = 0.8;
-        // rotate end points of cylinder slot
-        tk::real ri1x = 0.5 + cos(t)*(i1x-0.5) - sin(t)*(i1y-0.5),
-                 ri1y = 0.5 + sin(t)*(i1x-0.5) + cos(t)*(i1y-0.5),
-                 ri2x = 0.5 + cos(t)*(i2x-0.5) - sin(t)*(i2y-0.5),
-                 ri2y = 0.5 + sin(t)*(i2x-0.5) + cos(t)*(i2y-0.5),
-                 ri3x = 0.5 + cos(t)*(i3x-0.5) - sin(t)*(i3y-0.5),
-                 ri3y = 0.5 + sin(t)*(i3x-0.5) + cos(t)*(i3y-0.5);
-        // direction of slot sides
-        tk::real v1x = ri2x-ri1x, v1y = ri2y-ri1y,
-                 v2x = ri3x-ri2x, v2y = ri3y-ri2y;
-        // lengths of direction of slot sides vectors
-        tk::real v1 = std::sqrt(v1x*v1x + v1y*v1y),
-                 v2 = std::sqrt(v2x*v2x + v2y*v2y);
+      const auto& x = coord[0];
+      const auto& y = coord[1];
+      for (ncomp_t i=0; i<x.size(); ++i) {
+        const auto s = solution( ncomp, x[i], y[i], t );
+        for (ncomp_t c=0; c<ncomp; ++c)
+          unk(i,c,offset) = s[c];
+      }
+    }
+
+    //! \brief Query all side set IDs the user has configured for all components
+    //!   in this PDE system
+    //! \param[in,out] conf Set of unique side set IDs to add to
+    static void side( std::unordered_set< int >& conf ) {
+      using tag::param; using tag::transport; using tag::bcdir;
+      for (const auto& s : g_inputdeck.get< param, transport, bcdir >())
+        for (const auto& i : s)
+          conf.insert( std::stoi(i) );
+    }
+
+    //! \brief Query Dirichlet boundary condition value on a given side set for
+    //!    all components in this PDE system
+    //! \param[in] e Equation system index, i.e., which compressible
+    //!   flow equation system we operate on among the systems of PDEs
+    //! \param[in] ncomp Number of components in this transport equation
+    //! \param[in] t Physical time
+    //! \param[in] deltat Time step size
+    //! \param[in] side Pair of side set ID and node IDs on the side set
+    //! \param[in] coord Mesh node coordinates
+    //! \return Vector of pairs of bool and boundary condition value associated
+    //!   to mesh node IDs at which Dirichlet boundary conditions are set. Note
+    //!   that instead of the actual boundary condition value, we return the
+    //!   increment between t+dt and t, since that is what the solution requires
+    //!   as we solve for the soution increments and not the solution itself.
+    static std::unordered_map< std::size_t,
+                               std::vector< std::pair< bool, tk::real > > >
+    dirbc( tk::ctr::ncomp_type e,
+           tk::ctr::ncomp_type ncomp,
+           tk::real t,
+           tk::real deltat,
+           const std::pair< const int, std::vector< std::size_t > >& side,
+           const std::array< std::vector< tk::real >, 3 >& coord )
+    {
+      using tag::param; using tag::transport; using tag::bcdir;
+      using NodeBC = std::vector< std::pair< bool, tk::real > >;
+      std::unordered_map< std::size_t, NodeBC > bc;
+      const auto& ubc = g_inputdeck.get< param, transport, bcdir >();
+      if (!ubc.empty()) {
+        Assert( ubc.size() > e, "Indexing out of Dirichlet BC eq-vector" );
         const auto& x = coord[0];
         const auto& y = coord[1];
-        unk.fill( c, offset, 0.0 );
-        for (ncomp_t i=0; i<x.size(); ++i) {
-          // cone
-          r = std::sqrt((x[i]-kx)*(x[i]-kx) + (y[i]-ky)*(y[i]-ky)) / R0;
-          if (r<1.0) unk( i, c, offset ) = 0.6*(1.0-r);
-          // hump
-          r = std::sqrt((x[i]-hx)*(x[i]-hx) + (y[i]-hy)*(y[i]-hy)) / R0;
-          if (r<1.0)
-            unk( i, c, offset ) = 0.2*(1.0+cos(M_PI*std::min(r,1.0)));
-          // cylinder
-          r = std::sqrt((x[i]-cx)*(x[i]-cx) + (y[i]-cy)*(y[i]-cy)) / R0;
-          const std::array< tk::real, 2 > r1{{ v1x, v1y }},
-                                          r2{{ x[i]-ri1x, y[i]-ri1y }};
-          const auto d1 = (r1[0]*r2[1] - r2[0]*r1[1]) / v1;
-          const std::array< tk::real, 2 > r3{{ v2x, v2y }},
-                                          r4{{ x[i]-ri2x, y[i]-ri2y }};
-          const auto d2 = (r3[0]*r4[1] - r4[0]*r3[1]) / v2;
-          if (r<1.0 && (d1>0.05 || d1<0.0 || d2<0.0)) unk( i, c, offset ) = 0.6;
-        }
+        for (const auto& b : ubc[e])
+          if (std::stoi(b) == side.first)
+            for (auto n : side.second) {
+              Assert( x.size() > n, "Indexing out of coordinate array" );
+              auto s = solinc( ncomp, x[n], y[n], t, deltat );
+              auto& nbc = bc[n] = NodeBC( ncomp );
+              for (ncomp_t c=0; c<ncomp; ++c)
+                nbc[c] = { true, s[c] };
+            }
       }
+      return bc;
     }
 
     //! Assign prescribed shear velocity to nodes of tetrahedron element
@@ -401,7 +488,7 @@ class TransportProblemSlotCyl {
 
 //! List of all transport equation problem policies
 using TransportProblems = boost::mpl::vector< TransportProblemShearDiff
-                                            //, TransportProblemSlotCyl
+                                            , TransportProblemSlotCyl
                                             >;
 
 } // inciter::
