b02c050b5aa5ff5a477f175807a5520085ad0811
Jozsef Bakosi <jbakosi@lanl.gov>
2018-01-18 09:36:59 -0700

    Update copyright year in src

--- src/PDE/Transport/Transport.h
+++ src/PDE/Transport/Transport.h
@@ -1,7 +1,7 @@
 // *****************************************************************************
 /*!
   \file      src/PDE/Transport.h
-  \copyright 2012-2015, J. Bakosi, 2016-2017, Los Alamos National Security, LLC.
+  \copyright 2012-2015, J. Bakosi, 2016-2018, Los Alamos National Security, LLC.
   \brief     Governing equations describing transport of scalars
   \details   This file implements the physics operators governing transported
      scalars.
