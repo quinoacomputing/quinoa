7b0ba0efb575a4d4ed396a0750591164d5310eb2
Jozsef Bakosi <jbakosi@lanl.gov>
2018-01-25 09:22:57 -0700

    Simplify based on suggestions by @junghans

--- script/update_copyright.sh
+++ script/update_copyright.sh
@@ -12,16 +12,11 @@
 
 # Require at least one argument
 die () { echo >&2 "$@"; exit 1; }
-[ "$#" -eq 0 ] && die "Usage: $0 <directories>"
+[ -z $1 ] && die "Usage: $0 <directories>"
 
-declare -a dirs=( "$@" )
+find "$@" -type f -not -name update_copyright.sh -exec sed -i 's/opyright 2012-2015, J. Bakosi, 2016-2017/opyright 2012-2015, J. Bakosi, 2016-2018/g' {} +
+find "$@" -type f -not -name update_copyright.sh -exec sed -i 's/Copyright (c) 2016-2017/Copyright (c) 2016-2018/g' {} +
+find "$@" -type f -not -name update_copyright.sh -exec sed -i 's/Copyright 2016-2017/Copyright 2016-2018/g' {} +
 
-for i in "${dirs[@]}"
-do
-  find $i -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | grep -v update_copyright.sh | xargs sed -i 's/opyright 2012-2015, J. Bakosi, 2016-2017/opyright 2012-2015, J. Bakosi, 2016-2018/g'
-  find $i -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | grep -v update_copyright.sh | xargs sed -i 's/Copyright (c) 2016-2017/Copyright (c) 2016-2018/g'
-  find $i -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | grep -v update_copyright.sh | xargs sed -i 's/Copyright 2016-2017/Copyright 2016-2018/g'
-done
-
-find LICENSE -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright (c) 2016-2017/Copyright (c) 2016-2018/g'
-find LICENSE -type f -exec grep -Iq . {} \; -and -print | cut -f1 -d: | xargs sed -i 's/Copyright 2016-2017/Copyright 2016-2018/g'
+find LICENSE -type f -exec sed -i 's/Copyright (c) 2016-2017/Copyright (c) 2016-2018/g' {} +
+find LICENSE -type f -exec sed -i 's/Copyright 2016-2017/Copyright 2016-2018/g' {} +
