a21334c56ba4ba0d4db133edf7f576c57bbed937
Jozsef Bakosi <jbakosi@lanl.gov>
2018-04-30 11:49:18 -0600

    Locally ignore intel compiler warning

--- src/NoWarning/unsmeshmap.decl.h
+++ src/NoWarning/unsmeshmap.decl.h
@@ -61,6 +61,7 @@
   #pragma warning( disable: 181 )
   #pragma warning( disable: 1720 )
   #pragma warning( disable: 1125 )
+  #pragma warning( disable: 2282 )
 #endif
 
 #include "../LoadBalance/unsmeshmap.decl.h"
