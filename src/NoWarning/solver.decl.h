cd82ba2d4746474e2986154f61cec5216df25697
Jozsef Bakosi <jbakosi@lanl.gov>
2018-04-30 11:42:34 -0600

    Locally ignore intel compiler warning

--- src/NoWarning/solver.decl.h
+++ src/NoWarning/solver.decl.h
@@ -59,6 +59,7 @@
   #pragma warning( disable: 181 )
   #pragma warning( disable: 1720 )
   #pragma warning( disable: 1125 )
+  #pragma warning( disable: 2282 )
 #endif
 
 #include "../LinSys/solver.decl.h"
