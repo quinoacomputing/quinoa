2b93d0f8686b3a20fc25b04eab1506cd8415a12d
Jozsef Bakosi <jbakosi@lanl.gov>
2018-10-29 07:42:21 -0600

    Squashed 'cmake/' changes from 48350f56..d4210360
    d4210360 Allow finding MKL on cmake's default paths
    
    git-subtree-dir: cmake
    git-subtree-split: d4210360cd06779a784cd58b477b57a28ef038c5

--- FindMKL.cmake
+++ FindMKL.cmake
@@ -44,28 +44,27 @@ else()
   set(COR_LIB "mkl_core")
 endif()
 
-find_path(MKL_INCLUDE_DIR NAMES mkl.h HINTS $ENV{MKLROOT}/include)
+find_path(MKL_INCLUDE_DIR NAMES mkl.h
+                          HINTS $ENV{MKLROOT}/include
+                          PATH_SUFFIXES mkl)
 
 find_library(MKL_INTERFACE_LIBRARY
              NAMES ${INT_LIB}
              PATHS $ENV{MKLROOT}/lib
                    $ENV{MKLROOT}/lib/intel64
-                   $ENV{INTEL}/mkl/lib/intel64
-             NO_DEFAULT_PATH)
+                   $ENV{INTEL}/mkl/lib/intel64)
 
 find_library(MKL_SEQUENTIAL_LAYER_LIBRARY
              NAMES ${SEQ_LIB}
              PATHS $ENV{MKLROOT}/lib
                    $ENV{MKLROOT}/lib/intel64
-                   $ENV{INTEL}/mkl/lib/intel64
-             NO_DEFAULT_PATH)
+                   $ENV{INTEL}/mkl/lib/intel64)
 
 find_library(MKL_CORE_LIBRARY
              NAMES ${COR_LIB}
              PATHS $ENV{MKLROOT}/lib
                    $ENV{MKLROOT}/lib/intel64
-                   $ENV{INTEL}/mkl/lib/intel64
-             NO_DEFAULT_PATH)
+                   $ENV{INTEL}/mkl/lib/intel64)
 
 set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR})
 set(MKL_LIBRARIES ${MKL_INTERFACE_LIBRARY} ${MKL_SEQUENTIAL_LAYER_LIBRARY} ${MKL_CORE_LIBRARY})
