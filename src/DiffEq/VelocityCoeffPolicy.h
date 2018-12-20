1f8f67d6f470de1ca7f3c7d2b84654a44984d53e
Jozsef Bakosi <jbakosi@lanl.gov>
2018-12-20 10:11:19 -0700

    walker: reuse code for velocity models and statistics

--- src/DiffEq/VelocityCoeffPolicy.h
+++ src/DiffEq/VelocityCoeffPolicy.h
@@ -68,80 +68,9 @@
 #include "Types.h"
 #include "SystemComponents.h"
 #include "Walker/Options/CoeffPolicy.h"
+#include "Langevin.h"
 
 namespace walker {
-
-//! Calculate the 2nd order tensor Gij based on the simplified Langevin model
-//! \param[in] hts Inverse hydrodynamics time scale, e.g., eps/k
-//! \param[in] C0 Coefficient C0 in SLM
-//! \return Tensor Gij computed based on the simplified Langevin model
-static inline std::array< tk::real, 9 >
-slm( tk::real hts, tk::real C0 ) {
-
-  std::array< tk::real, 9 > G;
-  G.fill( 0.0 );
-  G[0] = G[4] = G[8] = -(0.5+0.75*C0) * hts;
-
-  return G;
-}
-
-//! Calculate the 2nd order tensor Gij based on the generalized Langevin model
-//! \param[in] hts Inverse hydrodynamics time scale, e.g., eps/k
-//! \param[in] C0 Coefficient C0 in SLM
-//! \param[in] rs Reynolds stress
-//! \param[in] dU Mean velocity gradient
-//! \return Tensor Gij computed based on the simplified Langevin model
-static inline std::array< tk::real, 9 >
-glm( tk::real hts,
-     tk::real C0,
-     const std::array< tk::real, 6 >& rs,
-     const std::array< tk::real, 9 >& dU )
-{
-  // Generalized Langevion model coefficients
-  std::array< tk::real, 2 > ALPHA{{ -(0.5 + 0.75*C0), 3.7 }};
-  std::array< tk::real, 3 > BETA{{ -0.2, 0.8, -0.2 }};
-  std::array< tk::real, 6 > GAMMA{{ -1.28, 3.01, -2.18, 0.0, 4.29, -3.09 }};
-
-  // Compute Reynolds stress anisotropy
-  tk::real tr = rs[0] + rs[1] + rs[2];
-  std::array< tk::real, 9 > b{{ rs[0]/tr-1.0/3.0,
-                                rs[3]/tr,
-                                rs[4]/tr,
-                                rs[3]/tr,
-                                rs[1]/tr-1.0/3.0,
-                                rs[5]/tr,
-                                rs[4]/tr,
-                                rs[5]/tr,
-                                rs[2]/tr-1.0/3.0 }};
-  // Compute Gij
-  std::array< tk::real, 9 > G;
-  G.fill( 0.0 );
-  for (std::size_t i=0; i<3; ++i ) {
-    // to main diagonal: hts * ALPHA1 * delta_ij + BETA1 * deltaij * d<Ul>/dxl
-    G[i*3+i] += hts*ALPHA[0] + BETA[0]*(dU[0] + dU[4] + dU[8]);
-    // to main diagonal: GAMMA1 * deltaij * bml * d<Um>/dxl
-    tk::real dtmp = 0.0;
-    for (std::size_t m=0; m<3; ++m)
-      for (std::size_t l=0; l<3; ++l )
-         dtmp += b[m*3+l]*dU[m*3+l];
-    G[i*3+i] += GAMMA[0]*dtmp;
-    // to main and off-diagonal
-    for (std::size_t j=0; j<3; ++j) {
-      G[i*3+j] += hts*ALPHA[1]*b[i*3+j] +  // eps/k * ALPHA2 * bij
-                  BETA[1]*dU[i*3+j] +      // BETA2 * d<Ui>/dj
-                  BETA[2]*dU[j*3+i] +      // BETA3 * d<Uj>/di
-                  // GAMMA4 * bij * d<Ul>/dxl
-                  GAMMA[3]*b[i*3+j]*(dU[0] + dU[4] + dU[8]);
-      for (std::size_t l=0; l<3; ++l)
-        G[i*3+j] += GAMMA[1]*b[j*3+l]*dU[i*3+l] + // GAMMA2 * bjl * d<Ui>/dxl
-                    GAMMA[2]*b[j*3+l]*dU[l*3+i] + // GAMMA3 * bjl * d<Ul>/dxi
-                    GAMMA[4]*b[i*3+l]*dU[l*3+j] + // GAMMA5 * bil * d<Ul>/dxj
-                    GAMMA[5]*b[i*3+l]*dU[j*3+l];  // GAMMA6 * bil * d<Uj>/dxl
-    }
-  }
-
-  return G;
-}
 
 //! Velocity equation coefficients policy with prescribed mean shear
 //! \details C0 is user-defined and we prescibe a hard-coded mean shear in the x
@@ -185,50 +114,12 @@ class Velocity_ConstShear {
     {
       using tk::ctr::lookup;
       using tk::ctr::mean;
-      using tk::ctr::Product;
-
-      // Extract diagonal of the Reynolds stress
-      Product r11, r22, r33, r12, r13, r23;
-      if (solve == ctr::DepvarType::FULLVAR) {
-
-        using tk::ctr::variance;
-        using tk::ctr::covariance;
-        r11 = variance( depvar, 0 );
-        r22 = variance( depvar, 1 );
-        r33 = variance( depvar, 2 );
-        r12 = covariance( depvar, 0, depvar, 1 );
-        r13 = covariance( depvar, 0, depvar, 2 );
-        r23 = covariance( depvar, 1, depvar, 2 );
-
-      } else if (solve == ctr::DepvarType::FLUCTUATION) {
-
-        // Since we are solving for the fluctuating velocity, the "ordinary"
-        // moments, e.g., <U1U1>, are really central moments, i.e., <u1u1>.
-        using tk::ctr::Term;
-        using tk::ctr::Moment;
-        auto d = static_cast< char >( std::toupper( depvar ) );
-        Term u( d, 0, Moment::ORDINARY );
-        Term v( d, 1, Moment::ORDINARY );
-        Term w( d, 2, Moment::ORDINARY );
-        r11 = tk::ctr::Product( { u, u } );
-        r22 = tk::ctr::Product( { v, v } );
-        r33 = tk::ctr::Product( { w, w } );
-        r12 = tk::ctr::Product( { u, v } );
-        r13 = tk::ctr::Product( { u, w } );
-        r23 = tk::ctr::Product( { v, w } );
-
-      } else Throw( "Depvar type not implemented" );
-
-      // Compute nonzero components of the Reynolds stress
-      std::array< tk::real, 6 > rs{{ lookup(r11,moments),
-                                     lookup(r22,moments),
-                                     lookup(r33,moments),
-                                     lookup(r12,moments),
-                                     lookup(r13,moments),
-                                     lookup(r23,moments) }};
 
       // Compute turbulent kinetic energy
-      tk::real k = (rs[0] + rs[1] + rs[2]) / 2.0;
+      auto rs = reynoldsStress( depvar, solve, moments );
+
+      // Compute turbulent kinetic energy
+      auto k = (rs[0] + rs[1] + rs[2]) / 2.0;
 
       // Access mean turbulence frequency
       tk::real O = lookup( mean(dissipation_depvar,0), moments );
@@ -288,49 +179,8 @@ class Velocity_ConstDissipation {
                  tk::real& eps,
                  std::array< tk::real, 9 >& G ) const
     {
-      using tk::ctr::lookup;
-      using tk::ctr::mean;
-      using tk::ctr::Product;
-
-      // Extract diagonal of the Reynolds stress
-      Product r11, r22, r33, r12, r13, r23;
-      if (solve == ctr::DepvarType::FULLVAR) {
-
-        using tk::ctr::variance;
-        using tk::ctr::covariance;
-        r11 = variance( depvar, 0 );
-        r22 = variance( depvar, 1 );
-        r33 = variance( depvar, 2 );
-        r12 = covariance( depvar, 0, depvar, 1 );
-        r13 = covariance( depvar, 0, depvar, 2 );
-        r23 = covariance( depvar, 1, depvar, 2 );
-
-      } else if (solve == ctr::DepvarType::FLUCTUATION) {
-
-        // Since we are solving for the fluctuating velocity, the "ordinary"
-        // moments, e.g., <U1U1>, are really central moments, i.e., <u1u1>.
-        using tk::ctr::Term;
-        using tk::ctr::Moment;
-        auto d = static_cast< char >( std::toupper( depvar ) );
-        Term u( d, 0, Moment::ORDINARY );
-        Term v( d, 1, Moment::ORDINARY );
-        Term w( d, 2, Moment::ORDINARY );
-        r11 = tk::ctr::Product( { u, u } );
-        r22 = tk::ctr::Product( { v, v } );
-        r33 = tk::ctr::Product( { w, w } );
-        r12 = tk::ctr::Product( { u, v } );
-        r13 = tk::ctr::Product( { u, w } );
-        r23 = tk::ctr::Product( { v, w } );
-
-      } else Throw( "Depvar type not implemented" );
-
-      // Compute nonzero components of the Reynolds stress
-      std::array< tk::real, 6 > rs{{ lookup(r11,moments),
-                                     lookup(r22,moments),
-                                     lookup(r33,moments),
-                                     lookup(r12,moments),
-                                     lookup(r13,moments),
-                                     lookup(r23,moments) }};
+      // Compute turbulent kinetic energy
+      auto rs = reynoldsStress( depvar, solve, moments );
 
       // Assign constant mean turbulence frequency
       tk::real O = 1.0;
@@ -379,24 +229,15 @@ class Velocity_HydroTimeScale {
                  char,
                  const std::map< tk::ctr::Product, tk::real >& moments,
                  const tk::Table& hts,
-                 ctr::DepvarType,
+                 ctr::DepvarType solve,
                  ctr::VelocityVariantType,
                  kw::sde_c0::info::expect::type C0,
                  tk::real t,
                  tk::real& eps,
                  std::array< tk::real, 9 >& G ) const
     {
-      using tk::ctr::lookup;
-      using tk::ctr::variance;
-
-      // Extract diagonal of the Reynolds stress
-      const auto R11 = variance( depvar, 0 );
-      const auto R22 = variance( depvar, 1 );
-      const auto R33 = variance( depvar, 2 );
-      // compute turbulent kinetic energy
-      tk::real k = ( lookup(R11,moments) +
-                     lookup(R22,moments) +
-                     lookup(R33,moments) ) / 2.0;
+      // Compute turbulent kinetic energy
+      auto k = tke( depvar, solve, moments );
 
       // Sample hydrodynamics timescale and prod/diss at time t
       auto ts = hydrotimescale( t, hts );  // eps/k
