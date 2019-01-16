b55b331ec59d6e006b285ec79a6b0478b2e31679
Jozsef Bakosi <jbakosi@lanl.gov>
2019-01-16 06:03:02 -0700

    Locally ignore compiler warning

--- src/NoWarning/meshwriter.decl.h
+++ src/NoWarning/meshwriter.decl.h
@@ -22,6 +22,7 @@
   #pragma clang diagnostic ignored "-Wextra-semi"
   #pragma clang diagnostic ignored "-Wextra-semi-stmt"
   #pragma clang diagnostic ignored "-Wdouble-promotion"
+  #pragma clang diagnostic ignored "-Wconversion"
   #pragma clang diagnostic ignored "-Wsign-conversion"
   #pragma clang diagnostic ignored "-Wfloat-equal"
   #pragma clang diagnostic ignored "-Wfloat-conversion"
