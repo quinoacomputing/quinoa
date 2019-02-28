c69d21fa7daf6a1cb7ab61857834cc59f363bfdf
Jozsef Bakosi <jbakosi@lanl.gov>
2019-02-28 08:58:06 -0700

    Locally ignore clang warning in Charm++-generated code

--- src/NoWarning/lbswitch.decl.h
+++ src/NoWarning/lbswitch.decl.h
@@ -17,6 +17,7 @@
   #pragma clang diagnostic ignored "-Wunused-parameter"
   #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
   #pragma clang diagnostic ignored "-Wshorten-64-to-32"
+  #pragma clang diagnostic ignored "-Wconversion"
   #pragma clang diagnostic ignored "-Wsign-conversion"
   #pragma clang diagnostic ignored "-Wold-style-cast"
   #pragma clang diagnostic ignored "-Wundef"
