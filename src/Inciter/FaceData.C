0b886c7a314dedd2ab315673de0f0ecfde4cc64e
Jozsef Bakosi <jbakosi@lanl.gov>
2019-03-05 11:47:19 -0700

    Remove dependency of FaceData.C on input deck
    No longer needed as the FaceData object is now only created for DG.

--- src/Inciter/FaceData.C
+++ src/Inciter/FaceData.C
@@ -10,19 +10,9 @@
 */
 // *****************************************************************************
 
-#include <algorithm>
-
 #include "Reorder.h"
 #include "DerivedData.h"
-#include "Inciter/InputDeck/InputDeck.h"
 #include "FaceData.h"
-#include "Centering.h"
-
-namespace inciter {
-
-extern ctr::InputDeck g_inputdeck;
-
-} // inciter::
 
 using inciter::FaceData;
 
@@ -38,16 +28,13 @@ FaceData::FaceData(
 //! \param[in] triinpoel Boundary-face connectivity
 // *****************************************************************************
 {
-  const auto scheme = g_inputdeck.get< tag::discr, tag::scheme >();
-  if (ctr::Scheme().centering(scheme) == tk::Centering::ELEM) {
-    auto esup = tk::genEsup( inpoel, 4 );
-    m_esuel = tk::genEsuelTet( inpoel, esup );
-    auto nbfac = tk::sumvalsize( m_bface );
-    m_nipfac = tk::genNipfac( 4, nbfac, m_esuel );
-    m_inpofa = tk::genInpofaTet( m_nipfac, nbfac, inpoel, m_triinpoel, m_esuel );
-    m_belem =  tk::genBelemTet( nbfac, m_inpofa, esup );
-    m_esuf = tk::genEsuf( 4, m_nipfac, nbfac, m_belem, m_esuel );
-    Assert( m_belem.size() == nbfac,
-           "Number of boundary-elements and number of boundary-faces unequal" );
-  }
+  auto esup = tk::genEsup( inpoel, 4 );
+  m_esuel = tk::genEsuelTet( inpoel, esup );
+  auto nbfac = tk::sumvalsize( m_bface );
+  m_nipfac = tk::genNipfac( 4, nbfac, m_esuel );
+  m_inpofa = tk::genInpofaTet( m_nipfac, nbfac, inpoel, m_triinpoel, m_esuel );
+  m_belem =  tk::genBelemTet( nbfac, m_inpofa, esup );
+  m_esuf = tk::genEsuf( 4, m_nipfac, nbfac, m_belem, m_esuel );
+  Assert( m_belem.size() == nbfac,
+         "Number of boundary-elements and number of boundary-faces unequal" );
 }
