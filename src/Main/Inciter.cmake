827dc41778d21c2cb2b2b1bb93e2b8ad06270f07
Jozsef Bakosi <jbakosi@lanl.gov>
2019-04-02 07:23:15 -0600

    Move PDE/Integrate to its own build target

--- src/Main/Inciter.cmake
+++ src/Main/Inciter.cmake
@@ -16,6 +16,7 @@ target_link_libraries(${INCITER_EXECUTABLE}
                       CGTransportPhysics
                       CompFlowProblem
                       CGCompFlowPhysics
+                      Integrate
                       MeshRefinement
                       LoadBalance
                       ZoltanInterOp
