58e2c704847402a55494faaff991430110ae43c0
Jozsef Bakosi <jbakosi@lanl.gov>
2019-06-10 16:13:37 -0600

    Correct workdir for running tools/recent_mods.sh

--- tools/recent_mods.sh
+++ tools/recent_mods.sh
@@ -18,6 +18,8 @@
 
 echo "Generating recently modified documentation pages..."
 
-export mods=$(paste -d "\n" <(git ls-tree -r --name-only HEAD $1/doc | while read filename; do echo "$(git log -1 --format="%ai" -- $filename) $filename"; done | sort -r | grep dox$ | head -n 5 | awk '{print $4}' | xargs grep -i -- '@page\|\\page\|@mainpage\|\\mainpage' | awk '{ if (!length($3)) print "  @ref index"; else print "  @ref " $3 }') <(git ls-tree -r --name-only HEAD $1/doc | while read filename; do echo "$(git log -1 --format="%ai" -- $filename) $filename"; done | sort -r | grep dox$ | head -n 5 | awk '{print $1}' | xargs -i date -d "{}" +"%b %d, %Y" | awk '{print "  @m_span{m-text m-dim} <em> " $0 " </em> @m_endspan <br>" }'))
+cd $1
+export mods=$(paste -d "\n" <(git ls-tree -r --name-only HEAD doc/pages | while read filename; do echo "$(git log -1 --format="%ai" -- $filename) $filename"; done | sort -r | grep dox$ | head -n 5 | awk '{print $4}' | xargs grep -i -- '@page\|\\page\|@mainpage\|\\mainpage' | awk '{ if (!length($3)) print "  @ref index"; else print "  @ref " $3 }') <(git ls-tree -r --name-only HEAD doc/pages | while read filename; do echo "$(git log -1 --format="%ai" -- $filename) $filename"; done | sort -r | grep dox$ | head -n 5 | awk '{print $1}' | xargs -i date -d "{}" +"%b %d, %Y" | awk '{print "  @m_span{m-text m-dim} <em> " $0 " </em> @m_endspan <br>" }'))
+cd -
 
 perl -i -p0e 's/(\@par Recently modified pages).*(\@section mainpage_tools Tools)/$1\n$ENV{mods}\n\n$2/s' pages/mainpage.dox
