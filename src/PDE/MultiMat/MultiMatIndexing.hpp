26ef4afef8740dea8bb64196deaf11afd9e1a318
adityakpandare <apandare@lanl.gov>
2019-10-01 08:20:16 -0600

    Comments according to code review.

--- src/PDE/MultiMat/MultiMatIndexing.hpp
+++ src/PDE/MultiMat/MultiMatIndexing.hpp
@@ -65,6 +65,8 @@ inline std::size_t pressureIdx( std::size_t /*nmat*/, std::size_t kmat )
 //!   the DG solution vector
 //! \param[in] nmat Number of materials
 //! \param[in] kmat Index of required material
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required material volume fraction
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
@@ -76,6 +78,8 @@ inline std::size_t volfracDofIdx( std::size_t nmat, std::size_t kmat,
 //!   from the DG solution vector
 //! \param[in] nmat Number of materials
 //! \param[in] kmat Index of required material
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required material continuity equation
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
@@ -90,6 +94,8 @@ inline std::size_t densityDofIdx( std::size_t nmat, std::size_t kmat,
 //!   0: X-component,
 //!   1: Y-component,
 //!   2: Z-component.
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required momentum equation component
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
@@ -101,6 +107,8 @@ inline std::size_t momentumDofIdx( std::size_t nmat, std::size_t idir,
 //!   from the DG solution vector
 //! \param[in] nmat Number of materials
 //! \param[in] kmat Index of required material
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required material total energy equation
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
@@ -115,6 +123,8 @@ inline std::size_t energyDofIdx( std::size_t nmat, std::size_t kmat,
 //!   0: X-component,
 //!   1: Y-component,
 //!   2: Z-component.
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required velocity component from vector of primitives
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
@@ -124,7 +134,10 @@ inline std::size_t velocityDofIdx( std::size_t nmat, std::size_t idir,
 
 //! \brief Get the index of the required DOF of material pressure from the DG
 //!   vector of primitives
+//! \param[in] nmat Number of materials
 //! \param[in] kmat Index of required material
+//! \param[in] ndof Number of solution DOFs stored in DG solution vector
+//! \param[in] idof Index of required solution DOF from DG solution vector
 //! \return Index of the required material pressure from vector of primitives
 //! \details This function is used to get the index of the required DOF in the
 //!   solution vector, which is of type tk::Fields.
