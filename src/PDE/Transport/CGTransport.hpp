db872a24fc18721890bc26abf7d161c859567637
Jozsef Bakosi <jbakosi@lanl.gov>
2019-12-09 16:16:24 -0700

    Remove upwind from edges that don't contribute
    Thanks @charest!

--- src/PDE/Transport/CGTransport.hpp
+++ src/PDE/Transport/CGTransport.hpp
@@ -249,7 +249,7 @@ class Transport {
                 for (std::size_t c=0; c<m_ncomp; ++c) {
                   R.var(r[c],p) -= J48 * s * (grad[a][j] - grad[b][j])
                                  * v[c][j]*(ru[0][c] + ru[1][c])
-                   - J48 * std::abs(grad[a][j] - grad[b][j])
+                   - J48 * std::abs(s * (grad[a][j] - grad[b][j]))
                          * std::abs(tk::dot(v[c],n))
                          * (ru[1][c] - ru[0][c]);
 
