e2a2e717b52ad28d3633d0a6ee81065984a987bc
Jozsef Bakosi <jbakosi@lanl.gov>
2019-12-27 12:22:13 -0700

    Add simple TaggedTuple printer
    This can do simple (unformatted) output by iterating through a
    TaggedTuple using brigand::for_each. Simple, because everything goes
    onto a single line. Two versions a defined: an operator<<() and a
    print() function. The first one simply prints the whole tuple, while the
    second one allows for a list of types (tags) whose data will not be
    printed. This latter will be useful to selectively ignore some data when
    printing CmdLine or InputDeck objects.
    
    Note that those tags in the TaggedTuple whose associated data is to be
    printed, must define a static function name(), returning a string that
    is characteristic of the name of the tag, e.g., instead of
    
    struct output {};
    
    now we need
    
    struct output { static std::string name() { return "output"; } };

---
+++ src/Base/TaggedTuplePrint.hpp
@@ -0,0 +1,80 @@
+// *****************************************************************************
+/*!
+  \file      src/Base/TaggedTupleiPrint.hpp
+  \copyright 2012-2015 J. Bakosi,
+             2016-2018 Los Alamos National Security, LLC.,
+             2019 Triad National Security, LLC.
+             All rights reserved. See the LICENSE file for details.
+  \brief     Tagged tuple printer
+  \details   Tagged tuple printer.
+*/
+// *****************************************************************************
+#ifndef TaggedTuplePrinter_h
+#define TaggedTuplePrinter_h
+
+#include <ostream>
+
+#include <brigand/algorithms/for_each.hpp>
+#include <brigand/sequences/has_key.hpp>
+
+#include "NoWarning/set.hpp"
+#include "TaggedTuple.hpp"
+
+namespace tk {
+
+//! Function object type to print contents of a TaggedTuple
+//! \tparam List brigand::list of types in the tagged tuple
+//! \tparam Ignore brigand::list of types to not print
+template< class List, class Ignore = brigand::set<> >
+struct TuplePrinter {
+  std::ostream& os;
+  const tk::TaggedTuple< List >& tuple;
+  //! Constructor
+  TuplePrinter( std::ostream& s, const tk::TaggedTuple< List >& t ) :
+    os(s), tuple(t) {}
+  //! Function call operator templated on the type being output
+  template< typename Key > void operator()( brigand::type_<Key> ) {
+    using ignored = brigand::has_key< Ignore, Key >;
+    if constexpr( std::is_same_v< ignored, brigand::false_type > ) {
+      using Tuple = tk::TaggedTuple< List >;
+      const auto& key = Key::name();
+      const auto& value = tuple.template get< Key >();
+      if constexpr( Tuple::template is_tagged_tuple< Key >::value )
+        os << "{ " << key << ": " << value << "} ";
+      else
+        os << key << ": '" << value << "' ";
+    }
+  }
+};
+
+//! Simple (unformatted) output of a TaggedTuple to an output stream
+//! \tparam List brigand::list of types in the tagged tuple
+//! \param os Output stream to output to
+//! \param t TaggedTuple to print
+//! \return Output stream
+template< class List >
+inline std::ostream&
+operator<< ( std::ostream& os, const tk::TaggedTuple< List >& t ) {
+  using keys = typename tk::TaggedTuple< List >::Keys;
+  os << std::boolalpha;
+  brigand::for_each< keys >( TuplePrinter< List >( os, t ) );
+  return os;
+}
+
+//! Simple (unformatted) output of a TaggedTuple to an output stream
+//! \tparam List brigand::list of types in the tagged tuple
+//! \tparam Ignore brigand::list of types to not print
+//! \param os Output stream to output to
+//! \param t TaggedTuple to print
+//! \return Output stream
+template< class List, class Ignore >
+inline void
+print( std::ostream& os, const tk::TaggedTuple< List >& t ) {
+  using keys = typename tk::TaggedTuple< List >::Keys;
+  os << std::boolalpha;
+  brigand::for_each< keys >( TuplePrinter< List, Ignore >( os, t ) );
+}
+
+} // tk::
+
+#endif // TaggedTuple_h
