11527aea5260bc86e918dfe9cd769fc6bbafb605
Jozsef Bakosi <jbakosi@lanl.gov>
2019-12-28 19:31:48 -0700

    Fix comment

--- tests/unit/Base/TestTaggedTuple.cpp
+++ tests/unit/Base/TestTaggedTuple.cpp
@@ -5,8 +5,8 @@
              2016-2018 Los Alamos National Security, LLC.,
              2019 Triad National Security, LLC.
              All rights reserved. See the LICENSE file for details.
-  \brief     Unit tests for tk::tuple::TaggedTuple
-  \details   Unit tests for tk::tuple::TaggedTuple
+  \brief     Unit tests for tk::TaggedTuple
+  \details   Unit tests for tk::TaggedTuple
 */
 // *****************************************************************************
 
