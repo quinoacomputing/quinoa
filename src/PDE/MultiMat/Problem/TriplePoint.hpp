f5b8f938b11c9c13c46fcb5baff41dd368d11d21
adityakpandare <apandare@lanl.gov>
2020-01-03 12:23:20 -0700

    Removed TriplePoint::side()

--- src/PDE/MultiMat/Problem/TriplePoint.hpp
+++ src/PDE/MultiMat/Problem/TriplePoint.hpp
@@ -52,10 +52,6 @@ class MultiMatProblemTriplePoint {
     static tk::SrcFn::result_type
     src( ncomp_t, ncomp_t, tk::real, tk::real, tk::real, tk::real );
 
-    //! \brief Query all side set IDs the user has configured for all components
-    //!   in this PDE system
-    static void side( std::unordered_set< int >& conf );
-
     //! Return field names to be output to file
     static std::vector< std::string > fieldNames( ncomp_t );
 
