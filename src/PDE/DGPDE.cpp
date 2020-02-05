56d89aca22811f12767bad7a60776d620933514a
Jozsef Bakosi <jbakosi@lanl.gov>
2020-02-04 17:16:48 -0700

    Add missing new file: PDE/DGPDE.cpp

---
+++ src/PDE/DGPDE.cpp
@@ -0,0 +1,26 @@
+// *****************************************************************************
+/*!
+  \file      src/PDE/DGPDE.cpp
+  \copyright 2012-2015 J. Bakosi,
+             2016-2018 Los Alamos National Security, LLC.,
+             2019-2020 Triad National Security, LLC.
+             All rights reserved. See the LICENSE file for details.
+  \brief     Functions common to discontinuous Galerkin PDE types
+  \details   Functions common to discontinuous Galerkin PDE types.
+*/
+// *****************************************************************************
+
+#include "DGPDE.hpp"
+
+tk::StateFn::result_type
+inciter::invalidBC( ncomp_t, ncomp_t, const std::vector< tk::real >&,
+           tk::real, tk::real, tk::real, tk::real,
+           const std::array< tk::real, 3> & )
+// *****************************************************************************
+//! State function for invalid/un-configured boundary conditions
+//! \note The function signature must follow tk::StateFn
+// *****************************************************************************
+{
+  Throw( "Invalid boundary condition set up in input file or the PDE does not "
+          "support this BC type" );
+}
