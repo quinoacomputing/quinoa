49a798daac8cc01a9399592e7ad3d2fc7ae86e73
Jozsef Bakosi <jbakosi@lanl.gov>
2020-02-20 12:47:00 -0700

    Squashed 'cmake/' changes from 309aaf2b2..c32920c65
    c32920c65 Add cmake code so quinoa looks for Lua and Sol2
    9f1b42b97 Add FindSol2.cmake
    ca743c97f Update copyright year
    
    git-subtree-dir: cmake
    git-subtree-split: c32920c653df24ee96e14aae4b533ff697157d5b

--- DisallowInSourceBuilds.cmake
+++ DisallowInSourceBuilds.cmake
@@ -3,7 +3,7 @@
 # \file      DisallowInSourceBuilds.cmake
 # \copyright 2012-2015 J. Bakosi,
 #            2016-2018 Los Alamos National Security, LLC.,
-#            2019 Triad National Security, LLC.
+#            2019-2020 Triad National Security, LLC.
 #            All rights reserved. See the LICENSE file for details.
 # \brief     Cmake code to disallow in-source builds
 #
---
+++ FindSol2.cmake
@@ -0,0 +1,43 @@
+################################################################################
+#
+# \file      FindSol2.cmake
+# \copyright 2012-2015 J. Bakosi,
+#            2016-2018 Los Alamos National Security, LLC.,
+#            2019-2020 Triad National Security, LLC.
+#            All rights reserved. See the LICENSE file for details.
+# \brief     Find the Sol2 Lua C++ binding library
+#
+################################################################################
+
+# Sol2: https://github.com/ThePhD/sol2
+#
+#  SOL2_FOUND - System has Sol2
+#  SOL2_INCLUDE_DIRS - The Sol2 include directory
+#
+#  Set the SOL2_ROOT cmake variable or shell environment variable before
+#  calling find_package to a path to add an additional search path, e.g.,
+#
+#  Usage:
+#
+#  set(SOL2_ROOT "/path/to/custom/brigand") # prefer over system
+#  find_package(Sol2)
+#  include_directories(${SOL2_INCLUDE_DIRS})
+
+# If already in cache, be silent
+if(SOL2_INCLUDE_DIRS)
+  set (SOL2_FIND_QUIETLY TRUE)
+endif()
+
+find_path(SOL2_INCLUDE_DIR NAMES sol.hpp
+                           HINTS ${SOL2_ROOT}/include
+                                 $ENV{SOL2_ROOT}/include
+                           PATH_SUFFIXES sol)
+
+set(SOL2_INCLUDE_DIRS ${SOL2_INCLUDE_DIR})
+
+# Handle the QUIETLY and REQUIRED arguments and set SOL2_FOUND to TRUE if
+# all listed variables are TRUE.
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Sol2 REQUIRED_VARS SOL2_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(SOL2_INCLUDE_DIRS)
