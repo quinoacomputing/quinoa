1d3d96bbd64d6c0816fe65c5b5c4c74155b58cc3
Jozsef Bakosi <jbakosi@lanl.gov>
2020-07-13 17:24:24 -0600

    Add some comments and convert some output to diagnostics

--- src/Inciter/ALECG.cpp
+++ src/Inciter/ALECG.cpp
@@ -431,8 +431,18 @@ ALECG::box( tk::real v )
   // Set user-defined IC box conditions
   for (const auto& eq : g_cgpde)
     eq.box( d->Boxvol(), d->T(), m_boxnodes, d->Coord(), m_u, m_boxnodes_set );
-  if (m_boxnodes_set.size() != m_boxnodes.size())
-    std::cout << thisIndex << ':' << m_boxnodes.size() - m_boxnodes_set.size() << '\n';
+
+  // If initiation type is linear, output number of nodes not yet initiated
+  const auto& ic = g_inputdeck.get< tag::param, eq, tag::ic >();
+  const auto& icbox = ic.get< tag::box >();
+  const auto& initiate = icbox.get< tag::initiate >();
+  const auto& inittype = initiate.get< tag::init >();
+  if (inittype[m_system] == ctr::InitiateType::LINEAR &&
+      m_boxnodes_set.size() != m_boxnodes.size())
+  {
+    std::cout << thisIndex << ':' << m_boxnodes.size() - m_boxnodes_set.size()
+              << '\n';
+  }
 
   // Compute left-hand side of PDEs
   lhs();
@@ -958,8 +968,18 @@ ALECG::solve()
   for (const auto& eq : g_cgpde)
     eq.box( d->Boxvol(), d->T()+d->Dt(), m_boxnodes, d->Coord(), m_u,
             m_boxnodes_set );
-  //if (m_boxnodes_set.size() != m_boxnodes.size())
-  //  std::cout << thisIndex << ':' << m_boxnodes.size() - m_boxnodes_set.size() << '\n';
+
+  // If initiation type is linear, output number of nodes not yet initiated
+  const auto& ic = g_inputdeck.get< tag::param, eq, tag::ic >();
+  const auto& icbox = ic.get< tag::box >();
+  const auto& initiate = icbox.get< tag::initiate >();
+  const auto& inittype = initiate.get< tag::init >();
+  if (inittype[m_system] == ctr::InitiateType::LINEAR &&
+      m_boxnodes_set.size() != m_boxnodes.size())
+  {
+    std::cout << thisIndex << ':' << m_boxnodes.size() - m_boxnodes_set.size()
+              << '\n';
+  }
 
   //! [Continue after solve]
   if (m_stage < 2) {
