9ed4b3b149f40c7a042a705de35eedba49131a70
Weizhao Li <wli37@ncsu.edu>
2022-02-28 16:19:35 -0500

    Temporally remove gradient condition for triggering consistent limiting due to DGP2

--- src/PDE/Limiter.cpp
+++ src/PDE/Limiter.cpp
@@ -1690,7 +1690,7 @@ void consistentMultiMatLimiting_P1(
   // to be applied. This if-test says that, the fix is applied when the change
   // in volume-fraction across a cell is greater than 0.1, *and* the
   // volume-fraction is between 0.1 and 0.9.
-  if ( dalmax > al_band &&
+  if ( //dalmax > al_band &&
        (almax > al_band && almax < (1.0-al_band)) )
   {
     // 1. consistent high-order dofs
