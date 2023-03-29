61af885cfddb3b15f1b2fbced2e340b8d6a7bfbd
adityakpandare <apandare@lanl.gov>
2023-03-29 09:37:57 -0600

    Squashed 'cmake/' content from commit 31eff65ce
    git-subtree-dir: cmake
    git-subtree-split: 31eff65ce1d52becc309ccf032fcfde6a8b2349e

---
+++ FindPEGTL.cmake
@@ -0,0 +1,79 @@
+################################################################################
+#
+# \file      FindPEGTL.cmake
+# \copyright 2012-2015 J. Bakosi,
+#            2016-2018 Los Alamos National Security, LLC.,
+#            2019-2021 Triad National Security, LLC.
+#            All rights reserved. See the LICENSE file for details.
+# \brief     Find PEGTL
+#
+################################################################################
+
+# PEGTL: https://github.com/taocpp/PEGTL
+#
+#  PEGTL_FOUND - System has PEGTL
+#  PEGTL_INCLUDE_DIRS - The PEGTL include directory
+#
+#  Set the PEGTL_ROOT cmake variable or shell environment variable before
+#  calling find_package to a path to add an additional search path, e.g.,
+#
+#  Usage:
+#
+#  set(PEGTL_ROOT "/path/to/custom/pegtl") # prefer over system
+#  find_package(PEGTL)
+#  include_directories(${PEGTL_INCLUDE_DIRS})
+
+function(_PEGTL_GET_VERSION _OUT_major _OUT_minor _OUT_micro _version_hdr)
+    file(STRINGS ${_version_hdr} _contents REGEX "#define TAO.*_PEGTL_VERSION_[A-Z]+[ \t]+")
+    if(_contents)
+        string(REGEX REPLACE ".*#define TAO.*_PEGTL_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" ${_OUT_major} "${_contents}")
+        string(REGEX REPLACE ".*#define TAO.*_PEGTL_VERSION_MINOR[ \t]+([0-9]+).*" "\\1" ${_OUT_minor} "${_contents}")
+        string(REGEX REPLACE ".*#define TAO.*_PEGTL_VERSION_PATCH[ \t]+([0-9]+).*" "\\1" ${_OUT_micro} "${_contents}")
+
+        if(NOT ${_OUT_major} MATCHES "[0-9]+")
+            message(FATAL_ERROR "Version parsing failed for TAO.*_PEGTL_VERSION_MAJOR!")
+        endif()
+        if(NOT ${_OUT_minor} MATCHES "[0-9]+")
+            message(FATAL_ERROR "Version parsing failed for TAO.*_PEGTL_VERSION_MINOR!")
+        endif()
+        if(NOT ${_OUT_micro} MATCHES "[0-9]+")
+            message(FATAL_ERROR "Version parsing failed for TAO.*_PEGTL_VERSION_PATCH!")
+        endif()
+
+        set(${_OUT_major} ${${_OUT_major}} PARENT_SCOPE)
+        set(${_OUT_minor} ${${_OUT_minor}} PARENT_SCOPE)
+        set(${_OUT_micro} ${${_OUT_micro}} PARENT_SCOPE)
+
+    else()
+        message(FATAL_ERROR "Include file ${_version_hdr} does not exist")
+    endif()
+endfunction()
+
+# If already in cache, be silent
+if(PEGTL_INCLUDE_DIRS)
+  set (PEGTL_FIND_QUIETLY TRUE)
+endif()
+
+find_path(PEGTL_INCLUDE_DIR NAMES pegtl.hpp
+                            HINTS ${PEGTL_ROOT}/include
+                                  $ENV{PEGTL_ROOT}/include
+                            PATH_SUFFIXES tao pegtl/include/tao)
+if(PEGTL_INCLUDE_DIR)
+  _PEGTL_GET_VERSION(PEGTL_MAJOR_VERSION PEGTL_MINOR_VERSION PEGTL_PATCH_VERSION ${PEGTL_INCLUDE_DIR}/pegtl/version.hpp)
+  set(PEGTL_VERSION ${PEGTL_MAJOR_VERSION}.${PEGTL_MINOR_VERSION}.${PEGTL_PATCH_VERSION})
+else()
+  set(PEGTL_VERSION 0.0.0)
+endif()
+
+if(PEGTL_INCLUDE_DIR)
+  set(PEGTL_INCLUDE_DIRS ${PEGTL_INCLUDE_DIR})
+else()
+  set(PEGTL_INCLUDE_DIRS "")
+endif()
+
+# Handle the QUIETLY and REQUIRED arguments and set PEGTL_FOUND to TRUE if
+# all listed variables are TRUE.
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PEGTL REQUIRED_VARS PEGTL_INCLUDE_DIRS VERSION_VAR PEGTL_VERSION)
+
+MARK_AS_ADVANCED(PEGTL_INCLUDE_DIRS)
---
+++ get_compiler_flags.cmake
@@ -0,0 +1,69 @@
+################################################################################
+#
+# \file      get_compiler_flags.cmake
+# \copyright 2012-2015 J. Bakosi,
+#            2016-2018 Los Alamos National Security, LLC.,
+#            2019-2021 Triad National Security, LLC.
+#            All rights reserved. See the LICENSE file for details.
+# \brief     Cmake code to get compiler flags
+#
+################################################################################
+
+macro(get_compiler_flags)
+
+  # Convert CMAKE_BUILD_TYPE to upper case so even if the user gives it via the
+  # command line, the tests below work as intended
+  string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
+
+  # Forward cmake compiler flags to automake builds by setting our own variables
+  # CFLAGS, CXXFLAGS, and FFLAGS. Only these flags should then be used by the
+  # individual external projects builds.
+  if(CMAKE_BUILD_TYPE STREQUAL "")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "DEBUG")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_DEBUG}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_DEBUG}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "RELEASE")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_RELEASE}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_RELEASE}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "RELWITHDEBINFO")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_RELWITHDEBINFO}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_RELWITHDEBINFO}")
+  endif()
+  if(CMAKE_BUILD_TYPE STREQUAL "MINSIZEREL")
+    set(CFLAGS "${CFLAGS} ${CMAKE_C_FLAGS_MINSIZEREL}")
+    set(CXXFLAGS "${CXXFLAGS} ${CMAKE_CXX_FLAGS_MINSIZEREL}")
+    set(FFLAGS "${FFLAGS} ${CMAKE_Fortran_FLAGS_MINSIZEREL}")
+  endif()
+
+  # Remove first empty character of compiler flag strings
+  if (CFLAGS)
+    STRING(REGEX REPLACE "^ " "" CFLAGS ${CFLAGS})
+  endif()
+  if (CXXFLAGS)
+    STRING(REGEX REPLACE "^ " "" CXXFLAGS ${CXXFLAGS})
+  endif()
+  if (FFLAGS)
+    STRING(REGEX REPLACE "^ " "" FFLAGS ${FFLAGS})
+  endif()
+
+  # Echo compiler flags string used
+  if (CFLAGS)
+    message(STATUS "C compiler flags: '${CFLAGS}'")
+  endif()
+  if (CXXFLAGS)
+    message(STATUS "C++ compiler flags: '${CXXFLAGS}'")
+  endif()
+  if (FFLAGS)
+    message(STATUS "Fortran compiler flags: '${FFLAGS}'")
+  endif()
+
+endmacro()
