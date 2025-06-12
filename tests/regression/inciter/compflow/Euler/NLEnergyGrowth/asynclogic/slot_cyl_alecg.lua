6da757af08380df23b486c6157ef625e51b53f58
adityakpandare <apandare@lanl.gov>
2025-06-12 16:15:45 -0600

    removed most tests

---
+++ tests/regression/inciter/compflow/Euler/NLEnergyGrowth/asynclogic/slot_cyl_alecg.lua
@@ -0,0 +1,38 @@
+inciter = {
+
+  title = "Euler equations computing nonlinear energy growth",
+
+  nstep = 10,    -- Max number of time steps
+  dt = 0.001,    -- Time step size
+  ttyi = 1,      -- TTY output interval
+  scheme = "alecg",
+
+  compflow = {
+    physics = "euler",
+    problem = "nl_energy_growth",
+    alpha = 0.25,
+    betax = 1.0,
+    betay = 0.75,
+    betaz = 0.5,
+    r0 = 2.0,
+    ce = -1.0,
+    kappa = 0.8
+  },
+
+  material = {
+    {
+      gamma = { 1.66666666666667 } -- =5/3 ratio of specific heats
+    }
+  },
+
+  bc = {
+    {
+      dirichlet = { 1, 2, 3, 4, 5, 6 }
+    }
+  },
+
+  field_output = {
+    interval = 5
+  }
+
+}
