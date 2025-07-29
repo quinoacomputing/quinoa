82a368cd680eac3038be22b83049670dc3eaf3d7
fairaudo <airaudofacundo@gmail.com>
2025-07-29 11:28:39 -0600

    added j != l check to save time

--- src/PDE/Integrate/Surface.cpp
+++ src/PDE/Integrate/Surface.cpp
@@ -364,14 +364,15 @@ update_rhs_fa( ncomp_t ncomp,
         for (std::size_t i=0; i<3; ++i)
           for (std::size_t j=0; j<3; ++j)
             for (std::size_t l=0; l<3; ++l)
-            {
-              std::size_t mark1 = ncomp+nmat+1+3*nsld+9*(solidx[k]-1)+3*i+l;
-              std::size_t mark2 = ncomp+nmat+1+3*nsld+9*(solidx[k]-1)+3*i+j;
-              riemannDeriv[3*nmat+ndof+3*nsld+27*(solidx[k]-1)+3*(3*i+j)+l][el] -=
-                wt * ( fl[mark1] * fn[j] - fl[mark2] * fn[l]);
-              riemannDeriv[3*nmat+ndof+3*nsld+27*(solidx[k]-1)+3*(3*i+j)+l][er] +=
-                wt * ( fl[mark1] * fn[j] - fl[mark2] * fn[l]);
-            }
+              if (j != l)
+              {
+                std::size_t mark1 = ncomp+nmat+1+3*nsld+9*(solidx[k]-1)+3*i+l;
+                std::size_t mark2 = ncomp+nmat+1+3*nsld+9*(solidx[k]-1)+3*i+j;
+                riemannDeriv[3*nmat+ndof+3*nsld+27*(solidx[k]-1)+3*(3*i+j)+l][el] -=
+                  wt * ( fl[mark1] * fn[j] - fl[mark2] * fn[l]);
+                riemannDeriv[3*nmat+ndof+3*nsld+27*(solidx[k]-1)+3*(3*i+j)+l][er] +=
+                  wt * ( fl[mark1] * fn[j] - fl[mark2] * fn[l]);
+              }
   }
 }
 
