f7340b02f09343ca6fe098a01ecac9c4a839ecc0
fairaudo <airaudofacundo@gmail.com>
2025-09-17 16:49:19 -0600

    fixed bug with new control variable

--- src/Inciter/DG.cpp
+++ src/Inciter/DG.cpp
@@ -1401,7 +1401,7 @@ DG::dt()
 
       // time-step suppression for unsteady problems
       tk::real coeff(1.0);
-      if (g_inputdeck.get< tag::cfl_ramping > && d->It() < 100) coeff = 0.01 * static_cast< tk::real >(d->It()+1);
+      if (g_inputdeck.get< tag::cfl_ramping >() && d->It() < 100) coeff = 0.01 * static_cast< tk::real >(d->It()+1);
 
       mindt *= coeff * g_inputdeck.get< tag::cfl >();
     }
@@ -2546,7 +2546,6 @@ std::vector< tk::real > DG::nonlinear_broyden(std::size_t e,
           x_old[i] = x[i];
           f_old[i] = f[i];
         }
-        abs_err_old = abs_err;
 
         // check if error condition is met and loop back
         if (rel_err < rel_tol || abs_err < abs_tol)
