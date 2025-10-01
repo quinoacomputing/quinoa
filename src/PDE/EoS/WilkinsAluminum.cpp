1b05d74f17cce248fb16f062ac9f73c930621807
adityakpandare <apandare@lanl.gov>
2025-10-01 11:50:31 -0600

    cleaned up the CauchyStress function args

--- src/PDE/EoS/WilkinsAluminum.cpp
+++ src/PDE/EoS/WilkinsAluminum.cpp
@@ -154,18 +154,12 @@ WilkinsAluminum::pressure(
 
 std::array< std::array< tk::real, 3 >, 3 >
 WilkinsAluminum::CauchyStress(
-  tk::real,
-  tk::real,
-  tk::real,
-  tk::real,
-  tk::real,
   tk::real alpha,
   std::size_t /*imat*/,
   const std::array< std::array< tk::real, 3 >, 3 >& defgrad ) const
 // *************************************************************************
-//! \brief Calculate the elastic Cauchy stress tensor from the material density,
-//!   momentum, total energy, and inverse deformation gradient tensor using the
-//!   WilkinsAluminum equation of state
+//! \brief Calculate the elastic Cauchy stress tensor from the material
+//!   inverse deformation gradient tensor using the WilkinsAluminum EOS
 //! \param[in] alpha Material volume fraction. Default is 1.0, so that for
 //!   the single-material system, this argument can be left unspecified by
 //!   the calling code
