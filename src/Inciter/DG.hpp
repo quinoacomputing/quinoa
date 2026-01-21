6292d33d45aeefe4ee513769ca832f13c7264b51
g-rydquist <grydquist@lanl.gov>
2026-01-21 07:53:20 -0700

    Remove F as argument from collectDt()

--- src/Inciter/DG.hpp
+++ src/Inciter/DG.hpp
@@ -214,7 +214,7 @@ class DG : public CBase_DG {
     void transferSol() {}
 
     //! (no-op)
-    void advance( tk::real, std::array< tk::real, 6 > ) {}
+    void advance( tk::real ) {}
 
     //! (no-op)
     void storeForces( std::array< tk::real, 6 > ) {}
