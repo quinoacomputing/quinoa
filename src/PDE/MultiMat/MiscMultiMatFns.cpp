6d338331b2a3ec79684f5b4055ac510a8b6036c3
adityakpandare <apandare@lanl.gov>
2026-09-12 05:58:22 -0600

    add mass-avg sound-speed in dg::dt calcs

--- src/PDE/MultiMat/MiscMultiMatFns.cpp
+++ src/PDE/MultiMat/MiscMultiMatFns.cpp
@@ -352,6 +352,8 @@ timeStepSizeMultiMat(
 {
   const auto ndof = g_inputdeck.get< tag::ndof >();
   const auto rdof = g_inputdeck.get< tag::rdof >();
+  const auto use_mass_avg =
+    g_inputdeck.get< tag::multimat, tag::dt_sos_massavg >();
   std::size_t ncomp = U.nprop()/rdof;
   std::size_t nprim = P.nprop()/rdof;
 
@@ -390,16 +392,30 @@ timeStepSizeMultiMat(
 
     // acoustic speed
     a = 0.0;
+    tk::real mixtureDensity = 0.0;
     for (std::size_t k=0; k<nmat; ++k)
     {
-      if (ugp[volfracIdx(nmat, k)] > 1.0e-04) {
-        auto gk = getDeformGrad(nmat, k, ugp);
-        gk = tk::rotateTensor(gk, fn);
-        a = std::max( a, mat_blk[k].compute< EOS::soundspeed >(
-          ugp[densityIdx(nmat, k)],
-          pgp[pressureIdx(nmat, k)], ugp[volfracIdx(nmat, k)], k, gk ) );
+      auto gk = getDeformGrad(nmat, k, ugp);
+      gk = tk::rotateTensor(gk, fn);
+      if (use_mass_avg > 0)
+      {
+        // mass averaging SoS
+        a += ugp[densityIdx(nmat,k)]*mat_blk[k].compute< EOS::soundspeed >(
+          ugp[densityIdx(nmat, k)], pgp[pressureIdx(nmat, k)],
+          ugp[volfracIdx(nmat, k)], k, gk );
+
+        mixtureDensity += ugp[densityIdx(nmat,k)];
+      }
+      else
+      {
+        if (ugp[volfracIdx(nmat, k)] > 1.0e-04) {
+          a = std::max( a, mat_blk[k].compute< EOS::soundspeed >(
+            ugp[densityIdx(nmat, k)],
+            pgp[pressureIdx(nmat, k)], ugp[volfracIdx(nmat, k)], k, gk ) );
+        }
       }
     }
+    if (use_mass_avg > 0) a /= mixtureDensity;
 
     dSV_l = geoFace(f,0) * (std::fabs(vn) + a);
 
